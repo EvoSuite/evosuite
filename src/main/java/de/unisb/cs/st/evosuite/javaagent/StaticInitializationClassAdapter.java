cd82a6c54b21f42f3a01e2b4b1ad00c93ef9eb1f
fraser <none@none>
2011-02-11 21:43:23 +0100

    Do not duplicate static constructor of interfaces (who would have thought such a thing exists!)

--- src/main/java/de/unisb/cs/st/evosuite/javaagent/StaticInitializationClassAdapter.java
+++ src/main/java/de/unisb/cs/st/evosuite/javaagent/StaticInitializationClassAdapter.java
@@ -3,18 +3,17 @@
  * 
  * This file is part of EvoSuite.
  * 
- * EvoSuite is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * EvoSuite is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser Public License
- * along with EvoSuite.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.unisb.cs.st.evosuite.javaagent;
@@ -29,34 +28,47 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 /**
- * Duplicate static initializers in methods, such that we can explicitly
- * restore the initial state of classes.
+ * Duplicate static initializers in methods, such that we can explicitly restore
+ * the initial state of classes.
  * 
  * @author Gordon Fraser
- *
+ * 
  */
 public class StaticInitializationClassAdapter extends ClassAdapter {
 
-	
-	private String className;
-	
+	private final String className;
+
 	public static List<String> static_classes = new ArrayList<String>();
-	
+
 	private static Logger logger = Logger.getLogger(StaticInitializationClassAdapter.class);
-	
-	public StaticInitializationClassAdapter (ClassVisitor visitor, String className) {
+
+	private boolean isInterface = false;
+
+	public StaticInitializationClassAdapter(ClassVisitor visitor, String className) {
 		super(visitor);
 		this.className = className;
 	}
 
-	public MethodVisitor visitMethod(int methodAccess, String name,
-			String descriptor, String signature, String[] exceptions) {
+	@Override
+	public void visit(int version, int access, String name, String signature,
+	        String superName, String[] interfaces) {
+		super.visit(version, access, name, signature, superName, interfaces);
+		isInterface = ((access & Opcodes.ACC_INTERFACE) == Opcodes.ACC_INTERFACE);
+	}
 
-		MethodVisitor mv = super.visitMethod(methodAccess, name, descriptor,
-				signature, exceptions);
-		if(name.equals("<clinit>")) {
-			logger.info("Found static initializer in class "+className);
-			MethodVisitor mv2 = super.visitMethod(methodAccess | Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "__STATIC_RESET", descriptor, signature, exceptions);
+	@Override
+	public MethodVisitor visitMethod(int methodAccess, String name, String descriptor,
+	        String signature, String[] exceptions) {
+
+		MethodVisitor mv = super.visitMethod(methodAccess, name, descriptor, signature,
+		                                     exceptions);
+		if (name.equals("<clinit>") && !isInterface) {
+			logger.info("Found static initializer in class " + className);
+			MethodVisitor mv2 = super.visitMethod(methodAccess | Opcodes.ACC_PUBLIC
+			                                              | Opcodes.ACC_STATIC,
+			                                      "__STATIC_RESET", descriptor,
+			                                      signature,
+			                                      exceptions);
 			static_classes.add(className.replace('/', '.'));
 			return new MultiMethodVisitor(mv2, mv);
 		}
