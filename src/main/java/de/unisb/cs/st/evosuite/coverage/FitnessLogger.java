aa7405f0a3822de54c83948f0bdd673d3946f581
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2011-02-22 15:30:05 +0100

    Add missing file

---
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/FitnessLogger.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.coverage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import de.unisb.cs.st.evosuite.Properties;
+import de.unisb.cs.st.evosuite.ga.Chromosome;
+import de.unisb.cs.st.evosuite.ga.FitnessFunction;
+import de.unisb.cs.st.evosuite.ga.SearchListener;
+import de.unisb.cs.st.evosuite.testcase.MaxStatementsStoppingCondition;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class FitnessLogger implements SearchListener {
+
+	private static Logger logger = Logger.getLogger(FitnessLogger.class);
+
+	private final List<Integer> evaluations_history = new ArrayList<Integer>();
+
+	private final List<Integer> statements_history = new ArrayList<Integer>();
+
+	private final List<Double> fitness_history = new ArrayList<Double>();
+
+	private final List<Integer> size_history = new ArrayList<Integer>();
+
+	private String name = null;
+
+	private int evaluations = 0;
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.ga.SearchListener#searchStarted(de.unisb.cs.st.evosuite.ga.FitnessFunction)
+	 */
+	@Override
+	public void searchStarted(FitnessFunction objective) {
+		evaluations = 0;
+		evaluations_history.clear();
+		statements_history.clear();
+		fitness_history.clear();
+		size_history.clear();
+		File dir = new File(Properties.REPORT_DIR + "/goals/");
+		dir.mkdir();
+		name = Properties.REPORT_DIR
+		        + "/goals/"
+		        + objective.toString().replace(" ", "_").replace(":", "-").replace("(",
+		                                                                           "").replace(")",
+		                                                                                       "");
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.ga.SearchListener#iteration(java.util.List)
+	 */
+	@Override
+	public void iteration(List<Chromosome> population) {
+		if (population.isEmpty())
+			return;
+
+		evaluations_history.add(evaluations);
+		statements_history.add(MaxStatementsStoppingCondition.getNumExecutedStatements());
+		fitness_history.add(population.get(0).getFitness());
+		size_history.add(population.get(0).size());
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.ga.SearchListener#searchFinished(java.util.List)
+	 */
+	@Override
+	public void searchFinished(List<Chromosome> population) {
+		if (name == null)
+			return;
+
+		File f = new File(name);
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(f, true));
+			out.write("Iteration,Evaluations,Statements,Fitness,Size\n");
+			for (int i = 0; i < fitness_history.size(); i++) {
+				out.write(i + ",");
+				out.write(evaluations_history.get(i) + ",");
+				out.write(statements_history.get(i) + ",");
+				out.write(fitness_history.get(i) + ",");
+				out.write(size_history.get(i) + "\n");
+			}
+			out.close();
+		} catch (IOException e) {
+			logger.error("Could not open csv file: " + e);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.ga.SearchListener#fitnessEvaluation(de.unisb.cs.st.evosuite.ga.Chromosome)
+	 */
+	@Override
+	public void fitnessEvaluation(Chromosome individual) {
+		evaluations++;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.unisb.cs.st.evosuite.ga.SearchListener#modification(de.unisb.cs.st.evosuite.ga.Chromosome)
+	 */
+	@Override
+	public void modification(Chromosome individual) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
