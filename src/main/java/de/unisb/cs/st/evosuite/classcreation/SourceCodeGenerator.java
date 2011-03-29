ed3e2a19e327363368a5f6e841353cc8d350a1c6
Andrey Tarasevich <devnull@localhost>
2011-03-29 03:18:20 +0200

    Added org.eclipse.jdt dependency. Please update maven dependencies. Added generation of the abstract class stubs.

---
+++ src/main/java/de/unisb/cs/st/evosuite/classcreation/SourceCodeGenerator.java
@@ -0,0 +1,403 @@
+/*
+ * Copyright (C) 2010 Saarland University
+ * 
+ * This file is part of EvoSuite.
+ * 
+ * EvoSuite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EvoSuite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with EvoSuite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.unisb.cs.st.evosuite.classcreation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.MarkerAnnotation;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.PackageDeclaration;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+/**
+ * Generates classes from given abstract methods and constructors.
+ * 
+ * Most of the code is written with AST API, but reflection API is
+ * used in order to get abstract class parameters: methods, 
+ * constructors etc.
+ * 
+ * @author Andrey Tarasevich
+ *
+ */
+public class SourceCodeGenerator {
+	
+	/** Current AST object. */
+	private AST ast;
+	
+	/** Container for the current AST. */
+	private CompilationUnit unit;
+	
+	/** List of the abstract methods for original abstract class */
+	private List<Method> abstractMethods = new LinkedList<Method>();
+	
+	/** Original abstract class. */
+	private Class<?> clazz;
+	
+	/** List of the constructors for original abstract class. */
+	private Constructor<?>[] abstractConstructors;
+	
+	/**
+	 * @param clazz abstract class for which stub should be generated.
+	 */
+	public SourceCodeGenerator(Class<?> clazz){
+		ast = AST.newAST(AST.JLS3);
+		unit = ast.newCompilationUnit();
+		this.clazz = clazz;
+		
+		// Get constructors and methods of abstract class. 
+		Method[] methods = clazz.getDeclaredMethods();
+		abstractConstructors = clazz.getConstructors();
+		
+		// Filter method and select only abstract methods.
+		for(Method m : methods)
+			if(java.lang.reflect.Modifier.isAbstract(m.getModifiers()))
+				abstractMethods.add(m);
+	}
+	
+	/**
+	 * Generates stub source code.
+	 * 
+	 * @return compilation unit of the stub
+	 */
+	@SuppressWarnings("unchecked")
+	public CompilationUnit generateSourceCode(){
+		PackageDeclaration pd = ast.newPackageDeclaration();
+		pd.setName(ast.newName(clazz.getPackage().getName()));
+		unit.setPackage(pd);
+		
+		// Create class stub.
+		TypeDeclaration type = ast.newTypeDeclaration();
+		type.setName(ast.newSimpleName(clazz.getSimpleName() + "Stub"));
+		type.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+		
+		type.setInterface(false);
+		
+		// Set inheritance.
+		type.setSuperclassType(ast.newSimpleType(ast.newName(clazz.getCanonicalName())));
+		
+		// Check for abstract constructors and if any - generate them.
+		if(abstractConstructors.length != 0)
+			type.bodyDeclarations().addAll(generateConstructors());
+		
+		// Generate stubs for the abstract methods.
+		type.bodyDeclarations().addAll(generateMethods());
+		
+		// Add class to the compilation unit.
+		unit.types().add(type);
+		
+		return unit;
+	}
+	
+	/**
+	 * Generates stubs for abstract methods. 
+	 * 
+	 * @return list of the abstract method stubs
+	 */
+	@SuppressWarnings("unchecked")
+	private List<MethodDeclaration> generateMethods(){
+		List<MethodDeclaration> generatedMethods = new LinkedList<MethodDeclaration>();
+		for(Method abstractMethod : abstractMethods){
+			
+			MethodDeclaration md = ast.newMethodDeclaration();
+			md.setName(ast.newSimpleName(abstractMethod.getName()));
+			
+			// Set "Override" annotation marker.
+			MarkerAnnotation ma = ast.newMarkerAnnotation();
+			ma.setTypeName(ast.newName("java.lang.Override"));
+			md.modifiers().add(ma);
+			
+			// Get original modifiers of the method, they are either 
+			// public or protected.
+			int modifiers = abstractMethod.getModifiers();
+			if(Modifier.isPublic(modifiers))
+				md.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+			else
+				md.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PROTECTED_KEYWORD));
+						
+			// Set return type of the method.
+			md.setReturnType2(generateType(abstractMethod.getReturnType()));
+			
+			// Generate method parameters if any.
+			List<?> parameters = generateMethodParameters(abstractMethod.getParameterTypes());
+			if(!parameters.isEmpty())
+				md.parameters().addAll(parameters);
+			
+			// Set default return value.
+			md.setBody(generateMethodBody(abstractMethod.getReturnType()));
+			
+			// Add exceptions thrown by method if any.
+			List<?> exceptions = (generateThrownExceptions(abstractMethod.getExceptionTypes()));
+			if(!exceptions.isEmpty())
+				md.thrownExceptions().addAll(exceptions);
+			
+			generatedMethods.add(md);
+		}
+		return generatedMethods;
+	}
+	
+	/**
+	 * Generates abstract constructor stubs.
+	 * 
+	 * @return list of the abstract constructor stubs.
+	 */
+	@SuppressWarnings("unchecked")
+	private List<MethodDeclaration> generateConstructors(){
+		List<MethodDeclaration> generatedConstructors = new LinkedList<MethodDeclaration>();
+		for(Constructor<?> abstractConstructor : abstractConstructors){
+			
+			MethodDeclaration cd = ast.newMethodDeclaration();
+			cd.setName(ast.newSimpleName(clazz.getSimpleName() + "Stub"));
+			cd.setConstructor(true);
+			
+			// Abstract constructors can only be public. 
+			cd.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+			
+			// Generate contructor parameters if any.
+			List<?> parameters = generateMethodParameters(abstractConstructor.getParameterTypes());
+			if(!parameters.isEmpty())
+				cd.parameters().addAll(parameters);
+			
+			// Set default return type.
+			cd.setBody(generateConstructorBody(abstractConstructor.getParameterTypes().length));
+			
+			// Add exceptions thrown by constructor if any.
+			List<?> exceptions = (generateThrownExceptions(abstractConstructor.getExceptionTypes()));
+			if(!exceptions.isEmpty())
+				cd.thrownExceptions().addAll(exceptions);
+		
+			generatedConstructors.add(cd);
+		}
+		return generatedConstructors;
+	}
+	
+	/**
+	 * Generates constructor body, i.e. creates only return statement 
+	 * with super constructor invocation.
+	 * 
+	 * @param numberOfParameters number of parameters in original 
+	 * constructor.
+	 * @return generated statements within a block.
+	 */
+	@SuppressWarnings("unchecked")
+	private Block generateConstructorBody(int numberOfParameters){
+		
+		// Constructor body should be in a block statement. 
+		Block block = ast.newBlock();
+		
+		SuperConstructorInvocation sci = ast.newSuperConstructorInvocation();
+		for(int i = 0; i < numberOfParameters; i++)
+			sci.arguments().add(ast.newName("var" + i));
+		
+		block.statements().add(sci);
+		return block;
+	}
+	
+	/**
+	 * Generates thrown exceptions.
+	 * 
+	 * @param thrownExceptions array of exception types in original 
+	 * method.
+	 * @return list of exceptions.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List generateThrownExceptions(Class<?>[] thrownExceptions){
+		List exceptions = new LinkedList();
+		for(Class<?> exception : thrownExceptions){
+			exceptions.add(ast.newName(exception.getCanonicalName()));
+		}
+		return exceptions;
+	}
+	
+	/**
+	 * Generates method parameters.
+	 * 
+	 * @param parameterTypes types of the parameters in original
+	 * method.
+	 * @return list of the parameters.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List generateMethodParameters(Class<?>[] parameterTypes){
+		List parameters = new LinkedList();
+		int counter = 0;
+		
+		for(Class<?> parameter : parameterTypes){
+			SingleVariableDeclaration svd = ast.newSingleVariableDeclaration();
+			svd.setType(generateType(parameter));
+			svd.setName(ast.newSimpleName("var" + counter++));
+			parameters.add(svd);
+		}
+		return parameters;
+	}
+	
+	/**
+	 * Transforms type from reflection representation to AST 
+	 * representation.
+	 * 
+	 * @param type reflection representation of the type.
+	 * @return AST representation of the type.
+	 */
+	private Type generateType(Class<?> type){
+		if(type.isPrimitive())
+			return generatePrimitiveType(type);
+		return generateSimpleType(type);		
+	}
+	
+	/**
+	 * Generates primitive type. (int, double, char etc).
+	 *  
+	 * @param type reflection representation of the type.
+	 * @return AST representation of the type/
+	 */
+	private Type generatePrimitiveType(Class<?> type){
+		return ast.newPrimitiveType(PrimitiveType.toCode(type.getCanonicalName()));
+	}
+	
+	/**
+	 * Generates simple type. (Class or String types  for instance).
+	 *  
+	 * @param type reflection representation of the type.
+	 * @return AST representation of the type/
+	 */
+	private Type generateSimpleType(Class<?> type){
+		return ast.newSimpleType(ast.newName(type.getCanonicalName()));
+	}
+	
+	/**
+	 * Generates stub method body, i.e. creates only return statement
+	 * with default value.
+	 *  
+	 * @param returnType return of the method.
+	 * @return block with return statement inside.
+	 */
+	@SuppressWarnings("unchecked")
+	private Block generateMethodBody(Class<?> returnType){
+		ReturnStatement rs = ast.newReturnStatement();
+		rs.setExpression(generateReturnExpression(returnType));
+		Block block = ast.newBlock();
+		block.statements().add(rs);
+		return block;
+	}
+	
+	/**
+	 * Generates return expression. 
+	 * 
+	 * @param returnType return type.
+	 * @return AST return expression.
+	 */
+	private Expression generateReturnExpression(Class<?> returnType){
+
+		// If return type is String, then set default string literal.
+		if(returnType.getName().equals("java.lang.String")){
+			StringLiteral sl = ast.newStringLiteral();
+			sl.setLiteralValue("null_value");
+			return sl;
+		}
+		
+		// if primitive, then generate primitive expression.
+		if(returnType.isPrimitive())
+			return generatePrimitiveExpression(generatePrimitiveType(returnType));
+		
+		// Generate only simplest constructors.
+		if(returnType.getTypeParameters().length == 0)
+			return generateConstructorCall(returnType);
+		
+		// Default "null" return statement.
+		return ast.newNullLiteral();
+	}
+	
+	/**
+	 * Generates expression for a primitive types. (int, boolean etc.)
+	 * 
+	 * @param type primitive type to generate.
+	 * @return expression with default value for the primitive type.
+	 */
+	private Expression generatePrimitiveExpression(Type type){	
+		PrimitiveType primitiveType = (PrimitiveType)type;
+		
+		// Set boolean default value - false.
+		if(primitiveType.getPrimitiveTypeCode().equals(PrimitiveType.BOOLEAN))
+			return ast.newBooleanLiteral(false);
+		
+		// Set char default value - '0'.
+		if(primitiveType.getPrimitiveTypeCode().equals(PrimitiveType.CHAR)){
+			CharacterLiteral cl = ast.newCharacterLiteral();
+			cl.setCharValue('0');
+			return cl;
+		}
+		
+		// Don't set return type for void.  
+		if(primitiveType.getPrimitiveTypeCode().equals(PrimitiveType.VOID))
+			return null;
+		
+		// Return default value for any numeric primitive type.
+		return ast.newNumberLiteral("0");
+	}
+	
+	/**
+	 * Generates simplest constructor calls.
+	 * 
+	 * @param clazz type for which constructor call should be created.
+	 * @return constructor call or null if not able to generate 
+	 * simple constructor call.
+	 */
+	private Expression generateConstructorCall(Class<?> clazz){
+
+		// Check if class can be instantiated.
+		if(canInstantiate(clazz)){
+			ClassInstanceCreation cic = ast.newClassInstanceCreation();
+			cic.setType(ast.newSimpleType(ast.newName(clazz.getCanonicalName())));
+			return cic;
+		}
+		
+		// Return null literal, if constructor can not be created.
+		return ast.newNullLiteral();
+	}
+	private boolean canInstantiate(Class<?> clazz){
+		
+		// If class is abstract, then return null, since it's not
+		// possible to instantiate abstract class. 
+		if(Modifier.isAbstract(clazz.getModifiers()))
+			return false;
+		
+		// Check if constructor without parameters exists. 
+		Constructor<?>[] constructors = clazz.getConstructors();
+		for(Constructor<?> c : constructors){
+			if(c.getParameterTypes().length == 0)
+				return true;
+		}
+		
+		return false;
+	}
+}
