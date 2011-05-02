1c0b16b4afc4f62e51c5e2aed731665793d0fd03
Sebastian Steenbuck <sebastian@steenbuck.org>
2011-05-02 16:26:38 +0200

    Added logger messages

--- src/main/java/de/unisb/cs/st/evosuite/testcase/TestFitnessFunction.java
+++ src/main/java/de/unisb/cs/st/evosuite/testcase/TestFitnessFunction.java
@@ -64,6 +64,7 @@ public abstract class TestFitnessFunction extends FitnessFunction implements Com
 		} catch (Exception e) {
 			System.out.println("TG: Exception caught: " + e);
 			e.printStackTrace();
+			logger.fatal("TG: Exception caught: ", e);
 			System.exit(1);
 		}
 
