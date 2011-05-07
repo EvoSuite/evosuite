0bc81119722c1c19ce445211553b6f1845e19fbd
mis <devnull@localhost>
2011-05-07 20:25:43 +0200

    everything seems to work again

--- src/main/java/de/unisb/cs/st/evosuite/coverage/dataflow/DefUseFactory.java
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/dataflow/DefUseFactory.java
@@ -2,48 +2,37 @@ package de.unisb.cs.st.evosuite.coverage.dataflow;
 
 import de.unisb.cs.st.evosuite.cfg.BytecodeInstruction;
 
+/**
+ * Can be used to create instances of Definition and Use
+ * 
+ *  When given an instruction this factory asks the DefUsePool
+ * whether it knows this instruction. If it does the pool
+ * reveals the initially assigned defUseIDs which are then
+ * put into the respective constructors.
+ * 
+ * @author Andre Mis
+ */
 public class DefUseFactory {
-
-	// convenience methods for now
-	
-	/**
-	 *  Returns a Definition instance
-	 * given a BytecodeInstruction for which isDefinition() is true
-	 *  Otherwise returns a Use instance
-	 * given a BytecodeInstruction for which isUse() is true
-	 * 
-	 * WARNING: when given the wrap for an IINC this method will return a Definition
-	 */
-	public static DefUse makeInstance(BytecodeInstruction wrap) {
-		if(!wrap.isDefUse())
-			throw new IllegalArgumentException("expect wrap of a defuse to create one");
-		if(wrap.isDefinition())
-			return makeDefinition(wrap);
-		if(wrap.isUse())
-			return makeUse(wrap);
-
-		throw new IllegalStateException("either isUse() or isDefinition() must return true on a defuse");
-	}
 	
 	/**
 	 * Returns a Use instance given a BytecodeInstruction for which isUse() is
 	 * true
 	 * 
 	 */
-	public static Use makeUse(BytecodeInstruction wrap) {
-		if (!wrap.isUse())
+	public static Use makeUse(BytecodeInstruction instruction) {
+		if (!instruction.isUse())
 			throw new IllegalArgumentException(
 					"expect wrap of a use to create one");
-		if (!DefUsePool.isKnown(wrap))
+		if (!DefUsePool.isKnown(instruction))
 			throw new IllegalArgumentException(
 					"expect DefUsePool to know the given BytecodeInstruction");
 
-		int defuseId = DefUsePool.getRegisteredDefUseId(wrap);
-		int defId = DefUsePool.getRegisteredDefId(wrap);
-		int useId = DefUsePool.getRegisteredUseId(wrap);
-		boolean isParameterUse = DefUsePool.isRegisteredParameterUse(wrap);
+		int defuseId = DefUsePool.getRegisteredDefUseId(instruction);
+		int defId = DefUsePool.getRegisteredDefId(instruction);
+		int useId = DefUsePool.getRegisteredUseId(instruction);
+		boolean isParameterUse = DefUsePool.isRegisteredParameterUse(instruction);
 
-		return new Use(wrap, defuseId, defId, useId, isParameterUse);
+		return new Use(instruction, defuseId, defId, useId, isParameterUse);
 	}
 
 	/**
@@ -51,20 +40,41 @@ public class DefUseFactory {
 	 * isDefinition() is true
 	 * 
 	 */
-	public static Definition makeDefinition(BytecodeInstruction wrap) {
-		if (!wrap.isDefinition())
+	public static Definition makeDefinition(BytecodeInstruction instruction) {
+		if (!instruction.isDefinition())
 			throw new IllegalArgumentException(
 					"expect wrap of a definition to create one");
-		if (!DefUsePool.isKnown(wrap))
+		if (!DefUsePool.isKnown(instruction))
 			throw new IllegalArgumentException(
 					"expect DefUsePool to know the given BytecodeInstruction");
 
-		int defuseId = DefUsePool.getRegisteredDefUseId(wrap);
-		int defId = DefUsePool.getRegisteredDefId(wrap);
-		int useId = DefUsePool.getRegisteredUseId(wrap);
-		boolean isParameterUse = DefUsePool.isRegisteredParameterUse(wrap);
+		int defuseId = DefUsePool.getRegisteredDefUseId(instruction);
+		int defId = DefUsePool.getRegisteredDefId(instruction);
+		int useId = DefUsePool.getRegisteredUseId(instruction);
+		boolean isParameterUse = DefUsePool.isRegisteredParameterUse(instruction);
 
-		return new Definition(wrap, defuseId, defId, useId, isParameterUse);
+		return new Definition(instruction, defuseId, defId, useId, isParameterUse);
 	}
-	
+
+	/**
+	 *  Convenience method to offer DefUse-Functionality for when 
+	 * it doesn't matter whether a Definition or Use is returned
+	 * 
+	 *  Returns a Definition instance
+	 * given a BytecodeInstruction for which isDefinition() is true
+	 *  Otherwise returns a Use instance
+	 * given a BytecodeInstruction for which isUse() is true
+	 * 
+	 * WARNING: when given the wrap for an IINC this method will return a Definition
+	 */
+	public static DefUse makeInstance(BytecodeInstruction instruction) {
+		if(!instruction.isDefUse())
+			throw new IllegalArgumentException("expect wrap of a defuse to create one");
+		if(instruction.isDefinition())
+			return makeDefinition(instruction);
+		if(instruction.isUse())
+			return makeUse(instruction);
+
+		throw new IllegalStateException("either isUse() or isDefinition() must return true on a defuse");
+	}
 }
