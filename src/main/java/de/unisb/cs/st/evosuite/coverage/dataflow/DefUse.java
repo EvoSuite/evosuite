3bd450768a4614e1a0c8e3a6daa5f46640d96260
mis <devnull@localhost>
2011-05-11 16:15:54 +0200

    bugfix in ControlFlowGraphs defues methods further polishing

--- src/main/java/de/unisb/cs/st/evosuite/coverage/dataflow/DefUse.java
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/dataflow/DefUse.java
@@ -18,16 +18,51 @@ public class DefUse extends BytecodeInstruction {
 	int useId;
 	boolean isParameterUse;
 	
+	String varName; 
+	
 	
 	protected DefUse(BytecodeInstruction wrap, int defuseId, int defId, int useId, boolean isParameterUse) {
 		super(wrap);
 		if(!isDefUse())
 			throw new IllegalArgumentException("only actual defuse instructions are accepted");
+		if (defuseId < 0)
+			throw new IllegalArgumentException("expect defUseId to be positive");
+		if(defId < 0 && useId < 0)
+			throw new IllegalArgumentException("expect either defId or useId to be set");
 		
 		this.defuseId = defuseId;
 		this.defId = defId;
 		this.useId = useId;
 		this.isParameterUse = isParameterUse;
+		this.varName = super.getDUVariableName();
+		if(this.varName == null)
+			throw new IllegalStateException("expect defUses to have non-null varaible names");
+	}
+	
+	/**
+	 *  Determines whether the given BytecodeInstruction constitutes
+	 * a Definition that can potentially become an active Definition for this DefUse
+	 * 
+	 * in the sense that if control flow passes through the instruction
+	 * of the given Definition that Definition becomes  active for this DefUse's variable
+	 * 
+	 * This is the case if the given Definition defines the same variable as this DefUse
+	 * So a Definition canBecomeActive for itself
+	 */
+	public boolean canBecomeActiveDefinition(BytecodeInstruction instruction) {
+		if(!instruction.isDefinition())
+			return false;
+		
+		Definition otherDef = DefUseFactory.makeDefinition(instruction);
+		return sharesVariableWith(otherDef);
+	}
+	
+	/**
+	 *  Determines whether the given DefUse reads or writes the same variable
+	 * as this DefUse 
+	 */
+	public boolean sharesVariableWith(DefUse du) {
+		return varName.equals(du.varName);
 	}
 	
 	public String getDUVariableType() {
@@ -40,6 +75,11 @@ public class DefUse extends BytecodeInstruction {
 		
 		logger.warn("unexpected state");
 		return "UNKNOWN";
+	}
+	
+	@Override
+	public String getDUVariableName() {
+		return varName;
 	}
 	
 	// getter
