fcb0edbc0085e0c3934bcb3169a2c267d26c76f4
mis <none@none>
2011-05-28 20:05:20 +0200

    made TestClass usable - as in made all branches passable (true and false) and removed possible dead locks and long loops

--- examples/amis/src/amis/ControlDependencyTestClass.java
+++ examples/amis/src/amis/ControlDependencyTestClass.java
@@ -11,136 +11,139 @@ public class ControlDependencyTestClass {
 	public ControlDependencyTestClass() {
 	}
 	
-//	
-//	public void simpleDoWhile() {
-//		do {
-//			anInt++;
-//		} while(anInt % 13!=0);
-//	}
-//	
-//	public int ifMethod(int anInt) {
-//		int i = anInt;
-//		if(i%2 == 0)
-//			i++;
-//		else
-//			i--;
-//		
-//		return i;
-//	}
-//	
-//	public int bigIfMethod(int anInt) {
-//		int i = anInt;
-//		if(i%17 == 0)
-//			if(i%2 == 0)
-//				i++;
-//			else
-//				i--;
-//		else
-//			if(i%2 == 0)
-//				return i-1;
-//			else
-//				i++;
-//		
-//		return i;
-//	}
-//	
-//	public int bigIfMethod2(int anInt) {
-//		int i = anInt;
-//		if(i%17 == 0)
-//			if(i%2 == 0)
-//				i++;
-//			else
-//				i--;
-//		else
-//			if(i%2 == 0)
-//				i--;
-//			else
-//				return i+1;
-//		
-//		return i;
-//	}
-//	
-//	public int ifMethodReturn(int anInt) {
-//		int i = anInt;
-//		if(i%2 == 0)
-//			return i;
-//		else
-//			return i-1;
-//	}
-//	
-//	public int bigIfMethodReturn(int anInt) {
-//		int i = anInt;
-//		if(i%17 == 0)
-//			if(i%2 == 0)
-//				return i;
-//			else
-//				return i-1;
-//		else
-//			if(i%2 == 0)
-//				return i+1;
-//			else
-//				return i;		
-//	}
-//	
-//	public int whileMethod(int anInt) {
-//		int i = 0;
-//		int c = 0;
-//		while(i<anInt) {
-//			i++;
-//			c++;
-//			if(c<15)
-//				continue;
-//			if(c==10)
-//				break;
-//		}
-//		return i;
-//	}
-//
-//	
-//	public int whileMethodReturn(int anInt) {
-//		int i = 0;
-//		while(i<anInt) {
-//			i++;
-//			return i;
-//		}
-//		return -1;
-//	}
-//
-//	public int whileMethodConditionalReturn(int anInt) {
-//		int i = 0;
-//		int c = 0;
-//		while(i<anInt) {
-//			i++;
-//			if(anInt%2 == 0)
-//				return i;
-//			c++;
-//			if(c>15)
-//				continue;
-//			if(c>10)
-//				break;
-//		}
-//		return -1;
-//	}	
-//	
-//	public int doWhileMethod(int anInt) {
-//		int i = anInt;
-//		do {
-//			i++;
-//		} while(i % 13 != 0);
-//	
-//		return i;
-//	}
-//	
-//	public int doWhileMethodReturn(int anInt) {
-//		int i = anInt;
-//		do {
-//			i++;
-//			if(i % 13 == 1)
-//				return i;
-//		} while(i % 13 != 0);
-//	
-//		return i;
-//	}
+	public void setAnInt(int anInt) {
+		this.anInt = anInt;
+	}
+	
+	public void simpleDoWhile() {
+		do {
+			anInt++;
+		} while(anInt >= 0 && (anInt % 13) !=0 );
+	}
+	
+	public int ifMethod(int anInt) {
+		int i = anInt;
+		if(i%2 == 0)
+			i++;
+		else
+			i--;
+		
+		return i;
+	}
+	
+	public int bigIfMethod(int anInt) {
+		int i = anInt;
+		if(i%17 == 0)
+			if(i%2 == 0)
+				i++;
+			else
+				i--;
+		else
+			if(i%2 == 0)
+				return i-1;
+			else
+				i++;
+		
+		return i;
+	}
+	
+	public int bigIfMethod2(int anInt) {
+		int i = anInt;
+		if(i%17 == 0)
+			if(i%2 == 0)
+				i++;
+			else
+				i--;
+		else
+			if(i%2 == 0)
+				i--;
+			else
+				return i+1;
+		
+		return i;
+	}
+	
+	public int ifMethodReturn(int anInt) {
+		int i = anInt;
+		if(i%2 == 0)
+			return i;
+		else
+			return i-1;
+	}
+	
+	public int bigIfMethodReturn(int anInt) {
+		int i = anInt;
+		if(i%17 == 0)
+			if(i%2 == 0)
+				return i;
+			else
+				return i-1;
+		else
+			if(i%2 == 0)
+				return i+1;
+			else
+				return i;		
+	}
+	
+	public int whileMethod(int anInt) {
+		int i = 0;
+		int c = 0;
+		while(i != (anInt % 10)) {
+			i++;
+			c++;
+			if(c>5)
+				break;
+			if(c<5)
+				continue;
+		}
+		return i;
+	}
+
+	
+	public int whileMethodReturn(int anInt) {
+		int i = 0;
+		while(i<anInt) {
+			i++;
+			return i;
+		}
+		return -1;
+	}
+
+	public int whileMethodConditionalReturn(int anInt) {
+		int i = 0;
+		int c = 0;
+		while(i % 10 != (anInt % 10)) {
+			i++;
+			if(anInt%2 == 0)
+				return i;
+			c++;
+			if(c>5)
+				break;
+			if(c<5)
+				continue;
+		}
+		return -1;
+	}	
+	
+	public int doWhileMethod(int anInt) {
+		int i = anInt;
+		do {
+			i++;
+		} while(i % 13 != 0);
+	
+		return i;
+	}
+	
+	public int doWhileMethodReturn(int anInt) {
+		int i = anInt;
+		do {
+			i++;
+			if(i % 13 == 1)
+				return i;
+		} while(i % 13 != 0);
+	
+		return i;
+	}
 	
 	public int forMethod(int anInt) {
 		int r = 0;
