adf1725f4d85b7a2a0d52f9c00ced7a8bc4a61bd
arcuri <none@none>
2011-06-07 17:07:38 +0200

    adding stufff to handle external processes

---
+++ src/main/java/de/unisb/cs/st/evosuite/process/Messages.java
@@ -0,0 +1,9 @@
+package de.unisb.cs.st.evosuite.process;
+
+public class Messages 
+{
+	public static final String NEW_SEARCH = "NEW_SEARCH";
+	public static final String CONTINUE_SEARCH = "CONTINUE_SEARCH";
+	public static final String FINISHED_COMPUTATION = "FINISHED_COMPUTATION";
+	public static final String NEED_RESTART = "NEED_RESTART";
+}
