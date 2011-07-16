13abd56f48531af5cc8c8c3ae57c716aed2878c5
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2011-07-16 07:08:41 +0200

    Extract Javalanche callgraph files

---
+++ src/main/java/de/unisb/cs/st/evosuite/callgraph/Tuple.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2011 Saarland University
+ * 
+ * This file is part of Javalanche.
+ * 
+ * Javalanche is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Javalanche is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License along with
+ * Javalanche. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package de.unisb.cs.st.evosuite.callgraph;
+
+import java.io.Serializable;
+
+public class Tuple implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	MethodDescription start;
+
+	MethodDescription end;
+
+	public Tuple(MethodDescription m1, MethodDescription m2) {
+		super();
+		this.start = m1;
+		this.end = m2;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((start == null) ? 0 : start.hashCode());
+		result = prime * result + ((end == null) ? 0 : end.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tuple other = (Tuple) obj;
+		if (start == null) {
+			if (other.start != null)
+				return false;
+		} else if (!start.equals(other.start))
+			return false;
+		if (end == null) {
+			if (other.end != null)
+				return false;
+		} else if (!end.equals(other.end))
+			return false;
+		return true;
+	}
+
+	public MethodDescription getStart() {
+		return start;
+	}
+
+	public MethodDescription getEnd() {
+		return end;
+	}
+
+	@Override
+	public String toString() {
+		return start + " - " + end;
+	}
+}
