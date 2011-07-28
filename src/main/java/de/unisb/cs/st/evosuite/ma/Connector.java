88c9a1e6ac09a960ad6cbfb7a6f284477054f4ba
pavlov <devnull@localhost>
2011-07-29 00:40:29 +0200

    Update

--- src/main/java/de/unisb/cs/st/evosuite/ma/Connector.java
+++ src/main/java/de/unisb/cs/st/evosuite/ma/Connector.java
@@ -5,8 +5,8 @@ import de.unisb.cs.st.evosuite.testsuite.TestSuiteChromosome;
 
 /**
  * The {@code Connector} Class is used to connect to the main algorithm of
- * EvoSuite and extract all needed information. This is possible by calling static
- * method externalCall.
+ * EvoSuite and extract all needed information. This is possible by calling
+ * static method externalCall.
  * 
  * @author Yury Pavlov
  */
@@ -17,6 +17,8 @@ public class Connector {
 	 */
 	private static final double MIN_DELTA_COVERAGE = 0.001;
 	private static final int MAX_ITERATION = 500;
+
+	private static final boolean MA_ACTIV = true;
 
 	private static int iterCount = 0;
 	private static double oldCoverageVal = Double.MAX_VALUE;
@@ -29,29 +31,30 @@ public class Connector {
 	 *            - SearchAlgorithm instance to work with it
 	 */
 	public static void externalCall(GeneticAlgorithm ga) {
-		double newCoverageVal = ((TestSuiteChromosome) ga.getBestIndividual()).getCoverage();
-		double deltaCoverage = oldCoverageVal - newCoverageVal;
-		
-		Debug.printDebConn("Iteration: " + iterCount + "\t\tCoverage: " + Double.toString(newCoverageVal));
+		if (MA_ACTIV) {
+			double newCoverageVal = ((TestSuiteChromosome) ga
+					.getBestIndividual()).getCoverage();
+			double deltaCoverage = oldCoverageVal - newCoverageVal;
 
-		/*
-		 * Call manual edition when coverage is smaller then 100% and delta is
-		 * too small and not change in few iterations
-		 */
-		if (newCoverageVal > 0)
-			if (deltaCoverage < MIN_DELTA_COVERAGE) {
-				iterCount++;
+			/*
+			 * Call manual edition when coverage is smaller then 100% and delta
+			 * is too small and not change in few iterations
+			 */
+			if (newCoverageVal > 0)
+				if (deltaCoverage < MIN_DELTA_COVERAGE) {
+					iterCount++;
 
-				if (iterCount > MAX_ITERATION) {
-					Editor editor = new Editor(ga);
+					if (iterCount > MAX_ITERATION) {
+						Editor editor = new Editor(ga);
+						iterCount = 0;
+					}
+
+				} else {
 					iterCount = 0;
 				}
 
-			} else {
-				iterCount = 0;
-			}
-
-		oldCoverageVal = newCoverageVal;
+			oldCoverageVal = newCoverageVal;
+		}
 	}
 
 }
