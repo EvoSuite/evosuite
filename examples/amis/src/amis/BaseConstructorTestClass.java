c995e8cd669fd95abf1168a91fdba5574f7e2cd9
mis <devnull@localhost>
2011-08-24 16:19:41 +0200

    added missing TestClass

---
+++ examples/amis/src/amis/BaseConstructorTestClass.java
@@ -0,0 +1,46 @@
+package amis;
+
+
+/**
+ * @author Andre Mis
+ *
+ */
+public class BaseConstructorTestClass {
+
+	private int targetField = 0;
+	
+	// constructors
+	
+	public BaseConstructorTestClass(int anInt) {
+		targetField = anInt;
+	}
+	
+	public BaseConstructorTestClass(int anInt, int anotherInt) {
+		this(anInt+anotherInt);
+		
+		targetField = 1;
+		new Object();
+		setTargetField(imPrivate());
+		setTargetField(targetField);
+		new BaseConstructorTestClass(3);
+	}
+//	
+//	public BaseConstructorTestClass(int anInt, int anotherInt, int andAnotherInt) {
+//		targetField = anInt+anotherInt+andAnotherInt;
+//	}
+//	
+//	public BaseConstructorTestClass(String aString) {
+//		targetField = aString.length();
+//	}
+//
+	public void setTargetField(int field) {
+		targetField = field;
+	}
+	public int getTargetField() {
+		return targetField;
+	}
+	private int imPrivate() {
+		return 7;
+	}
+	
+}
