f5872b87ba15f69136208ba937bbc19c5cbb8384
arcuri <none@none>
2011-10-06 00:43:21 +0200

    minor fix

--- src/test/java/de/unisb/cs/st/evosuite/TestSUTWithSimpleSingleMethod.java
+++ src/test/java/de/unisb/cs/st/evosuite/TestSUTWithSimpleSingleMethod.java
@@ -4,6 +4,7 @@ import org.junit.*;
 
 import com.examples.with.different.packagename.SingleMethod;
 
+import de.unisb.cs.st.evosuite.ga.Chromosome;
 import de.unisb.cs.st.evosuite.ga.GeneticAlgorithm;
 
 /**
@@ -15,23 +16,32 @@ public class TestSUTWithSimpleSingleMethod extends SystemTest{
 	@Test
 	public void testSingleMethod(){
 		EvoSuite evosuite = new EvoSuite();
+		int generations = 1;
+		
+		String targetClass = SingleMethod.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass; //needed, otherwise exception in TestCluster when reading results
 		
 		String[] command = new String[]{				
 				//EvoSuite.JAVA_CMD,
-				"-generateSuite",
+				"-generateTests",
 				"-class",
-				SingleMethod.class.getCanonicalName(),
+				targetClass,
 				"-Dhtml=false",
 				"-Dplot=false",
 				"-Djunit_tests=false",
 				"-Dshow_progress=false",
-				"-Dgenerations=1"
+				"-Dgenerations="+generations
 		};
 		
 		Object result = evosuite.parseCommandLine(command);
 		
 		Assert.assertTrue(result != null);
 		Assert.assertTrue("Invalid result type :"+result.getClass(), result instanceof GeneticAlgorithm);
+		
+		GeneticAlgorithm ga = (GeneticAlgorithm) result;
+		Assert.assertEquals("Wrong number of generations: ", generations, ga.getAge());
+		Chromosome best = ga.getBestIndividual();
+		Assert.assertEquals("Wrong number of statements: ",2 , best.size());
 		
 		//TODO, check best individual has only one method, and calling it should return "foo"
 	}
