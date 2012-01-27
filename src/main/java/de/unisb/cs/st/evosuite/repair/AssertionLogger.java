57a305d0c74acd4905e2af885cf318d66415b42f
hoeschele <devnull@localhost>
2012-01-27 19:24:18 +0100

    Quick Fix for bugs resulting in generation of non compiling test cases:
    1. Multiple declarations of variables with same name for primitive types and their wrapper classes
    
    2. Missing declaration of variables for last statements

--- src/main/java/de/unisb/cs/st/evosuite/repair/AssertionLogger.java
+++ src/main/java/de/unisb/cs/st/evosuite/repair/AssertionLogger.java
@@ -8,63 +8,69 @@ package de.unisb.cs.st.evosuite.repair;
  * 
  */
 public class AssertionLogger {
+	
+	public static boolean COVERAGE = false;
 
 	// Added preliminary implementation
 	
 	public static void assertEquals(String className, int testId, int assertionId,
 	        double expected, double actual, double delta) {
-		if (Math.abs(expected - actual) < delta) {
+		if (Math.abs(expected - actual) < delta && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertEquals(String className, int testId, int assertionId,
 	        long expected, long actual) {
-		if (expected == actual) {
+		if (expected == actual && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertEquals(String className, int testId, int assertionId,
 	        java.lang.Object expected, java.lang.Object actual) {
-		if (expected.equals(actual)) {
+		if (expected.equals(actual) && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertFalse(String className, int testId, int assertionId,
 	        boolean condition) {
-		if (!condition) {
+		if (!condition && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertNotNull(String className, int testId, int assertionId,
 	        java.lang.Object object) {
-		if (object != null) {
+		if (object != null && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertNull(String className, int testId, int assertionId,
 	        java.lang.Object object) {
-		if (object == null) {
+		if (object == null && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void assertTrue(String className, int testId, int assertionId,
 	        boolean condition) {
-		if (condition) {
+		if (condition && COVERAGE) {
 			System.out.println("Assertion Violated: " + className + ":" + testId + ":" + assertionId);
 		}
 	}
 
 	public static void fail() {
-		System.out.println("Fail");
+		if (COVERAGE) {
+			System.out.println("Fail");			
+		}
 	}
 
 	public static void fail(String message) {
-		System.out.println("Fail: " + message);
+		if (COVERAGE) {			
+			System.out.println("Fail: " + message);
+		}
 	}
 }
