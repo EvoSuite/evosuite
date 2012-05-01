f0701f76955d3965cff9df53429e92fe7f58ea41
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-05-01 10:57:30 +0100

    Fixes in AssignmentStatement creation and mutation

--- src/main/java/de/unisb/cs/st/evosuite/testcase/FieldReference.java
+++ src/main/java/de/unisb/cs/st/evosuite/testcase/FieldReference.java
@@ -295,7 +295,7 @@ public class FieldReference extends VariableReferenceImpl {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
+		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
