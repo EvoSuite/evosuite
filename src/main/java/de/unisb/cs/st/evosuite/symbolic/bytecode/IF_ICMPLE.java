2374ee5ef3b340c8236a9891d0a1e1c804b563c3
fraser <none@none>
2012-05-29 14:09:00 +0100

    Update license headers in all files

--- src/main/java/de/unisb/cs/st/evosuite/symbolic/bytecode/IF_ICMPLE.java
+++ src/main/java/de/unisb/cs/st/evosuite/symbolic/bytecode/IF_ICMPLE.java
@@ -1,25 +1,20 @@
-/*
- * Copyright (C) 2005,2006 United States Government as represented by the
- * Administrator of the National Aeronautics and Space Administration (NASA).
- * All Rights Reserved.
- * 
- * Copyright (C) 2011 Saarland University
- * 
- * This file is part of EvoSuite, but based on the SymbC extension of JPF
- * 
+/**
+ * Copyright (C) 2012 Gordon Fraser, Andrea Arcuri
+ *
+ * This file is part of EvoSuite.
+ *
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser Public License along with
+ *
+ * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package de.unisb.cs.st.evosuite.symbolic.bytecode;
 
 import gov.nasa.jpf.jvm.StackFrame;
--- src/main/java/de/unisb/cs/st/evosuite/utils/Randomness.java
+++ src/main/java/de/unisb/cs/st/evosuite/utils/Randomness.java
@@ -1,21 +1,20 @@
-/*
- * Copyright (C) 2010 Saarland University
- * 
- * This file is part of the GA library.
- * 
- * GA is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Lesser Public License as published by the Free Software
+/**
+ * Copyright (C) 2012 Gordon Fraser, Andrea Arcuri
+ *
+ * This file is part of EvoSuite.
+ *
+ * EvoSuite is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
- * GA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser Public License along with
- * GA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package de.unisb.cs.st.evosuite.utils;
 
 import java.io.Serializable;
