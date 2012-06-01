24d5e84ccaff8338e84be88f6a6dc3e66fa19541
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-06-01 23:03:18 +0100

    Minor interface adaptations for Bestest

--- src/main/java/de/unisb/cs/st/evosuite/coverage/mutation/MutationFactory.java
+++ src/main/java/de/unisb/cs/st/evosuite/coverage/mutation/MutationFactory.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2012 Gordon Fraser, Andrea Arcuri
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -53,24 +53,19 @@ public class MutationFactory extends AbstractFitnessFactory {
 	 */
 	@Override
 	public List<TestFitnessFunction> getCoverageGoals() {
+		return getCoverageGoals(null);
+	}
+
+	public List<TestFitnessFunction> getCoverageGoals(String targetMethod) {
 		if (goals != null)
 			return goals;
 
 		goals = new ArrayList<TestFitnessFunction>();
 
-		// String targetMethod = Properties.TARGET_METHOD;
-
-		// TODO: What about methods without mutants?
-		/*
-		// Branchless methods
-		String class_name = Properties.TARGET_CLASS;
-		for (String method : BranchPool.getBranchlessMethods()) {
-			if (targetMethod.equals("") || method.endsWith(targetMethod))
-				goals.add(new BranchCoverageTestFitness(new BranchCoverageGoal(
-				        class_name, method.substring(method.lastIndexOf(".") + 1))));
-		}
-		*/
 		for (Mutation m : MutationPool.getMutants()) {
+			if (targetMethod != null && !m.getMethodName().endsWith(targetMethod))
+				continue;
+
 			if (MutationTimeoutStoppingCondition.isDisabled(m))
 				continue;
 			if (strong)
