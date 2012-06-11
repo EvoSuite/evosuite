1e2dff618ae8af080b094a0a9367cdd248f7e6e6
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-06-11 21:01:47 +0100

    Change debug messages from warn to debug

--- src/main/java/de/unisb/cs/st/evosuite/contracts/ContractChecker.java
+++ src/main/java/de/unisb/cs/st/evosuite/contracts/ContractChecker.java
@@ -1,17 +1,18 @@
 /**
- * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite contributors
- *
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -108,7 +109,7 @@ public class ContractChecker extends ExecutionObserver {
 			 * TODO: at this point, for the fitness function we still consider the coverage given by the 
 			 * following statements. Maybe that should be changed? At the moment, we only stop if exceptions 
 			 */
-			logger.warn("Not checking contracts for invalid test");
+			logger.debug("Not checking contracts for invalid test");
 			return;
 		}
 
