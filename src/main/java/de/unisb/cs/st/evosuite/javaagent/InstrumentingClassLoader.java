4fbc089192e7e0e3c3d06e504db2cfe9f8ef2076
fraser <none@none>
2012-06-15 09:23:11 +0100

    Exclude yourkit from instrumentation

--- src/main/java/de/unisb/cs/st/evosuite/javaagent/InstrumentingClassLoader.java
+++ src/main/java/de/unisb/cs/st/evosuite/javaagent/InstrumentingClassLoader.java
@@ -1,17 +1,18 @@
 /**
- * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite contributors
- *
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -79,7 +80,7 @@ public class InstrumentingClassLoader extends ClassLoader {
 		//packages that should not be instrumented
 		return new String[] { "java.", "javax.", "sun.", "de.unisb.cs.st.evosuite",
 		        "de.unisb.cs.st.specmate", "org.xml", "org.w3c",
-		        "testing.generation.evosuite",
+		        "testing.generation.evosuite", "com.yourkit",
 		        // Need to have these in here to avoid trouble with UnsatisfiedLinkErrors on Mac OS X and Java/Swing apps
 		        "apple.", "com.apple."
 		};
