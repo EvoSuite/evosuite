e1cbacdeb15738160b037b9659b57d0d55c44cbf
fraser <none@none>
2012-06-15 09:22:28 +0100

    Add property to make BranchEval optional

--- src/main/java/de/unisb/cs/st/evosuite/Properties.java
+++ src/main/java/de/unisb/cs/st/evosuite/Properties.java
@@ -1,17 +1,18 @@
 /**
- * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite contributors
- *
+ * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -518,6 +519,9 @@ public class Properties {
 	// Experimental
 	@Parameter(key = "calculate_cluster", description = "Automatically calculate test cluster during setup")
 	public static boolean CALCULATE_CLUSTER = false;
+
+	@Parameter(key = "branch_eval", description = "Jeremy's branch evaluation")
+	public static boolean BRANCH_EVAL = false;
 
 	@Parameter(key = "branch_statement", description = "Require statement coverage for branch coverage")
 	public static boolean BRANCH_STATEMENT = false;
