74c0578fe06cf163b97bc1c73718dd53372b43b2
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-06-26 16:37:37 +0100

    Add Exsyst to src/exsyst

---
+++ src/exsyst/java/org/exsyst/ui/YWorksDrawable.java
@@ -0,0 +1,6 @@
+package org.exsyst.ui;
+
+public interface YWorksDrawable {
+	public void addToYWorksEnvironment(YWorksEnvironment env);
+	public void addEdgesToYWorksEnvironment(YWorksEnvironment env);
+}
