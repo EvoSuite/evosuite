52d12aed8cd2656a48c7487785a1a9e1ce17c28b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-07-02 17:17:08 +0100

    Add workaround for java reflection bug causing java.lang.reflect.GenericSignatureFormatError

--- src/main/java/org/evosuite/testcase/FieldReference.java
+++ src/main/java/org/evosuite/testcase/FieldReference.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,6 +50,26 @@ public class FieldReference extends VariableReferenceImpl {
 	 */
 	public FieldReference(TestCase testCase, Field field, VariableReference source) {
 		super(testCase, field.getGenericType());
+		assert (field != null);
+		assert (source != null || Modifier.isStatic(field.getModifiers())) : "No source object was supplied, therefore we assumed the field to be static. However asking the field if it was static, returned false";
+		this.field = field;
+		this.source = source;
+		//		logger.info("Creating new field assignment for field " + field + " of object "
+		//		        + source);
+
+	}
+
+	/**
+	 * We need this constructor to work around a bug in Java Generics which
+	 * causes a java.lang.reflect.GenericSignatureFormatError when accessing
+	 * getType
+	 * 
+	 * @param testCase
+	 * @param type
+	 */
+	public FieldReference(TestCase testCase, Field field, Type fieldType,
+	        VariableReference source) {
+		super(testCase, fieldType);
 		assert (field != null);
 		assert (source != null || Modifier.isStatic(field.getModifiers())) : "No source object was supplied, therefore we assumed the field to be static. However asking the field if it was static, returned false";
 		this.field = field;
