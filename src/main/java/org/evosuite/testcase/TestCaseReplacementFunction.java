8d739c84e94bd4857db2b7c7b3d5e5720c39dec6
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-08-23 14:35:44 +0100

    Fix chromosome replacement functions

--- src/main/java/org/evosuite/testcase/TestCaseReplacementFunction.java
+++ src/main/java/org/evosuite/testcase/TestCaseReplacementFunction.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,12 +20,12 @@ package org.evosuite.testcase;
 import org.evosuite.Properties;
 import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.ReplacementFunction;
-import org.evosuite.ga.SelectionFunction;
-
 
 /**
- * <p>TestCaseReplacementFunction class.</p>
- *
+ * <p>
+ * TestCaseReplacementFunction class.
+ * </p>
+ * 
  * @author Gordon Fraser
  */
 public class TestCaseReplacementFunction extends ReplacementFunction {
@@ -33,26 +33,35 @@ public class TestCaseReplacementFunction extends ReplacementFunction {
 	private static final long serialVersionUID = 2894768695219052674L;
 
 	/**
-	 * <p>Constructor for TestCaseReplacementFunction.</p>
-	 *
-	 * @param maximize a boolean.
+	 * <p>
+	 * Constructor for TestCaseReplacementFunction.
+	 * </p>
+	 * 
+	 * @param maximize
+	 *            a boolean.
 	 */
 	public TestCaseReplacementFunction(boolean maximize) {
 		super(maximize);
 	}
 
 	/**
-	 * <p>Constructor for TestCaseReplacementFunction.</p>
+	 * <p>
+	 * Constructor for TestCaseReplacementFunction.
+	 * </p>
 	 */
 	public TestCaseReplacementFunction() {
 		super(false);
 	}
 
 	/**
-	 * <p>getLengthSum</p>
-	 *
-	 * @param chromosome1 a {@link org.evosuite.testcase.ExecutableChromosome} object.
-	 * @param chromosome2 a {@link org.evosuite.testcase.ExecutableChromosome} object.
+	 * <p>
+	 * getLengthSum
+	 * </p>
+	 * 
+	 * @param chromosome1
+	 *            a {@link org.evosuite.testcase.ExecutableChromosome} object.
+	 * @param chromosome2
+	 *            a {@link org.evosuite.testcase.ExecutableChromosome} object.
 	 * @return a int.
 	 */
 	public int getLengthSum(ExecutableChromosome chromosome1,
@@ -60,28 +69,38 @@ public class TestCaseReplacementFunction extends ReplacementFunction {
 		return chromosome1.size() + chromosome2.size();
 	}
 
-
 	/** {@inheritDoc} */
 	@Override
 	public boolean keepOffspring(Chromosome parent1, Chromosome parent2,
 	        Chromosome offspring1, Chromosome offspring2) {
 
-		int cmp = compareBestOffspringToBestParent(parent1,parent2,offspring1,offspring2);
-		
+		int cmp = compareBestOffspringToBestParent(parent1, parent2, offspring1,
+		                                           offspring2);
+
 		if (Properties.CHECK_PARENTS_LENGTH) {
-			
-			int offspringLength = getLengthSum((ExecutableChromosome) offspring1, (ExecutableChromosome) offspring2);
-			int parentLength = getLengthSum((ExecutableChromosome) parent1,	 (ExecutableChromosome) parent2);
-			
+
+			int offspringLength = getLengthSum((ExecutableChromosome) offspring1,
+			                                   (ExecutableChromosome) offspring2);
+			int parentLength = getLengthSum((ExecutableChromosome) parent1,
+			                                (ExecutableChromosome) parent2);
+
 			//if equivalent, only accept if it does not increase the length
-			if (cmp==0 &&  offspringLength <= parentLength) {
+			if (cmp == 0 && offspringLength <= parentLength) {
 				return true;
 			} else {
-				return cmp > 0;
+				if (maximize) {
+					return cmp > 0;
+				} else {
+					return cmp < 0;
+				}
 			}
-		}  else {
+		} else {
 			//default check
-			return cmp >= 0;
+			if (maximize) {
+				return cmp >= 0;
+			} else {
+				return cmp <= 0;
+			}
 		}
 	}
 }
