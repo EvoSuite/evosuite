686268d46d5cb6f737f4fa9ce33548caafe7f717
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-09-03 07:35:56 +0100

    New attempt at controlling NaN / Infinity in local search

--- src/main/java/org/evosuite/testcase/FloatLocalSearch.java
+++ src/main/java/org/evosuite/testcase/FloatLocalSearch.java
@@ -53,9 +53,7 @@ public class FloatLocalSearch<T extends Number> extends LocalSearch {
 
 		NumericalPrimitiveStatement<T> p = (NumericalPrimitiveStatement<T>) test.test.getStatement(statement);
 		double value = p.getValue().doubleValue();
-		if (value == Double.NaN || value == Double.POSITIVE_INFINITY
-		        || value == Double.NEGATIVE_INFINITY || value == Float.NaN
-		        || value == Float.POSITIVE_INFINITY || value == Float.NEGATIVE_INFINITY) {
+		if (Double.isInfinite(value) || Double.isNaN(value)) {
 			return false;
 		}
 
@@ -79,9 +77,7 @@ public class FloatLocalSearch<T extends Number> extends LocalSearch {
 	        LocalSearchObjective objective, int precision,
 	        NumericalPrimitiveStatement<T> p) {
 		double value = p.getValue().doubleValue();
-		if (value == Double.NaN || value == Double.POSITIVE_INFINITY
-		        || value == Double.NEGATIVE_INFINITY || value == Float.NaN
-		        || value == Float.POSITIVE_INFINITY || value == Float.NEGATIVE_INFINITY) {
+		if (Double.isInfinite(value) || Double.isNaN(value)) {
 			return false;
 		}
 
