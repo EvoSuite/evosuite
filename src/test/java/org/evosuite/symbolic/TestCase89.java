b6fe20c98625665be0204427132a76e56be2f7e7
Juan Galeotti <galeotti@cs.uni-saarland.de>
2012-09-04 22:26:40 +0200

    bugfix: VerifyError in inner classes constructors

---
+++ src/test/java/org/evosuite/symbolic/TestCase89.java
@@ -0,0 +1,25 @@
+package org.evosuite.symbolic;
+
+public class TestCase89 {
+
+	public class InnerClass {
+
+		private int innerField;
+
+		public InnerClass(int val) {
+			innerField = TestCase89.this.outerField;
+			outerField = val;
+		}
+		
+	}
+
+	protected int outerField = 4;
+
+	public static void test(int int0) {
+		TestCase89 outerClassInstance = new TestCase89();
+		InnerClass innerClass = outerClassInstance.new InnerClass(
+				int0);
+		int int1 = outerClassInstance.outerField;
+		Assertions.checkEquals(int0, int1);
+	}
+}
