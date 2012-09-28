ab95bbfaa39e8e202d238ff038dce0cc9e8005fe
gordon <none@none>
2012-09-28 14:19:32 +0100

    Bugfix in testcase: TestSuiteReplacementFunction takes minimization option as parameter

--- src/test/java/org/evosuite/ga/TestReplacementFunction.java
+++ src/test/java/org/evosuite/ga/TestReplacementFunction.java
@@ -1,31 +1,28 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.evosuite.ga;
 
 import org.evosuite.Properties;
-import org.evosuite.ga.Chromosome;
-import org.evosuite.ga.ReplacementFunction;
-import org.evosuite.testcase.*;
 import org.evosuite.testsuite.TestSuiteChromosome;
 import org.evosuite.testsuite.TestSuiteReplacementFunction;
-import org.junit.*;
-import java.util.*;
-
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Andrea Arcuri
@@ -33,25 +30,24 @@ import java.util.*;
  */
 
 public class TestReplacementFunction {
-	
+
 	public static final boolean defaultCheckParentLength = Properties.CHECK_PARENTS_LENGTH;
-	
-	
+
 	@After
-	public void reset(){
+	public void reset() {
 		Properties.CHECK_PARENTS_LENGTH = defaultCheckParentLength;
 	}
-	
+
 	@Test
-	public void testParentReplacement(){
-		
-		FakeTestSuiteChromosome offspring1 = new FakeTestSuiteChromosome(2,10);
-		FakeTestSuiteChromosome offspring2 = new FakeTestSuiteChromosome(1,10);
-		FakeTestSuiteChromosome parent1 = new FakeTestSuiteChromosome(2,5);
-		FakeTestSuiteChromosome parent2 = new FakeTestSuiteChromosome(2,5);
-		
-		ReplacementFunction SUT = new TestSuiteReplacementFunction();
-		
+	public void testParentReplacement() {
+
+		FakeTestSuiteChromosome offspring1 = new FakeTestSuiteChromosome(2, 10);
+		FakeTestSuiteChromosome offspring2 = new FakeTestSuiteChromosome(1, 10);
+		FakeTestSuiteChromosome parent1 = new FakeTestSuiteChromosome(2, 5);
+		FakeTestSuiteChromosome parent2 = new FakeTestSuiteChromosome(2, 5);
+
+		ReplacementFunction SUT = new TestSuiteReplacementFunction(true);
+
 		Properties.CHECK_PARENTS_LENGTH = false;
 		boolean result = SUT.keepOffspring(parent1, parent2, offspring1, offspring2);
 		Assert.assertTrue(result);
@@ -60,28 +56,28 @@ public class TestReplacementFunction {
 		result = SUT.keepOffspring(parent1, parent2, offspring1, offspring2);
 		Assert.assertTrue(!result);
 	}
-	
-	
-	
-	private class FakeTestSuiteChromosome extends TestSuiteChromosome{
-	
-		private int code;
-		private int length;
-		
-		public FakeTestSuiteChromosome(int c,int l){
+
+	private class FakeTestSuiteChromosome extends TestSuiteChromosome {
+
+		private static final long serialVersionUID = 1L;
+
+		private final int code;
+		private final int length;
+
+		public FakeTestSuiteChromosome(int c, int l) {
 			code = c;
 			length = l;
 		}
-		
+
 		@Override
-		public int totalLengthOfTestCases(){
+		public int totalLengthOfTestCases() {
 			return length;
 		}
-		
+
 		@Override
-		public int compareTo(Chromosome other){
-			int k = ((FakeTestSuiteChromosome)other).code;
-			return code-k;
+		public int compareTo(Chromosome other) {
+			int k = ((FakeTestSuiteChromosome) other).code;
+			return code - k;
 		}
 	}
 }
