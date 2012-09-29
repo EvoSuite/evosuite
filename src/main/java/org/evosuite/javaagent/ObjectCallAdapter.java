a9113a0bde902b27d532daab395b2b8a8f0c5f83
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-09-29 10:44:13 +0100

    Fixes for FindBugs warnings

--- src/main/java/org/evosuite/javaagent/ObjectCallAdapter.java
+++ src/main/java/org/evosuite/javaagent/ObjectCallAdapter.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,22 +25,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * <p>ObjectCallAdapter class.</p>
- *
+ * <p>
+ * ObjectCallAdapter class.
+ * </p>
+ * 
  * @author Gordon Fraser
  */
 public class ObjectCallAdapter extends MethodVisitor {
 
 	/** Constant <code>logger</code> */
-	protected static Logger logger = LoggerFactory.getLogger(ObjectCallAdapter.class);
+	protected static final Logger logger = LoggerFactory.getLogger(ObjectCallAdapter.class);
 
 	Map<String, String> descriptors = null;
 
 	/**
-	 * <p>Constructor for ObjectCallAdapter.</p>
-	 *
-	 * @param mv a {@link org.objectweb.asm.MethodVisitor} object.
-	 * @param descriptors a {@link java.util.Map} object.
+	 * <p>
+	 * Constructor for ObjectCallAdapter.
+	 * </p>
+	 * 
+	 * @param mv
+	 *            a {@link org.objectweb.asm.MethodVisitor} object.
+	 * @param descriptors
+	 *            a {@link java.util.Map} object.
 	 */
 	public ObjectCallAdapter(MethodVisitor mv, Map<String, String> descriptors) {
 		super(Opcodes.ASM4, mv);
