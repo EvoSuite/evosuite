61d93069a598261be2f25390df77b489abd8f4da
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2012-09-30 07:59:47 +0100

    Add test case for non-dynamic seeding case

--- src/test/java/org/evosuite/TestTrivialForDynamicSeeding.java
+++ src/test/java/org/evosuite/TestTrivialForDynamicSeeding.java
@@ -33,6 +33,7 @@ public class TestTrivialForDynamicSeeding extends SystemTest {
 		String targetClass = TrivialForDynamicSeeding.class.getCanonicalName();
 
 		Properties.TARGET_CLASS = targetClass;
+		Properties.DYNAMIC_POOL = 1d / 3d;
 
 		String[] command = new String[] { "-generateSuite", "-class", targetClass };
 
@@ -47,5 +48,29 @@ public class TestTrivialForDynamicSeeding extends SystemTest {
 		System.out.println("EvolvedTestSuite:\n" + best);
 
 		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+	}
+
+	@Test
+	public void testConcatenatedStringEqualsWithoutSeeding() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = TrivialForDynamicSeeding.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.DYNAMIC_POOL = 0.0;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		Assert.assertTrue(result != null);
+		Assert.assertTrue("Invalid result type :" + result.getClass(),
+		                  result instanceof GeneticAlgorithm);
+
+		GeneticAlgorithm ga = (GeneticAlgorithm) result;
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		Assert.assertEquals("Unexpected coverage: ", 2d / 3d, best.getCoverage(), 0.001);
 	}
 }
