ac50f7f207f110ccae23348a86940e4ce126448f
gordon <none@none>
2012-10-01 08:55:13 +0100

    Start refactoring for context sensitive branch coverage

---
+++ src/main/java/org/evosuite/coverage/ibranch/IBranchTestFitness.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.evosuite.coverage.ibranch;
+
+import org.evosuite.coverage.branch.Branch;
+import org.evosuite.setup.CallContext;
+import org.evosuite.testcase.ExecutionResult;
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.TestFitnessFunction;
+
+/**
+ * @author Gordon Fraser
+ * 
+ */
+public class IBranchTestFitness extends TestFitnessFunction {
+
+	private static final long serialVersionUID = -1399396770125054561L;
+
+	public IBranchTestFitness(Branch branch, CallContext context) {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#getFitness(org.evosuite.testcase.TestChromosome, org.evosuite.testcase.ExecutionResult)
+	 */
+	@Override
+	public double getFitness(TestChromosome individual, ExecutionResult result) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.TestFitnessFunction#compareTo(org.evosuite.testcase.TestFitnessFunction)
+	 */
+	@Override
+	public int compareTo(TestFitnessFunction other) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
