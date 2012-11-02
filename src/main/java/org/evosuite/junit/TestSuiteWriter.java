a28f2180c5e261330006d3adf9f450286bbac59b
fraser <none@none>
2012-11-02 16:27:14 +0000

    Fix test indentation

--- src/main/java/org/evosuite/junit/TestSuiteWriter.java
+++ src/main/java/org/evosuite/junit/TestSuiteWriter.java
@@ -32,10 +32,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
@@ -84,13 +80,13 @@ public class TestSuiteWriter implements Opcodes {
 
 	private final TestCodeVisitor visitor = new TestCodeVisitor();
 
-	private static final String METHOD_SPACE             = "  ";
-	private static final String BLOCK_SPACE              = "     "; 
-	private static final String INNER_BLOCK_SPACE        = "         "; 
-	private static final String INNER_INNER_BLOCK_SPACE  = "             "; 
-	
+	private static final String METHOD_SPACE = "  ";
+	private static final String BLOCK_SPACE = "    ";
+	private static final String INNER_BLOCK_SPACE = "      ";
+	private static final String INNER_INNER_BLOCK_SPACE = "        ";
+
 	private final String EXECUTOR_SERVICE = "executor";
-	
+
 	class TestFilter implements IOFileFilter {
 		@Override
 		public boolean accept(File f, String s) {
@@ -256,7 +252,8 @@ public class TestSuiteWriter implements Opcodes {
 	 * @return a {@link java.lang.String} object.
 	 */
 	protected String makeDirectory(String directory) {
-		String dirname = directory + File.separator + Properties.CLASS_PREFIX.replace('.', File.separatorChar); // +"/GeneratedTests";
+		String dirname = directory + File.separator
+		        + Properties.CLASS_PREFIX.replace('.', File.separatorChar); // +"/GeneratedTests";
 		File dir = new File(dirname);
 		logger.debug("Target directory: " + dirname);
 		dir.mkdirs();
@@ -333,7 +330,7 @@ public class TestSuiteWriter implements Opcodes {
 				import_names.add(imp.getName());
 		}
 		List<String> imports_sorted = new ArrayList<String>(import_names);
-		
+
 		//Add import info for EvoSuite classes used in the generated test suite
 		imports_sorted.add(Sandbox.class.getCanonicalName());
 		imports_sorted.add(java.util.concurrent.ExecutorService.class.getCanonicalName());
@@ -344,8 +341,7 @@ public class TestSuiteWriter implements Opcodes {
 		imports_sorted.add(org.junit.BeforeClass.class.getCanonicalName());
 		imports_sorted.add(org.junit.After.class.getCanonicalName());
 		imports_sorted.add(org.junit.AfterClass.class.getCanonicalName());
-		
-		
+
 		Collections.sort(imports_sorted);
 		for (String imp : imports_sorted) {
 			builder.append("import ");
@@ -463,17 +459,17 @@ public class TestSuiteWriter implements Opcodes {
 	 */
 	public String getUnitTest(String name) {
 		List<ExecutionResult> results = new ArrayList<ExecutionResult>();
-		
+
 		/*
 		 * if there was any security exception, then we need to scaffold the
 		 * test cases with a sandbox
 		 */
 		boolean wasSecurityException = false;
-		
+
 		for (int i = 0; i < testCases.size(); i++) {
 			ExecutionResult result = runTest(testCases.get(i));
 			results.add(result);
-			if(!wasSecurityException){
+			if (!wasSecurityException) {
 				wasSecurityException = result.hasSecurityException();
 			}
 		}
@@ -482,7 +478,7 @@ public class TestSuiteWriter implements Opcodes {
 
 		builder.append(getHeader(name, results));
 		builder.append(getBeforeAndAfterMethods(wasSecurityException));
-		
+
 		for (int i = 0; i < testCases.size(); i++) {
 			builder.append(testToString(i, i, results.get(i)));
 		}
@@ -505,40 +501,41 @@ public class TestSuiteWriter implements Opcodes {
 		ExecutionResult result = runTest(testCases.get(testId));
 		results.add(result);
 		boolean wasSecurityException = result.hasSecurityException();
-		
+
 		StringBuilder builder = new StringBuilder();
 
 		builder.append(getHeader(name + "_" + testId, results));
 		builder.append(getBeforeAndAfterMethods(wasSecurityException));
 		builder.append(testToString(testId, testId, results.get(0)));
 		builder.append(getFooter());
-		
+
 		return builder.toString();
 	}
 
 	/**
-	 * Get the code of methods for @BeforeClass, @Before, @AfterClass and @After.
-	 * In those methods, the EvoSuite framework for running the generated test
-	 * cases is handled (e.g., use of customized SecurityManager)
+	 * Get the code of methods for @BeforeClass, @Before, @AfterClass and
+	 * 
+	 * @After. In those methods, the EvoSuite framework for running the
+	 *         generated test cases is handled (e.g., use of customized
+	 *         SecurityManager)
 	 * 
 	 * @return
 	 */
-	protected String getBeforeAndAfterMethods(boolean wasSecurityException){
-		
-		if(! wasSecurityException){
+	protected String getBeforeAndAfterMethods(boolean wasSecurityException) {
+
+		if (!wasSecurityException) {
 			/*
 			 * For the moment, the code generated in this method is only
 			 * used there is a need for a security manager
 			 */
 			return "";
 		}
-		
+
 		/*
 		 * TODO: for now we only have code for sandbox.
 		 * But, in the future, we should also handle the instrumenting classloader
 		 */
-		
-		
+
 		/*
 		 * Note: the result of this method is always a constant.
 		 * But we don't code it as a constant just for ease of maintenance.
@@ -546,81 +543,76 @@ public class TestSuiteWriter implements Opcodes {
 		 * not much of the point to try to optimize it 
 		 */
 		StringBuilder bd = new StringBuilder("");
-						
-		bd.append("\n");
-		
-	
-		//-------- Fields       ---------------------------------------------		
-		
-		bd.append(METHOD_SPACE);
-		bd.append("private static ExecutorService "+EXECUTOR_SERVICE+"; \n");
-		
+
 		bd.append("\n");
 
-		
+		//-------- Fields       ---------------------------------------------		
+
+		bd.append(METHOD_SPACE);
+		bd.append("private static ExecutorService " + EXECUTOR_SERVICE + "; \n");
+
+		bd.append("\n");
+
 		//-------- BeforeClass  ---------------------------------------------
-		
+
 		bd.append(METHOD_SPACE);
 		bd.append("@BeforeClass \n");
-		
+
 		bd.append(METHOD_SPACE);
-		bd.append("public static void initEvoSuiteFramework(){ \n");		
+		bd.append("public static void initEvoSuiteFramework(){ \n");
 		bd.append(BLOCK_SPACE);
 		bd.append("Sandbox.initializeSecurityManagerForSUT(); \n");
 		bd.append(BLOCK_SPACE);
-		bd.append(EXECUTOR_SERVICE+" = Executors.newCachedThreadPool(); \n");		
+		bd.append(EXECUTOR_SERVICE + " = Executors.newCachedThreadPool(); \n");
 		bd.append(METHOD_SPACE);
 		bd.append("} \n");
-		
+
 		bd.append("\n");
-		
+
 		//-------- AfterClass  ---------------------------------------------
 
 		bd.append(METHOD_SPACE);
-		bd.append("@AfterClass \n");		
+		bd.append("@AfterClass \n");
 		bd.append(METHOD_SPACE);
 		bd.append("public static void clearEvoSuiteFramework(){ \n");
 		bd.append(BLOCK_SPACE);
-		bd.append(EXECUTOR_SERVICE+".shutdownNow(); \n");		
+		bd.append(EXECUTOR_SERVICE + ".shutdownNow(); \n");
 		bd.append(BLOCK_SPACE);
 		bd.append("Sandbox.resetDefaultSecurityManager(); \n");
 		bd.append(METHOD_SPACE);
 		bd.append("} \n");
-				
+
 		bd.append("\n");
 
 		//-------- Before  --------------------------------------------------
 
 		bd.append(METHOD_SPACE);
-		bd.append("@Before \n");		
+		bd.append("@Before \n");
 		bd.append(METHOD_SPACE);
 		bd.append("public void initTestCase(){ \n");
 		bd.append(BLOCK_SPACE);
 		bd.append("Sandbox.goingToExecuteSUTCode(); \n");
 		bd.append(METHOD_SPACE);
 		bd.append("} \n");
-		
+
 		bd.append("\n");
-		
+
 		//-------- After   --------------------------------------------------
-		
+
 		bd.append(METHOD_SPACE);
-		bd.append("@After \n");		
+		bd.append("@After \n");
 		bd.append(METHOD_SPACE);
 		bd.append("public void doneWithTestCase(){ \n");
 		bd.append(BLOCK_SPACE);
 		bd.append("Sandbox.goingToEndExecutingSUTCode(); \n");
 		bd.append(METHOD_SPACE);
 		bd.append("} \n");
-		
+
 		bd.append("\n");
-		
-		
-		return bd.toString(); 
+
+		return bd.toString();
 	}
-	
-	
-	
+
 	/**
 	 * Convert one test case to a Java method
 	 * 
@@ -633,7 +625,7 @@ public class TestSuiteWriter implements Opcodes {
 	protected String testToString(int number, int id, ExecutionResult result) {
 
 		boolean wasSecurityException = result.hasSecurityException();
-		
+
 		StringBuilder builder = new StringBuilder();
 		builder.append("\n");
 		builder.append(METHOD_SPACE);
@@ -641,7 +633,7 @@ public class TestSuiteWriter implements Opcodes {
 		builder.append(getInformation(id));
 		builder.append("\n");
 		builder.append(adapter.getMethodDefinition("test" + number));
-		
+
 		/*
 		 * A test case might throw a lot of different kinds of exceptions. 
 		 * These might come from SUT, and might also come from the framework itself (eg, see ExecutorService.submit).
@@ -652,39 +644,41 @@ public class TestSuiteWriter implements Opcodes {
 		 */
 		builder.append(" throws Exception ");
 		builder.append(" {\n");
-		
+
 		// ---------   start with the body -------------------------
-		
-		if(wasSecurityException){
+		String CODE_SPACE = INNER_BLOCK_SPACE;
+
+		if (wasSecurityException) {
 			builder.append(BLOCK_SPACE);
-			builder.append("Future<?> future = "+EXECUTOR_SERVICE+".submit(new Runnable(){ \n");
+			builder.append("Future<?> future = " + EXECUTOR_SERVICE
+			        + ".submit(new Runnable(){ \n");
 			builder.append(INNER_BLOCK_SPACE);
 			builder.append("@Override \n");
 			builder.append(INNER_BLOCK_SPACE);
 			builder.append("public void run() { \n");
+			CODE_SPACE = INNER_INNER_BLOCK_SPACE;
 		}
-		
+
 		for (String line : adapter.getTestString(id, testCases.get(id),
 		                                         result.exposeExceptionMapping(), visitor).split("\\r?\\n")) {
-			builder.append(INNER_INNER_BLOCK_SPACE);
+			builder.append(CODE_SPACE);
 			builder.append(line);
 			builder.append("\n");
 		}
 
-		if(wasSecurityException){
+		if (wasSecurityException) {
 			builder.append(INNER_BLOCK_SPACE);
-			builder.append("} \n");  //closing run(){
+			builder.append("} \n"); //closing run(){
 			builder.append(BLOCK_SPACE);
 			builder.append("}); \n"); //closing submit
 
 			long time = Properties.TIMEOUT + 1000; // we add one second just to be sure, that to avoid issues with test cases taking exactly TIMEOUT ms
 			builder.append(BLOCK_SPACE);
-			builder.append("future.get("+time+", TimeUnit.MILLISECONDS); \n"); 
+			builder.append("future.get(" + time + ", TimeUnit.MILLISECONDS); \n");
 		}
-		
+
 		// ---------   end of the body ----------------------------
-		
-		
+
 		builder.append(METHOD_SPACE);
 		builder.append("}\n");
 
