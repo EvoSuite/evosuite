37d762ca78ec64d76f36d1a69929ddcb2a063c0e
mattia <devnull@localhost>
2012-11-14 18:28:17 +0100

    Changed method getDistancesForCOntrolDependentBranchesOf in ControlFlowDistanceCalculator. The new method has the same sematic but avoid an errore that still I cannot explain. In the method there are comments about the error and a commented assertion.

--- src/main/java/org/evosuite/coverage/branch/ControlFlowDistanceCalculator.java
+++ src/main/java/org/evosuite/coverage/branch/ControlFlowDistanceCalculator.java
@@ -289,16 +289,18 @@ public class ControlFlowDistanceCalculator {
 		Set<ControlFlowDistance> r = new HashSet<ControlFlowDistance>();
 		Set<ControlDependency> nextToLookAt = instruction.getControlDependencies();
 
-		if (nextToLookAt.isEmpty()) {
-			// instruction only dependent on root branch
-			// since this method is called by getNonRootDistance(MethodCall)
-			// which in turn is only called when a MethodCall for this branch's
-			// method was found in the given result, i can safely assume that
-			// the 0-distance is a control dependence distance for the given
-			// instruction ... right?
-			r.add(new ControlFlowDistance());
-		}
-
+		//XXX TODO BUG?!
+//UNCOMMENT THIS		
+//		if (nextToLookAt.isEmpty()) {
+//			// instruction only dependent on root branch
+//			// since this method is called by getNonRootDistance(MethodCall)
+//			// which in turn is only called when a MethodCall for this branch's
+//			// method was found in the given result, i can safely assume that
+//			// the 0-distance is a control dependence distance for the given
+//			// instruction ... right?
+//			r.add(new ControlFlowDistance());
+//		}
+		
 		for (ControlDependency next : nextToLookAt) {
 			if (instruction.equals(next.getBranch().getInstruction()))
 				continue; // avoid loops
@@ -312,6 +314,16 @@ public class ControlFlowDistanceCalculator {
 			r.add(nextDistance);
 		}
 
+//UNCOMMENT THIS
+//		if (!nextToLookAt.isEmpty() && r.isEmpty()){
+//			throw new IllegalStateException("why?");
+//		}
+//		
+		//COMMENT THIS
+		if(r.isEmpty()){
+			r.add(new ControlFlowDistance());
+		}
+		
 		return r;
 	}
 
