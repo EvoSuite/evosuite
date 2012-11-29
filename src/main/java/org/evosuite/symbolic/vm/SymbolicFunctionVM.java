7002c9f37fcaedf4712b73fae7579e9a252f88c1
Juan Galeotti <galeotti@cs.uni-saarland.de>
2012-11-29 15:57:05 +0100

    length,matches,toLower,toUpper,toString,trim are refactored

--- src/main/java/org/evosuite/symbolic/vm/SymbolicFunctionVM.java
+++ src/main/java/org/evosuite/symbolic/vm/SymbolicFunctionVM.java
@@ -57,6 +57,12 @@ import org.evosuite.symbolic.vm.string.Equals;
 import org.evosuite.symbolic.vm.string.EqualsIgnoreCase;
 import org.evosuite.symbolic.vm.string.IndexOf;
 import org.evosuite.symbolic.vm.string.LastIndexOf;
+import org.evosuite.symbolic.vm.string.Length;
+import org.evosuite.symbolic.vm.string.Matches;
+import org.evosuite.symbolic.vm.string.ToLowerCase;
+import org.evosuite.symbolic.vm.string.ToString;
+import org.evosuite.symbolic.vm.string.ToUpperCase;
+import org.evosuite.symbolic.vm.string.Trim;
 import org.evosuite.symbolic.vm.string.buffer.StringBuffer_Init.StringBufferInit_S;
 import org.evosuite.symbolic.vm.string.buffer.StringBuffer_ToString;
 import org.evosuite.symbolic.vm.string.buffer.StringBuffer_Append.*;
@@ -267,8 +273,8 @@ public final class SymbolicFunctionVM extends AbstractVM {
 		addFunctionToTable(new LastIndexOf.LastIndexOf_S(env));
 		addFunctionToTable(new LastIndexOf.LastIndexOf_CI(env));
 		addFunctionToTable(new LastIndexOf.LastIndexOf_SI(env));
-		// addFunctionToTable(new Length(env));
-		// addFunctionToTable(new Matches(env));
+		addFunctionToTable(new Length(env));
+		addFunctionToTable(new Matches(env));
 		// addFunctionToTable(new RegionMatches(env));
 		// addFunctionToTable(new Replace.Replace_C(env));
 		// addFunctionToTable(new Replace.Replace_CS(env));
@@ -276,10 +282,10 @@ public final class SymbolicFunctionVM extends AbstractVM {
 		// addFunctionToTable(new ReplaceFirst(env));
 		// addFunctionToTable(new StartsWith(env));
 		// addFunctionToTable(new Substring(env));
-		// addFunctionToTable(new ToLowerCase(env));
-		// addFunctionToTable(new ToString(env));
-		// addFunctionToTable(new ToUpperCase(env));
-		// addFunctionToTable(new Trim(env));
+		addFunctionToTable(new ToLowerCase(env));
+		addFunctionToTable(new ToString(env));
+		addFunctionToTable(new ToUpperCase(env));
+		addFunctionToTable(new Trim(env));
 		// addFunctionToTable(new ValueOf.ValueOf_O(env));
 		//
 
--- src/main/java/org/evosuite/symbolic/vm/string/ToLowerCase.java
+++ src/main/java/org/evosuite/symbolic/vm/string/ToLowerCase.java
@@ -2,36 +2,40 @@ package org.evosuite.symbolic.vm.string;
 
 import org.evosuite.symbolic.expr.Operator;
 import org.evosuite.symbolic.expr.str.StringUnaryExpression;
+import org.evosuite.symbolic.expr.str.StringValue;
 import org.evosuite.symbolic.vm.NonNullReference;
 import org.evosuite.symbolic.vm.SymbolicEnvironment;
+import org.evosuite.symbolic.vm.SymbolicFunction;
 import org.evosuite.symbolic.vm.SymbolicHeap;
 
-public final class ToLowerCase extends StringFunction {
+public final class ToLowerCase extends SymbolicFunction {
 
 	private static final String TO_LOWER_CASE = "toLowerCase";
 
 	public ToLowerCase(SymbolicEnvironment env) {
-		super(env, TO_LOWER_CASE, Types.TO_STR_DESCRIPTOR);
+		super(env, Types.JAVA_LANG_STRING, TO_LOWER_CASE, Types.TO_STR_DESCRIPTOR);
 	}
 
 	@Override
-	protected void INVOKEVIRTUAL_String(String receiver) {
-		this.stringReceiverExpr = getStringExpression(env.topFrame().operandStack
-				.peekOperand(), receiver);
+	public Object executeFunction() {
+
+		// object receiver
+		NonNullReference symb_str = this.getSymbReceiver();
+		String conc_str = (String) this.getConcReceiver();
+
+		// return value
+		String conc_ret_val = (String) this.getConcRetVal();
+		NonNullReference symb_ret_val = (NonNullReference) this.getSymbRetVal();
+
+		StringValue string_expr = env.heap.getField(Types.JAVA_LANG_STRING,
+				SymbolicHeap.$STRING_VALUE, conc_str, symb_str, conc_str);
+		StringUnaryExpression symb_value = new StringUnaryExpression(
+				string_expr, Operator.TOLOWERCASE, conc_ret_val);
+
+		env.heap.putField(Types.JAVA_LANG_STRING, SymbolicHeap.$STRING_VALUE,
+				conc_ret_val, symb_ret_val, symb_value);
+
+		return this.getSymbRetVal();
 	}
 
-	@Override
-	public void CALL_RESULT(Object res) {
-		if (res != null) {
-			StringUnaryExpression symb_value = new StringUnaryExpression(
-					stringReceiverExpr, Operator.TOLOWERCASE, (String) res);
-
-			NonNullReference symb_receiver = (NonNullReference) env.topFrame().operandStack
-					.peekRef();
-			String conc_receiver = (String) res;
-			env.heap.putField(Types.JAVA_LANG_STRING,
-					SymbolicHeap.$STRING_VALUE, conc_receiver, symb_receiver,
-					symb_value);
-		}
-	}
 }
