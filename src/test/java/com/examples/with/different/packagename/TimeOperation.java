983f2d646460e50ff56bed50a01daa7fc6a7fb06
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-03-08 19:57:19 +0000

    Add tests for System.currentTimeMillis and Random.nextInt

---
+++ src/test/java/com/examples/with/different/packagename/TimeOperation.java
@@ -0,0 +1,12 @@
+package com.examples.with.different.packagename;
+
+public class TimeOperation {
+
+	public void testMe() {
+		long time = System.currentTimeMillis();
+		if(time == 100) {
+			System.out.println("Test");
+		}
+	}
+	
+}
