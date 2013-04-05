5432b11acf533f9eddc3e9c17063d4f3a7ecee05
gordon <none@none>
2013-04-05 17:44:18 +0100

    Add yet more generics tests

---
+++ src/test/java/com/examples/with/different/packagename/GenericSuperclassOmittingTypeParameters.java
@@ -0,0 +1,17 @@
+package com.examples.with.different.packagename;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("rawtypes")
+public class GenericSuperclassOmittingTypeParameters extends ArrayList {
+
+	private static final long serialVersionUID = 1L;
+
+	public boolean testMe(Object o) {
+		if(super.contains(o))
+			return true;
+		else
+			return false;
+	}
+	
+}
