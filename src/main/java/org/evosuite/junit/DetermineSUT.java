1ec58c37db405e5d0c839ee3f5422808c235c2c1
gordon <none@none>
2013-04-09 09:16:09 +0100

    Include target classpath when determining SUT for JUnit test class

--- src/main/java/org/evosuite/junit/DetermineSUT.java
+++ src/main/java/org/evosuite/junit/DetermineSUT.java
@@ -3,18 +3,22 @@
  */
 package org.evosuite.junit;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.evosuite.instrumentation.BytecodeInstrumentation;
+import org.evosuite.utils.ResourceList;
 import org.evosuite.utils.Utils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
@@ -36,9 +40,9 @@ import org.slf4j.LoggerFactory;
 public class DetermineSUT {
 
 	private final static Logger logger = LoggerFactory.getLogger(DetermineSUT.class);
-	
+
 	private String targetName = "";
-	
+
 	private Set<String> superClasses = new HashSet<String>();
 
 	private class TargetClassSorter implements Comparator<String> {
@@ -56,20 +60,22 @@ public class DetermineSUT {
 		}
 	}
 
-	public String getSUTName(String fullyQualifiedTargetClass) {
+	public String getSUTName(String fullyQualifiedTargetClass, String targetClassPath) {
 		this.targetName = fullyQualifiedTargetClass;
+		Set<String> targetClasses = analyzeTargetClasspath(targetClassPath);
 		Set<String> candidateClasses = new HashSet<String>();
 		try {
-			candidateClasses.addAll(determineCalledClasses(fullyQualifiedTargetClass));
+			candidateClasses.addAll(determineCalledClasses(fullyQualifiedTargetClass,
+			                                               targetClasses));
 		} catch (ClassNotFoundException e) {
 			// Ignore, the set will be empty?
-			logger.error("Class not found: " + e,e);
+			logger.error("Class not found: " + e, e);
 			return "";
 		}
 
-		if(candidateClasses.isEmpty())
+		if (candidateClasses.isEmpty())
 			return "<UNKNOWN>";
-		
+
 		List<String> sortedNames = new ArrayList<String>(candidateClasses);
 		Collections.sort(sortedNames, new TargetClassSorter(fullyQualifiedTargetClass));
 
@@ -77,8 +83,8 @@ public class DetermineSUT {
 		return sortedNames.get(0);
 	}
 
-	public Set<String> determineCalledClasses(String fullyQualifiedTargetClass)
-	        throws ClassNotFoundException {
+	public Set<String> determineCalledClasses(String fullyQualifiedTargetClass,
+	        Set<String> targetClasses) throws ClassNotFoundException {
 		Set<String> calledClasses = new HashSet<String>();
 
 		String className = fullyQualifiedTargetClass.replace('.', '/');
@@ -94,8 +100,8 @@ public class DetermineSUT {
 			reader.accept(classNode, ClassReader.SKIP_FRAMES);
 			superClasses = getSuperClasses(classNode);
 
-			if(isJUnitTest(classNode)) {
-				handleClassNode(calledClasses, classNode);
+			if (isJUnitTest(classNode)) {
+				handleClassNode(calledClasses, classNode, targetClasses);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -107,40 +113,52 @@ public class DetermineSUT {
 		return calledClasses;
 	}
 
+	private Set<String> analyzeTargetClasspath(String classPath) {
+		Pattern pattern = Pattern.compile(".*\\.class");
+		Collection<String> classes = ResourceList.getResources(classPath, pattern);
+		Set<String> classNames = new HashSet<String>();
+		for (String fileName : classes) {
+			classNames.add(fileName.replace(".class", "").replaceAll(File.separator, "."));
+		}
+		return classNames;
+	}
+
 	@SuppressWarnings("unchecked")
-	private void handleClassNode(Set<String> calledClasses, ClassNode cn) throws IOException {
+	private void handleClassNode(Set<String> calledClasses, ClassNode cn,
+	        Set<String> targetClasses) throws IOException {
 		List<MethodNode> methods = cn.methods;
 		for (MethodNode mn : methods) {
-			handleMethodNode(calledClasses, cn, mn);
+			handleMethodNode(calledClasses, cn, mn, targetClasses);
 		}
 	}
 
 	private boolean isValidClass(String name) throws IOException {
 		if (BytecodeInstrumentation.isJavaClass(name))
 			return false;
-		
-		if(name.startsWith("junit"))
+
+		if (name.startsWith("junit"))
 			return false;
 
-		if(name.startsWith("org.junit"))
+		if (name.startsWith("org.junit"))
 			return false;
 
 		if (name.startsWith(targetName))
 			return false;
-		
-		if(superClasses.contains(name))
+
+		if (superClasses.contains(name))
 			return false;
-		
+
 		ClassNode sutNode = loadClassNode(name);
-		if(isJUnitTest(sutNode)) {
+		if (isJUnitTest(sutNode)) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
 	@SuppressWarnings("unchecked")
-	private void handleMethodNode(Set<String> calledClasses, ClassNode cn, MethodNode mn) throws IOException {
+	private void handleMethodNode(Set<String> calledClasses, ClassNode cn, MethodNode mn,
+	        Set<String> targetClasses) throws IOException {
 		InsnList instructions = mn.instructions;
 		Iterator<AbstractInsnNode> iterator = instructions.iterator();
 
@@ -148,57 +166,60 @@ public class DetermineSUT {
 			AbstractInsnNode insn = iterator.next();
 			if (insn instanceof MethodInsnNode) {
 				String name = Utils.getClassNameFromResourcePath(((MethodInsnNode) insn).owner);
+				if (!targetClasses.contains(name))
+					continue;
+
 				if (isValidClass(name))
 					calledClasses.add(name);
 			}
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private boolean isJUnitTest(ClassNode cn) throws IOException {
 		// We do not consider abstract classes
-		if((cn.access & Opcodes.ACC_ABSTRACT) == Opcodes.ACC_ABSTRACT)
+		if ((cn.access & Opcodes.ACC_ABSTRACT) == Opcodes.ACC_ABSTRACT)
 			return false;
-		
-		if(hasJUnitSuperclass(cn))
+
+		if (hasJUnitSuperclass(cn))
 			return true;
-		
+
 		List<MethodNode> methods = cn.methods;
 		for (MethodNode mn : methods) {
 			List<AnnotationNode> annotations = mn.visibleAnnotations;
-			if(annotations != null) {
-				for(AnnotationNode an : annotations) {
-					if(an.desc.equals("Lorg/junit/Test;"))
+			if (annotations != null) {
+				for (AnnotationNode an : annotations) {
+					if (an.desc.equals("Lorg/junit/Test;"))
 						return true;
 				}
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private Set<String> getSuperClasses(ClassNode cn) throws IOException {
 		Set<String> superClasses = new HashSet<String>();
 		String currentSuper = cn.superName;
-		while(!currentSuper.equals("java/lang/Object")) {
+		while (!currentSuper.equals("java/lang/Object")) {
 			superClasses.add(Utils.getClassNameFromResourcePath(currentSuper));
 			ClassNode superNode = loadClassNode(currentSuper);
 			currentSuper = superNode.superName;
 		}
 		return superClasses;
 	}
-	
+
 	private boolean hasJUnitSuperclass(ClassNode cn) throws IOException {
-		if(cn.superName.equals("java/lang/Object"))
+		if (cn.superName.equals("java/lang/Object"))
 			return false;
 
-		if(cn.superName.equals("junit/framework/TestCase"))
+		if (cn.superName.equals("junit/framework/TestCase"))
 			return true;
 
 		ClassNode superClass = loadClassNode(cn.superName);
 		return hasJUnitSuperclass(superClass);
 	}
-	
+
 	private ClassNode loadClassNode(String className) throws IOException {
 		ClassReader reader = new ClassReader(className);
 
@@ -211,11 +232,12 @@ public class DetermineSUT {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DetermineSUT det = new DetermineSUT();
-		for (String className : args) {
-			System.out.println(det.getSUTName(className));
+		if (args.length != 2) {
+			System.err.println("Expected parameters: <TestCase> <Target Classpath>");
+			return;
 		}
-
+		DetermineSUT det = new DetermineSUT();
+		System.out.println(det.getSUTName(args[0], args[1]));
 	}
 
 }
