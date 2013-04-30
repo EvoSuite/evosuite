8af87e63bc0bc1d1ab840b43591a2601c89dde29
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-04-30 11:14:56 +0100

    Add message to uncompilable code exception

--- src/main/java/org/evosuite/testcase/UncompilableCodeException.java
+++ src/main/java/org/evosuite/testcase/UncompilableCodeException.java
@@ -4,4 +4,11 @@ public class UncompilableCodeException extends RuntimeException {
 
 	private static final long serialVersionUID = 2111256673991944796L;
 
+	public UncompilableCodeException() {
+		super();
+	}
+
+	public UncompilableCodeException(String message) {
+		super(message);
+	}
 }
