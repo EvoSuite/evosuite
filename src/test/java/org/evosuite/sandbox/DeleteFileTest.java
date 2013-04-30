71957a2cbbd3154fb3714ab1d68016f493dd0210
gordon <none@none>
2013-04-30 11:34:15 +0100

    Add test that tries to delete file in a process

--- src/test/java/org/evosuite/sandbox/DeleteFileTest.java
+++ src/test/java/org/evosuite/sandbox/DeleteFileTest.java
@@ -12,9 +12,11 @@ import org.apache.commons.io.FileUtils;
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.SystemTest;
+import org.junit.Assume;
 import org.junit.Test;
 
 import com.examples.with.different.packagename.DeleteFileExample;
+import com.examples.with.different.packagename.DeleteFileProcess;
 import com.examples.with.different.packagename.DeleteFileThread;
 
 /**
@@ -71,4 +73,31 @@ public class DeleteFileTest extends SystemTest {
 		           toDelete.exists());
 		toDelete.delete();
 	}
+
+	@Test
+	public void testDeleteOnProcess() throws IOException {
+		Assume.assumeTrue(new File("/bin/rm").exists());
+
+		String tmpdir = System.getProperty("java.io.tmpdir");
+		File toDelete = new File(tmpdir + File.separator
+		        + "this_file_should_not_be_deleted_by_evosuite");
+		FileUtils.write(toDelete, "BlahBlah");
+
+		assertTrue(toDelete.exists());
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = DeleteFileProcess.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.SANDBOX = true;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		evosuite.parseCommandLine(command);
+
+		assertTrue("File has been deleted: " + toDelete.getAbsolutePath(),
+		           toDelete.exists());
+		toDelete.delete();
+	}
+
 }
