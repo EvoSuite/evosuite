8053a782629beb2400dd47b1b79844c47be29af5
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-04-30 20:22:43 +0100

    New test

---
+++ src/test/java/org/evosuite/TestMemberClasses.java
@@ -0,0 +1,35 @@
+package org.evosuite;
+
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.MemberClassWithParameter;
+
+public class TestMemberClasses extends SystemTest {
+	@Test
+	public void testMemberclass() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = MemberClassWithParameter.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		Assert.assertTrue(result != null);
+		Assert.assertTrue("Invalid result type :" + result.getClass(),
+		                  result instanceof GeneticAlgorithm);
+
+		GeneticAlgorithm<?> ga = (GeneticAlgorithm<?>) result;
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		// int goals = TestSuiteGenerator.getFitnessFactory().getCoverageGoals().size();
+		// Assert.assertEquals("Wrong number of goals: ", 3, goals);
+		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+	}
+}
