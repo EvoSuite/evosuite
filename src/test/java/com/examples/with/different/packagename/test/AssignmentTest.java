2ba1e19e4966ba257c9d9afad798930ce9240178
gordon <none@none>
2013-05-01 23:50:00 +0100

    Fix assignment regression test

--- src/test/java/com/examples/with/different/packagename/test/AssignmentTest.java
+++ src/test/java/com/examples/with/different/packagename/test/AssignmentTest.java
@@ -10,25 +10,14 @@ public class AssignmentTest {
 
 	public void foo(AssignmentTest other) {
 		if (other.foo.x != 0 && foo.x != 0) {
-			if (other.foo.x == 3 * (foo.x + 1) + 17) {
-				// Target
-				System.out.println("test");
-			}
+			// Target
+			System.out.println("test");
 		}
 	}
 
 	public void bar(AssignmentTest other) {
-		if (other.foo.x != 0 && foo.x != 0 && other != this) {
-			if (other.foo.x * 2 == 4 * (foo.x + 2) + 20) {
-				// Target
-				System.out.println("test");
-			}
-		}
-	}
-
-	public void zoo(AssignmentTest other, int[] y) {
-		if (other.foo.x != 0 && foo.x != 0 && y.length == 2 && other != this) {
-			if (y[0] == foo.x && y[1] == other.foo.x && y[0] != y[1]) {
+		if (other.foo.x != 0 && foo.x != 0) {
+			if (other.foo.x != foo.x) {
 				// Target
 				System.out.println("test");
 			}
