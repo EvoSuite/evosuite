eae09f408abbd55e30fe7d9e585a95b8064788a2
arcuri <none@none>
2013-05-11 09:06:49 +0200

    more support to run CTG from command line

--- src/main/java/org/evosuite/continuous/job/JobExecutor.java
+++ src/main/java/org/evosuite/continuous/job/JobExecutor.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 import org.evosuite.continuous.persistency.StorageManager;
 
+/**
+ * Job executor will run EvoSuite on separate processes.
+ * There will be no communication with these masters/clients, whose
+ * visible side-effects would only be files written on local disk.
+ * This does simplify the architecture a lot, especially considering we can 
+ * have several instances running in parallel.
+ * Downside is not a big deal, as the searches in a schedule are anyway run independently. 
+ * 
+ * @author arcuri
+ *
+ */
 public class JobExecutor {
 
-	public JobExecutor(StorageManager storage) {
+	public JobExecutor(StorageManager storage, int timeInMinutes, String projectClassPath, int totalMemoryInMB) {
 		// TODO Auto-generated constructor stub
 	}
 
