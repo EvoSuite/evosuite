d5d8eae66393ccaceec3ec5ffef5d00079fe4c5f
Andrea Arcuri <arcuri82@gmail.com>
2013-05-14 00:26:36 +0200

    adding some stubs and comment ideas  for CTG

---
+++ src/main/java/org/evosuite/continuous/job/schedule/BudgetSchedule.java
@@ -0,0 +1,11 @@
+package org.evosuite.continuous.job.schedule;
+
+/**
+ * CUTs with more branches will be given more time (ie search budget)
+ * 
+ * @author arcuri
+ *
+ */
+public class BudgetSchedule {
+	//TODO
+}
---
+++ src/main/java/org/evosuite/continuous/job/schedule/SeedingSchedule.java
@@ -0,0 +1,13 @@
+package org.evosuite.continuous.job.schedule;
+
+/**
+ * Choose a precise order in which the CUTs will be targeted.
+ * Test cases for a CUT can be used for seeding in the search
+ * of the following CUTs in the schedule
+ * 
+ * @author arcuri
+ *
+ */
+public class SeedingSchedule {
+	//TODO
+}
