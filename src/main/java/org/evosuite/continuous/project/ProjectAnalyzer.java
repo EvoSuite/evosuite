335774a1c557279e82e434f1df74d31e9601a7ab
arcuri <none@none>
2013-07-01 18:50:46 +0200

    CTG: analysis of current project stats

--- src/main/java/org/evosuite/continuous/project/ProjectAnalyzer.java
+++ src/main/java/org/evosuite/continuous/project/ProjectAnalyzer.java
@@ -1,9 +1,17 @@
 package org.evosuite.continuous.project;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.regex.Pattern;
 
+import org.evosuite.Properties;
+import org.evosuite.continuous.project.ProjectStaticData.ClassInfo;
+import org.evosuite.coverage.branch.BranchPool;
+import org.evosuite.instrumentation.InstrumentingClassLoader;
+import org.evosuite.sandbox.Sandbox;
 import org.evosuite.utils.ResourceList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>
@@ -16,12 +24,16 @@ import org.evosuite.utils.ResourceList;
  * as we use the generated statistics <i>only</i> for heuristics
  * </p>
  * 
+ * <p>
+ * Note: this class assumes the classpath is properly set
+ * 
  * @author arcuri
  *
  */
 public class ProjectAnalyzer {
  
-
+	private static Logger logger = LoggerFactory.getLogger(ProjectAnalyzer.class);
+	
 	/**
 	 * the folder/jar where to find the .class files used as CUTs
 	 */
@@ -34,15 +46,59 @@ public class ProjectAnalyzer {
 	
 	public ProjectStaticData analyze(){
 		Pattern pattern = Pattern.compile("[^\\$]*.class");
-		Collection<String> resources = ResourceList.getResources(target, pattern);
+		Collection<String> classes = ResourceList.getResources(target, pattern);
 		
 		ProjectStaticData data = new ProjectStaticData();
 		
-		for (String resource : resources) {
-			//TODO: processing/validation should be done here
+		for (String fileName : classes) {
+			String className = fileName.replace(".class", "").replaceAll(File.separator, ".");
+
+			Class<?> theClass = null; 
+			int numberOfBranches = -1;			
+			boolean hasCode = false;
 			
+			Properties.TARGET_CLASS = className;
+			InstrumentingClassLoader instrumenting = new InstrumentingClassLoader();
+			
+			BranchPool.reset();
+			
+			try{
+				/*
+				 * to access number of branches, we need to use
+				 * instrumenting class loader. But loading a class would
+				 * execute its static code, and so we need to 
+				 * use a security manager. 
+				 */
+				Sandbox.goingToExecuteUnsafeCodeOnSameThread();
+				instrumenting.loadClass(className);
+				
+				numberOfBranches = BranchPool.getBranchCounter();
+				hasCode = (numberOfBranches > 0) || (BranchPool.getBranchlessMethods().size() > 0);
+
+				/*
+				 * just to avoid possible issues with instrumenting classloader
+				 */
+				theClass = ClassLoader.getSystemClassLoader().loadClass(className);
+
+				//TODO kind
+				//if(theClass.isInterface()){
+				//	kind = ClassKind.INTERFACE;
+				//} else if(theClass.is  Modifier.isAbstract( someClass.getModifiers() );
+				
+			} catch  (Exception e) {
+				logger.warn("Cannot handle "+className);
+				continue;
+			}
+			finally {
+				Sandbox.doneWithExecutingUnsafeCodeOnSameThread();
+				BranchPool.reset();
+				Properties.TARGET_CLASS = "";
+			}
+			
+			data.addNewClass(new ClassInfo(theClass, numberOfBranches, hasCode));
 		}
 		
 		return data;
 	}
 }
+
