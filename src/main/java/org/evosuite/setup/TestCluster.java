18cdf570bd6b434e646efd7bf6e3e18fcf43e032
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-07-01 21:14:51 +0100

    Do not instantiate class types

--- src/main/java/org/evosuite/setup/TestCluster.java
+++ src/main/java/org/evosuite/setup/TestCluster.java
@@ -730,6 +730,8 @@ public class TestCluster {
 			return selectedClass;
 		} else if (!clazz.isParameterizedType())
 			return clazz;
+		else if(clazz.isClass())
+			return clazz;
 
 		List<Type> parameterTypes = new ArrayList<Type>();
 		List<TypeVariable<?>> typeParameters = clazz.getTypeVariables();
