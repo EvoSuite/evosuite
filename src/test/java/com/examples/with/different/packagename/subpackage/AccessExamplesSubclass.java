8cba8a0df54e32540b3227d53b1d40df92581851
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-07-04 12:28:42 +0100

    Add missing test class

---
+++ src/test/java/com/examples/with/different/packagename/subpackage/AccessExamplesSubclass.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.examples.with.different.packagename.subpackage;
+
+import com.examples.with.different.packagename.AccessExamples;
+
+/**
+ * @author Gordon Fraser
+ *
+ */
+public class AccessExamplesSubclass extends AccessExamples {
+
+}
