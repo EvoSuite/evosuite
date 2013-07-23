8260688411a3a407cf77d7130fa1d9a1bdd524ff
Andrea Arcuri <arcuri82@gmail.com>
2013-07-23 15:30:31 +0200

    some more comments for JavaAgent

--- src/main/java/org/evosuite/agent/InstrumentingAgent.java
+++ src/main/java/org/evosuite/agent/InstrumentingAgent.java
@@ -5,6 +5,21 @@ import java.lang.instrument.Instrumentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This is the entry point for the JavaAgent.
+ * This is responsible to instrument the code in
+ * the generated JUnit test cases.
+ * During EvoSuite search, EvoSuite does not need an agent,
+ * as instrumentation can be done at classloader level.
+ * 
+ * <p>
+ * Note: we need JavaAgent in JUnit as the classes could have already
+ * been loaded/instrumented, eg as it happens when tools like
+ * Emma, Cobertura, Javalanche, etc., are used.
+ * 
+ * @author arcuri
+ *
+ */
 public class  InstrumentingAgent {
 
 	private static final Logger logger = LoggerFactory.getLogger(InstrumentingAgent.class);
