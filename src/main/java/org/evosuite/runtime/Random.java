8c6267886f0c8b52343e185d16f55b1ca796d009
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-08-08 11:38:01 +0100

    Fix random calls for making EvoSuite tests deterministic

--- src/main/java/org/evosuite/runtime/Random.java
+++ src/main/java/org/evosuite/runtime/Random.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,8 +21,10 @@
 package org.evosuite.runtime;
 
 /**
- * <p>Random class.</p>
- *
+ * <p>
+ * Random class.
+ * </p>
+ * 
  * @author Gordon Fraser
  */
 public class Random {
@@ -37,7 +39,7 @@ public class Random {
 
 	/**
 	 * Replacement function for nextInt
-	 *
+	 * 
 	 * @return a int.
 	 */
 	public static int nextInt() {
@@ -47,8 +49,9 @@ public class Random {
 
 	/**
 	 * Replacement function for nextInt
-	 *
-	 * @param max a int.
+	 * 
+	 * @param max
+	 *            a int.
 	 * @return a int.
 	 */
 	public static int nextInt(int max) {
@@ -58,7 +61,7 @@ public class Random {
 
 	/**
 	 * Replacement function for nextFloat
-	 *
+	 * 
 	 * @return a float.
 	 */
 	public static float nextFloat() {
@@ -67,8 +70,18 @@ public class Random {
 	}
 
 	/**
+	 * Replacement function for nextFloat
+	 * 
+	 * @return a float.
+	 */
+	public static float nextDouble() {
+		wasAccessed = true;
+		return currentNumber++;
+	}
+
+	/**
 	 * Replacement function for nextLong
-	 *
+	 * 
 	 * @return a long.
 	 */
 	public static long nextLong() {
@@ -78,8 +91,9 @@ public class Random {
 
 	/**
 	 * Set the next random number to a value
-	 *
-	 * @param number a int.
+	 * 
+	 * @param number
+	 *            a int.
 	 */
 	public static void setNextRandom(int number) {
 		currentNumber = number;
@@ -96,7 +110,7 @@ public class Random {
 	/**
 	 * Getter to check whether this runtime replacement was accessed during test
 	 * execution
-	 *
+	 * 
 	 * @return a boolean.
 	 */
 	public static boolean wasAccessed() {
