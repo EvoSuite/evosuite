77ea53a3ef1b6f1f98b2c4255aca4c1a7825c8a9
gordon <none@none>
2013-10-30 10:15:09 +0000

    Test fixes

--- src/test/java/org/evosuite/TestWrongCommand.java
+++ src/test/java/org/evosuite/TestWrongCommand.java
@@ -1,24 +1,24 @@
 /**
  * Copyright (C) 2011,2012 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
- *
+ * 
  * This file is part of EvoSuite.
- *
+ * 
  * EvoSuite is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- *
+ * 
  * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Public License along with
  * EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.evosuite;
 
-import org.evosuite.EvoSuite;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Andrea Arcuri
@@ -26,12 +26,12 @@ import org.junit.*;
  */
 public class TestWrongCommand {
 
-	@Test
-	public void testWrongCommand(){
+	@Test(expected = IllegalArgumentException.class)
+	public void testWrongCommand() {
 		EvoSuite evosuite = new EvoSuite();
-		
-		Object result = evosuite.parseCommandLine(new String[]{"foo"});
-		
-		Assert.assertTrue( result == null);
+
+		Object result = evosuite.parseCommandLine(new String[] { "foo" });
+
+		Assert.assertTrue(result == null);
 	}
 }
