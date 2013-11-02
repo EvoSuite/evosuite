0518603182983a47dceb6068885e7b7e498aa03e
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-11-02 21:29:19 +0000

    PMD fixes

--- src/main/java/org/evosuite/localsearch/LocalSearch.java
+++ src/main/java/org/evosuite/localsearch/LocalSearch.java
@@ -4,6 +4,6 @@ import org.evosuite.ga.Chromosome;
 
 public interface LocalSearch<T extends Chromosome> {
 
-	public abstract boolean doSearch(T individual, LocalSearchObjective<T> objective);
+	public boolean doSearch(T individual, LocalSearchObjective<T> objective);
 	
 }
