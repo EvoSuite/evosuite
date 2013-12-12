f920300bac1931501863fd2f40700000e1908912
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2013-12-12 06:32:51 +0000

    Change in dynamic seeding probability for experiment

--- src/main/java/org/evosuite/seeding/ConstantPoolManager.java
+++ src/main/java/org/evosuite/seeding/ConstantPoolManager.java
@@ -36,7 +36,8 @@ public class ConstantPoolManager {
 
 	private void initDefaultProbabilities() {
 		probabilities = new double[pools.length];
-		double p = 1d / probabilities.length;
+//		double p = 1d / probabilities.length;
+		double p = (1d - Properties.DYNAMIC_POOL) / (probabilities.length - 1);
 		for (int i = 0; i < probabilities.length; i++) {
 			probabilities[i] = p;
 		}
