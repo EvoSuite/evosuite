1a19b22b2a73b92a6707ba062450d103e6127e14
Juan Galeotti <galeotti@cs.uni-saarland.de>
2013-12-13 23:58:34 +0100

    enhance of test case

--- src/test/java/com/examples/with/different/packagename/statichack/StaticHack.java
+++ src/test/java/com/examples/with/different/packagename/statichack/StaticHack.java
@@ -9,13 +9,17 @@ public class StaticHack {
 		throw new IllegalStateException();
 	}
 	
+	public static int reset() throws IllegalStateException{
+		counter=0;
+		throw new IllegalStateException("Cannot invoke reset()");
+	}
+	
 	public boolean checkCounter() {
 		if (counter==0) {
 			return true;
 		} else {
 			return false;
 		}
-			
 	}
 	
 }
