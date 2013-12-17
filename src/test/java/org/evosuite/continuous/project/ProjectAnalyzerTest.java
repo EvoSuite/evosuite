f1212bf1d0432364136060f0eec23e506cc5fe58
fraser <none@none>
2013-12-17 16:09:21 +0000

    Fix classpath in test case

--- src/test/java/org/evosuite/continuous/project/ProjectAnalyzerTest.java
+++ src/test/java/org/evosuite/continuous/project/ProjectAnalyzerTest.java
@@ -1,7 +1,9 @@
 package org.evosuite.continuous.project;
 
 import org.evosuite.continuous.project.ProjectStaticData.ClassInfo;
+import org.evosuite.utils.ClassPathHandler;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.examples.with.different.packagename.continuous.Simple;
@@ -9,26 +11,28 @@ import com.examples.with.different.packagename.continuous.Trivial;
 
 public class ProjectAnalyzerTest {
 
+	@BeforeClass
+	public static void initClass() {
+		ClassPathHandler.getInstance().changeTargetCPtoTheSameAsEvoSuite();
+	}
+
 	@Test
-	public void testBranches(){
-		
-		String[] cuts = new String[]{
-				Simple.class.getName(),
-				Trivial.class.getName()
-		};
-		
+	public void testBranches() {
+
+		String[] cuts = new String[] { Simple.class.getName(), Trivial.class.getName() };
+
 		ProjectAnalyzer pa = new ProjectAnalyzer(cuts);
-		
+
 		ProjectStaticData data = pa.analyze();
 
 		Assert.assertEquals(2, data.getTotalNumberOfClasses());
-		
+
 		ClassInfo simple = data.getClassInfo(Simple.class.getName());
 		Assert.assertNotNull(simple);
 		Assert.assertEquals(2, simple.numberOfBranches);
-		
-		ClassInfo trivial = data.getClassInfo(Trivial.class.getName()); 
+
+		ClassInfo trivial = data.getClassInfo(Trivial.class.getName());
 		Assert.assertNotNull(trivial);
-		Assert.assertEquals(1, trivial.numberOfBranches);		
+		Assert.assertEquals(1, trivial.numberOfBranches);
 	}
 }
