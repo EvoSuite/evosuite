5b6b63f619f624f0aea908187c5699b0dd409ff9
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-02-24 17:42:34 +0100

    using getClassName() instead of writing java.lang.Object

--- src/main/java/org/evosuite/testcase/TestCodeVisitor.java
+++ src/main/java/org/evosuite/testcase/TestCodeVisitor.java
@@ -649,10 +649,10 @@ public class TestCodeVisitor extends TestVisitor {
 				        + getVariableName(dest) + ");";
 		} else {
 			if (((Boolean) value).booleanValue())
-				testCode += "assertTrue(" + getVariableName(source) + ".equals((java.lang.Object)"
+				testCode += "assertTrue(" + getVariableName(source) + ".equals((" + this.getClassName(Object.class) +")"
 				        + getVariableName(dest) + "));";
 			else
-				testCode += "assertFalse(" + getVariableName(source) + ".equals((java.lang.Object)"
+				testCode += "assertFalse(" + getVariableName(source) + ".equals((" + this.getClassName(Object.class) +")"
 				        + getVariableName(dest) + "));";
 		}
 	}
