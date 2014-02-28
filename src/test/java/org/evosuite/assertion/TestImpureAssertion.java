6de7cab0a30c9041e695d262f8ddbcaf0ae9225b
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-02-28 11:48:34 +0100

    tests for checking purity of inspectors during JUnit analysis

---
+++ src/test/java/org/evosuite/assertion/TestImpureAssertion.java
@@ -0,0 +1,64 @@
+package org.evosuite.assertion;
+
+import static org.junit.Assert.assertFalse;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.testsuite.SearchStatistics;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.utils.ReportGenerator.StatisticEntry;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.inspector.ImpureInspector;
+
+public class TestImpureAssertion extends SystemTest {
+	private boolean reset_statick_field__property;
+	private boolean junit_check_property;
+	private boolean junit_tests_property;
+	
+	@Before
+	public void saveProperties() {
+		reset_statick_field__property = Properties.RESET_STATIC_FIELDS;
+		junit_check_property = Properties.JUNIT_CHECK;
+		junit_tests_property = Properties.JUNIT_TESTS;
+
+		Properties.RESET_STATIC_FIELDS = true;
+		Properties.JUNIT_CHECK = true;
+		Properties.JUNIT_TESTS = true;
+	}
+
+	@After
+	public void restoreProperties() {
+		Properties.RESET_STATIC_FIELDS = reset_statick_field__property ;
+		Properties.JUNIT_CHECK = junit_check_property;
+		Properties.JUNIT_TESTS = junit_tests_property;
+
+	}
+
+	@Test
+	public void test() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = ImpureInspector.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		String[] command = new String[] {"-generateSuite", "-class", targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+		double best_fitness = best.getFitness();
+		Assert.assertTrue("Optimal coverage was not achieved ", best_fitness == 0.0);
+		
+		StatisticEntry entry = SearchStatistics.getInstance().getLastStatisticEntry();
+		assertFalse(entry.hadUnstableTests);
+	}
+	
+	
+}
