cc998c6397054af691f6ca0b76ac6e692b54543f
arcuri <none@none>
2014-03-04 23:55:22 +0100

    adding tests

--- src/test/java/org/evosuite/sandbox/DeleteFileTest.java
+++ src/test/java/org/evosuite/sandbox/DeleteFileTest.java
@@ -14,6 +14,7 @@ import org.evosuite.Properties;
 import org.evosuite.SystemTest;
 import org.evosuite.ga.GeneticAlgorithm;
 import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
@@ -22,12 +23,76 @@ import com.examples.with.different.packagename.DeleteFileCommonsIO;
 import com.examples.with.different.packagename.DeleteFileExample;
 import com.examples.with.different.packagename.DeleteFileProcess;
 import com.examples.with.different.packagename.DeleteFileThread;
+import com.examples.with.different.packagename.sandbox.DeleteInStaticInitializer;
 
 /**
  * @author Gordon Fraser
  * 
  */
 public class DeleteFileTest extends SystemTest {
+	
+	private static final boolean DEFAULT_RESET_STATIC = Properties.RESET_STATIC_FIELDS; 
+	
+	@After
+	public void tearDown(){
+		Properties.RESET_STATIC_FIELDS = DEFAULT_RESET_STATIC;
+	}
+	
+	@Test
+	public void testDeleteStaticNoReset() throws IOException {
+
+		Properties.RESET_STATIC_FIELDS = false;
+		
+		String tmpdir = System.getProperty("java.io.tmpdir");
+		File toDelete = new File(tmpdir + File.separator
+		        + "this_file_should_not_be_deleted_by_evosuite");
+		FileUtils.write(toDelete, "BlahBlah");
+
+		assertTrue(toDelete.exists());
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = DeleteInStaticInitializer.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.SANDBOX = true;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		evosuite.parseCommandLine(command);
+
+		assertTrue("File has been deleted: " + toDelete.getAbsolutePath(),
+		           toDelete.exists());
+		toDelete.delete();
+	}
+	
+	@Test
+	public void testDeleteStaticWithReset() throws IOException {
+
+		Properties.RESET_STATIC_FIELDS = true;
+		
+		String tmpdir = System.getProperty("java.io.tmpdir");
+		File toDelete = new File(tmpdir + File.separator
+		        + "this_file_should_not_be_deleted_by_evosuite");
+		FileUtils.write(toDelete, "BlahBlah");
+
+		assertTrue(toDelete.exists());
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = DeleteInStaticInitializer.class.getCanonicalName();
+
+		Properties.TARGET_CLASS = targetClass;
+		Properties.SANDBOX = true;
+
+		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+		evosuite.parseCommandLine(command);
+
+		assertTrue("File has been deleted: " + toDelete.getAbsolutePath(),
+		           toDelete.exists());
+		toDelete.delete();
+	}
+	
+	
 	@Test
 	public void testDeleteOnExit() throws IOException {
 
