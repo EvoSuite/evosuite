63147c714dfcac30aacb89938bdec2e7bef1285e
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-03-04 14:48:24 +0100

    CheapPurityAnalysis supports purity of Jdk pure methods

---
+++ src/test/java/com/examples/with/different/packagename/inspector/JdkPureInspector.java
@@ -0,0 +1,21 @@
+package com.examples.with.different.packagename.inspector;
+
+public class JdkPureInspector {
+
+	private final Character character;
+	private final char char_value;
+	
+	public JdkPureInspector(char c) {
+		character = new Character(c);
+		char_value = c;
+	}
+	
+	public boolean equalsToZ() {
+		return character.charValue()=='z';
+	}
+	
+	public boolean isLowerCase() {
+		return Character.isLowerCase(char_value);
+	}
+	
+}
