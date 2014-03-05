9efbef2494d6150a6f306e59fb14fd5441162e97
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-03-05 10:26:08 +0100

    bugfix StackOverflow when analyzing overriding recursive methods in CheapPurityAnalysis

---
+++ src/test/java/org/evosuite/assertion/purity/TestToStringInspector.java
@@ -0,0 +1,116 @@
+package org.evosuite.assertion.purity;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.assertion.CheapPurityAnalyzer;
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.testsuite.SearchStatistics;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.utils.ReportGenerator.StatisticEntry;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.Type;
+
+import com.examples.with.different.packagename.inspector.AbstractToStringInspector;
+import com.examples.with.different.packagename.inspector.ImpureToStringInspector;
+import com.examples.with.different.packagename.inspector.ToStringInspector;
+
+public class TestToStringInspector extends SystemTest {
+	private final boolean DEFAULT_RESET_STATIC_FIELDS = Properties.RESET_STATIC_FIELDS;
+	private final boolean DEFAULT_JUNIT_CHECK = Properties.JUNIT_CHECK;
+	private final boolean DEFAULT_JUNIT_TESTS = Properties.JUNIT_TESTS;
+	private final boolean DEFAULT_PURE_INSPECTORS = Properties.PURE_INSPECTORS;
+	private final boolean DEFAULT_SANDBOX = Properties.SANDBOX;
+
+	@Before
+	public void saveProperties() {
+		Properties.SANDBOX = true;
+		Properties.RESET_STATIC_FIELDS = true;
+		Properties.JUNIT_CHECK = true;
+		Properties.JUNIT_TESTS = true;
+		Properties.PURE_INSPECTORS = true;
+	}
+
+	@After
+	public void restoreProperties() {
+		Properties.RESET_STATIC_FIELDS = DEFAULT_RESET_STATIC_FIELDS;
+		Properties.JUNIT_CHECK = DEFAULT_JUNIT_CHECK;
+		Properties.JUNIT_TESTS = DEFAULT_JUNIT_TESTS;
+		Properties.PURE_INSPECTORS = DEFAULT_PURE_INSPECTORS;
+		Properties.SANDBOX = DEFAULT_SANDBOX;
+	}
+
+	@Test
+	public void testPureToString() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = ToStringInspector.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		String[] command = new String[] { "-generateSuite", "-class",
+				targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+		double best_fitness = best.getFitness();
+		Assert.assertTrue("Optimal coverage was not achieved ",
+				best_fitness == 0.0);
+
+		CheapPurityAnalyzer purityAnalyzer = CheapPurityAnalyzer.getInstance();
+
+		String descriptor = Type
+				.getMethodDescriptor(Type.getType(String.class));
+		boolean toString = purityAnalyzer.isPure(targetClass, "toString",
+				descriptor);
+		assertTrue(toString);
+
+		boolean abstractToString = purityAnalyzer.isPure(
+				AbstractToStringInspector.class.getCanonicalName(), "toString",
+				descriptor);
+		assertTrue(abstractToString);
+
+		StatisticEntry entry = SearchStatistics.getInstance()
+				.getLastStatisticEntry();
+		assertFalse(entry.hadUnstableTests);
+	}
+
+	@Test
+	public void testImpureToString() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = ImpureToStringInspector.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		String[] command = new String[] { "-generateSuite", "-class",
+				targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+		double best_fitness = best.getFitness();
+		Assert.assertTrue("Optimal coverage was not achieved ",
+				best_fitness == 0.0);
+
+		CheapPurityAnalyzer purityAnalyzer = CheapPurityAnalyzer.getInstance();
+
+		String descriptor = Type
+				.getMethodDescriptor(Type.getType(String.class));
+		boolean toString = purityAnalyzer.isPure(targetClass, "toString",
+				descriptor);
+		assertFalse(toString);
+
+		StatisticEntry entry = SearchStatistics.getInstance()
+				.getLastStatisticEntry();
+		assertFalse(entry.hadUnstableTests);
+	}
+
+}
