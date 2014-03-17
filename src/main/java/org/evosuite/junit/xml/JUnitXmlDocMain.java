ac4edb5f0e02be6b899bf2f7248ed504217b650b
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-03-17 21:57:40 +0100

    bugfix wrong import

--- src/main/java/org/evosuite/junit/xml/JUnitXmlDocMain.java
+++ src/main/java/org/evosuite/junit/xml/JUnitXmlDocMain.java
@@ -1,12 +1,6 @@
 package org.evosuite.junit.xml;
 
-import java.util.List;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
-
-import com.examples.with.different.packagename.junit.FooTest;
 
 public class JUnitXmlDocMain {
 
