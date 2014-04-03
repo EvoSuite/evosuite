f966dbbd67d7b2cd4f22d4f97abe8171f549fa05
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-04-03 11:32:26 +0200

    Test cases for default values of static fields

---
+++ src/test/java/com/examples/with/different/packagename/stable/HasClinit.java
@@ -0,0 +1,121 @@
+package com.examples.with.different.packagename.stable;
+
+import java.lang.reflect.Field;
+
+public class HasClinit {
+
+	private static final boolean SOME_TRUE_VALUE = true;
+	private static final boolean SOME_BOOLEAN_VALUE = false;
+	private static final byte SOME_BYTE_VALUE = 2;
+	private static final short SOME_SHORT_VALUE = 22222;
+	private static final char SOME_CHAR_VALUE = 'c';
+	private static final int SOME_INT_VALUE = 22222;
+	private static final long SOME_LONG_VALUE = 2;
+	private static final float SOME_FLOAT_VALUE = 2f;
+	private static final double SOME_DOUBLE_VALUE = 2f;
+	private static final String SOME_STRING_VALUE = "Hello World!";
+	private static final String[] SOME_STRING_ARRAY = new String[] {"Hello World!"};
+
+	public boolean reflecStringBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_STRING_VALUE");
+		Object object = field.get(null);
+		if (object==null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecDoubleBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_DOUBLE_VALUE");
+		double doubleValue = field.getLong(null);
+		if (doubleValue!=2f) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecFloatBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_FLOAT_VALUE");
+		long floatValue = field.getLong(null);
+		if (floatValue!=2f) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	
+	public boolean reflecIntBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_INT_VALUE");
+		int intValue = field.getInt(null);
+		if (intValue!=22222) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecLongBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_LONG_VALUE");
+		long longValue = field.getLong(null);
+		if (longValue!=2) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecShortBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_SHORT_VALUE");
+		short shortValue = field.getShort(null);
+		if (shortValue!=22222) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecCharBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_CHAR_VALUE");
+		char charValue = field.getChar(null);
+		if (charValue!='c') {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflectDoubleBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_DOUBLE_VALUE");
+		double doubleValue = field.getDouble(null);
+		if (doubleValue!=2f) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean reflecBooleanBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_TRUE_VALUE");
+		boolean booleanValue = field.getBoolean(null);
+		if (booleanValue!=true) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean reflecByteBranch() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Field field = HasClinit.class.getDeclaredField("SOME_BYTE_VALUE");
+		byte byteValue = field.getByte(null);
+		if (byteValue!=2) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	
+}
---
+++ src/test/java/org/evosuite/assertion/stable/TestNoClinit.java
@@ -0,0 +1,72 @@
+package org.evosuite.assertion.stable;
+
+import static org.junit.Assert.assertFalse;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.testsuite.SearchStatistics;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.utils.ReportGenerator.StatisticEntry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.stable.NoClinit;
+import com.examples.with.different.packagename.stable.RandomUUIDUser;
+
+public class TestNoClinit extends SystemTest {
+	private final boolean DEFAULT_RESET_STATIC_FIELDS = Properties.RESET_STATIC_FIELDS;
+	private final boolean DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS = Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS;
+	private final boolean DEFAULT_REPLACE_CALLS = Properties.REPLACE_CALLS;
+	private final boolean DEFAULT_JUNIT_CHECK = Properties.JUNIT_CHECK;
+	private final boolean DEFAULT_JUNIT_TESTS = Properties.JUNIT_TESTS;
+	private final boolean DEFAULT_PURE_INSPECTORS = Properties.PURE_INSPECTORS;
+	private final boolean DEFAULT_SANDBOX = Properties.SANDBOX;
+	
+
+	@Before
+	public void configureProperties() {
+		Properties.RESET_STATIC_FIELDS = true;
+		Properties.SANDBOX = true;
+		Properties.REPLACE_CALLS = true;
+		Properties.JUNIT_CHECK = true;
+		Properties.JUNIT_TESTS = true;
+		Properties.PURE_INSPECTORS = true;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS =true;
+
+	}
+
+	@After
+	public void restoreProperties() {
+		Properties.RESET_STATIC_FIELDS = DEFAULT_RESET_STATIC_FIELDS;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS = DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS;
+		Properties.SANDBOX = DEFAULT_SANDBOX;
+		Properties.REPLACE_CALLS = DEFAULT_REPLACE_CALLS;
+		Properties.JUNIT_CHECK = DEFAULT_JUNIT_CHECK;
+		Properties.JUNIT_TESTS = DEFAULT_JUNIT_TESTS;
+		Properties.PURE_INSPECTORS = DEFAULT_PURE_INSPECTORS;
+	}
+
+	@Test
+	public void testNoClinit() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = NoClinit.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		String[] command = new String[] { "-generateSuite", "-class",
+				targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		StatisticEntry entry = SearchStatistics.getInstance()
+				.getLastStatisticEntry();
+		assertFalse(entry.hadUnstableTests);
+	}
+
+}
