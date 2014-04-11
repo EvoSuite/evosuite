4ec7b9b51c6798b33d2a866ecdac72b0c19d8a95
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-04-11 10:16:34 +0200

    bugfix: GenericMethod.isOverload() had a typo in a loop

---
+++ src/test/java/org/evosuite/assertion/stable/TestExitVM.java
@@ -0,0 +1,69 @@
+package org.evosuite.assertion.stable;
+
+import static org.junit.Assert.assertFalse;
+
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTest;
+import org.evosuite.ga.GeneticAlgorithm;
+import org.evosuite.testsuite.SearchStatistics;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.utils.ReportGenerator.StatisticEntry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.examples.with.different.packagename.stable.ExitVM;
+import com.examples.with.different.packagename.stable.RuntimeUser;
+
+public class TestExitVM extends SystemTest {
+
+	private final boolean DEFAULT_REPLACE_CALLS = Properties.REPLACE_CALLS;
+	private final boolean DEFAULT_JUNIT_CHECK = Properties.JUNIT_CHECK;
+	private final boolean DEFAULT_JUNIT_TESTS = Properties.JUNIT_TESTS;
+	private final boolean DEFAULT_PURE_INSPECTORS = Properties.PURE_INSPECTORS;
+	private final boolean DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS = Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS;
+	private final boolean DEFAULT_SANDBOX = Properties.SANDBOX;
+
+	@Before
+	public void before() {
+		Properties.SANDBOX = true;
+		Properties.REPLACE_CALLS = true;
+		Properties.JUNIT_CHECK = true;
+		Properties.JUNIT_TESTS = true;
+		Properties.PURE_INSPECTORS = true;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS = true;
+	}
+
+	@After
+	public void after() {
+		Properties.SANDBOX = DEFAULT_SANDBOX;
+		Properties.REPLACE_CALLS = DEFAULT_REPLACE_CALLS;
+		Properties.JUNIT_CHECK = DEFAULT_JUNIT_CHECK;
+		Properties.JUNIT_TESTS = DEFAULT_JUNIT_TESTS;
+		Properties.PURE_INSPECTORS = DEFAULT_PURE_INSPECTORS;
+		Properties.JUNIT_CHECK_ON_SEPARATE_PROCESS = DEFAULT_JUNIT_CHECK_ON_SEPARATE_PROCESS;
+	}
+
+	@Test
+	public void testRuntimeUser() {
+		EvoSuite evosuite = new EvoSuite();
+
+		String targetClass = ExitVM.class.getCanonicalName();
+		Properties.TARGET_CLASS = targetClass;
+		String[] command = new String[] { "-generateSuite", "-class",
+				targetClass };
+
+		Object result = evosuite.parseCommandLine(command);
+
+		GeneticAlgorithm<?> ga = getGAFromResult(result);
+		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println("EvolvedTestSuite:\n" + best);
+
+		StatisticEntry entry = SearchStatistics.getInstance()
+				.getLastStatisticEntry();
+		assertFalse(entry.hadUnstableTests);
+
+	}
+
+}
