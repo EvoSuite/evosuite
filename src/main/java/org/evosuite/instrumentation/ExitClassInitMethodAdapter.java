6739df665a350b22816b0e69d835cec30a09d48f
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-04-14 08:51:29 +0200

    bugfix: wrong unused import

--- src/main/java/org/evosuite/instrumentation/ExitClassInitMethodAdapter.java
+++ src/main/java/org/evosuite/instrumentation/ExitClassInitMethodAdapter.java
@@ -28,8 +28,6 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
-import com.sun.tools.classfile.Opcode;
-
 /**
  * Adds a call to ExecutionTracer.passedClassInitializationMethod() when
  * the <clinit> method begins its execution.
