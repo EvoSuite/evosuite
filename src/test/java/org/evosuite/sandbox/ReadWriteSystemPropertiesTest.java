73ae6c2ddd73373e2ef091abde05d0b7bd35068a
Andrea Arcuri <arcuri82@gmail.com>
2014-04-15 08:16:11 +0200

    reverted back removed test

--- src/test/java/org/evosuite/sandbox/ReadWriteSystemPropertiesTest.java
+++ src/test/java/org/evosuite/sandbox/ReadWriteSystemPropertiesTest.java
@@ -1,12 +1,11 @@
 package org.evosuite.sandbox;
 
-import static org.junit.Assert.assertFalse;
-
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.SystemTest;
@@ -16,11 +15,10 @@ import org.evosuite.result.TestGenerationResult;
 import org.evosuite.result.TestGenerationResultBuilder;
 import org.evosuite.testcase.TestCase;
 import org.evosuite.testcase.TestCaseExecutor;
-import org.evosuite.testsuite.SearchStatistics;
 import org.evosuite.testsuite.TestSuiteChromosome;
-import org.evosuite.utils.ReportGenerator.StatisticEntry;
 import org.junit.Test;
 
+import com.examples.with.different.packagename.DeleteFileCommonsIO;
 import com.examples.with.different.packagename.sandbox.ReadLineSeparator;
 import com.examples.with.different.packagename.sandbox.ReadWriteSystemProperties;
 
@@ -30,12 +28,10 @@ public class ReadWriteSystemPropertiesTest extends SystemTest {
 	private static final String aProperty = System.getProperty(ReadWriteSystemProperties.A_PROPERTY);
 	
 	private final boolean DEFAULT_REPLACE_CALLS = Properties.REPLACE_CALLS; 
-	private final boolean DEFAULT_SANDBOX = Properties.SANDBOX; 
 	
 	@After
 	public void reset(){
 		Properties.REPLACE_CALLS = DEFAULT_REPLACE_CALLS;
-		Properties.SANDBOX = DEFAULT_SANDBOX;
 	}
 	
 	@BeforeClass
@@ -137,9 +133,25 @@ public class ReadWriteSystemPropertiesTest extends SystemTest {
 		Assert.assertEquals(userDir, currentUserDir);
 		Assert.assertEquals(aProperty, currentAProperty);
 		
-		StatisticEntry entry = SearchStatistics.getInstance()
-				.getLastStatisticEntry();
-		assertFalse(entry.hadUnstableTests);
+		//now check the JUnit generation
+		List<TestCase> list = best.getTests();
+		int n = list.size();
+		Assert.assertTrue(n > 0);
+		
+		TestCaseExecutor.initExecutor(); //needed because it gets pulled down after the search
+		
+		for(TestCase tc : list){
+			Assert.assertFalse(tc.isUnstable());
+		}
+		
+		JUnitAnalyzer.removeTestsThatDoNotCompile(list);
+		Assert.assertEquals(n, list.size());		
+		JUnitAnalyzer.handleTestsThatAreUnstable(list);
+		Assert.assertEquals(n, list.size());
+
+		for(TestCase tc : list){
+			Assert.assertFalse(tc.isUnstable());
+		}
 		
 		Assert.assertEquals(userDir, currentUserDir);
 		Assert.assertEquals(aProperty, currentAProperty);
