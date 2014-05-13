59443a3df299ee60dccd77c27d9f4068d008cdae
arcuri <none@none>
2014-05-13 21:27:12 +0200

    adding some more logging

--- src/main/java/org/evosuite/executionmode/TestGeneration.java
+++ src/main/java/org/evosuite/executionmode/TestGeneration.java
@@ -189,6 +189,9 @@ public class TestGeneration {
 	
 	private static List<TestGenerationResult> generateTests(Properties.Strategy strategy, String target,
 	        List<String> args) {
+		
+		LoggingUtils.getEvoLogger().info("Going to generate test cases for class: "+target);
+		
 		String classPath = ClassPathHandler.getInstance().getEvoSuiteClassPath();		
 		String cp = ClassPathHandler.getInstance().getTargetProjectClasspath();
 		
