15dafc4dd8f84989752c0ecef5480221f69d5b0e
arcuri <none@none>
2014-06-13 18:16:59 +0200

    moving tests, fixed compilation issues

--- runtime/src/test/java/org/evosuite/mock/javax/swing/MockJFileChooserTest.java
+++ runtime/src/test/java/org/evosuite/mock/javax/swing/MockJFileChooserTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import javax.swing.JFileChooser;
 
-import org.evosuite.Properties;
+
 import org.evosuite.runtime.Runtime;
 import org.evosuite.runtime.RuntimeSettings;
 import org.evosuite.runtime.mock.java.io.MockFile;
