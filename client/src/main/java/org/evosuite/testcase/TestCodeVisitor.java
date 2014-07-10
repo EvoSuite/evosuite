5a44673a1c7f10aeb9e9ae1946affd01d154433a
gordon <none@none>
2014-07-10 21:42:58 +0100

    Avoid infinite loop in resolving generics

--- client/src/main/java/org/evosuite/testcase/TestCodeVisitor.java
+++ client/src/main/java/org/evosuite/testcase/TestCodeVisitor.java
@@ -20,7 +20,6 @@ package org.evosuite.testcase;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.GenericArrayType;
-import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
