c5bf87bf42d4596f6a08268ac0337fdd26d51999
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-07-16 23:12:56 +0200

    eclipse plugin: seed can be configured / launch separate process for EvoSuite / reformat

--- plugins/eclipse/evosuite-eclipse/src/main/java/org/evosuite/eclipse/popup/actions/TestGenerationJob.java
+++ plugins/eclipse/evosuite-eclipse/src/main/java/org/evosuite/eclipse/popup/actions/TestGenerationJob.java
@@ -3,12 +3,18 @@
  */
 package org.evosuite.eclipse.popup.actions;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -31,11 +37,9 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
-import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.classpath.ResourceList;
 import org.evosuite.eclipse.properties.EvosuitePropertyPage;
-import org.evosuite.result.TestGenerationResult;
 import org.evosuite.rmi.MasterServices;
 import org.evosuite.rmi.service.ClientState;
 import org.evosuite.rmi.service.ClientStateInformation;
@@ -57,7 +61,8 @@ public class TestGenerationJob extends Job {
 
 	protected final Map<String, Double> coverage = null;
 
-	public TestGenerationJob(Shell shell, final IResource target, String targetClass) {
+	public TestGenerationJob(Shell shell, final IResource target,
+			String targetClass) {
 		super("EvoSuite Test Generation: " + targetClass);
 		this.targetClass = targetClass;
 		this.target = target;
@@ -89,12 +94,12 @@ public class TestGenerationJob extends Job {
 			// manager that allows everything here, just to make RMI happy
 			System.setSecurityManager(new DumbSecurityManager());
 		}
-		Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+		Thread.currentThread().setContextClassLoader(
+				this.getClass().getClassLoader());
 	}
 
 	@Override
 	protected IStatus run(final IProgressMonitor monitor) {
-		EvoSuite evosuite = new EvoSuite();
 
 		monitor.beginTask("EvoSuite test suite generation", 100);
 
@@ -110,7 +115,7 @@ public class TestGenerationJob extends Job {
 
 				if (curr.getEntryKind() == IClasspathEntry.CPE_LIBRARY) {
 					IPath path = curr.getPath();
-					if(path.toFile().getName().startsWith("evosuite")) {
+					if (path.toFile().getName().startsWith("evosuite")) {
 						System.out.println("Skipping evosuite.jar");
 						continue;
 					}
@@ -122,22 +127,26 @@ public class TestGenerationJob extends Job {
 					if (path.toFile().exists()) {
 						classPath += path.toOSString();
 						System.out.println("Adding CPE_LIBRARY to classpath: "
-						        + path.toOSString());
+								+ path.toOSString());
 					} else {
-						classPath += target.getWorkspace().getRoot().getLocation().toOSString()
-						        + path.toOSString();
+						classPath += target.getWorkspace().getRoot()
+								.getLocation().toOSString()
+								+ path.toOSString();
 						System.out.println("Adding CPE_LIBRARY to classpath: "
-						        + target.getWorkspace().getRoot().getLocation().toOSString()
-						        + path.toOSString());
+								+ target.getWorkspace().getRoot().getLocation()
+										.toOSString() + path.toOSString());
 					}
 				} else if (curr.getEntryKind() == IClasspathEntry.CPE_CONTAINER) {
 					if (curr.isExported()) {
-						if (curr.toString().equals("org.eclipse.jdt.launching.JRE_CONTAINER")) {
+						if (curr.toString().equals(
+								"org.eclipse.jdt.launching.JRE_CONTAINER")) {
 							System.out.println("Found JRE container");
-						} else if (curr.toString().startsWith("org.eclipse.jdt.junit.JUNIT_CONTAINER")) {
+						} else if (curr.toString().startsWith(
+								"org.eclipse.jdt.junit.JUNIT_CONTAINER")) {
 							System.out.println("Found JUnit container");
 						} else {
-							System.out.println("Found unknown container: " + curr);
+							System.out.println("Found unknown container: "
+									+ curr);
 						}
 					} else {
 						System.out.println("Container not exported: " + curr);
@@ -148,13 +157,14 @@ public class TestGenerationJob extends Job {
 				} else if (curr.getEntryKind() == IClasspathEntry.CPE_VARIABLE) {
 					System.out.println("Path: " + curr.getPath());
 					System.out.println("Resolved Path: "
-					        + JavaCore.getResolvedVariablePath(curr.getPath()));
+							+ JavaCore.getResolvedVariablePath(curr.getPath()));
 					if (!first)
 						classPath += File.pathSeparator;
 					else
 						first = false;
 
-					classPath += JavaCore.getResolvedVariablePath(curr.getPath());
+					classPath += JavaCore.getResolvedVariablePath(curr
+							.getPath());
 				} else if (curr.getEntryKind() == IClasspathEntry.CPE_SOURCE) {
 					System.out.println("Don't handle CPE_SOURCE yet");
 				} else {
@@ -162,22 +172,26 @@ public class TestGenerationJob extends Job {
 				}
 			}
 			ResourceList.resetCache();
-			if(!first)
+			if (!first)
 				classPath += File.pathSeparator;
-			
-			classPath += target.getWorkspace().getRoot().findMember(jProject.getOutputLocation()).getLocation().toOSString();
+
+			classPath += target.getWorkspace().getRoot()
+					.findMember(jProject.getOutputLocation()).getLocation()
+					.toOSString();
+
+			String baseDir = target.getProject().getLocation().toOSString();
 			Properties.TARGET_CLASS = targetClass;
 			List<String> commands = new ArrayList<String>();
 			commands.addAll(Arrays.asList(new String[] {
-			        "-generateSuite",
-			        "-class",
-			        targetClass,
-			        "-Dtest_dir=" + target.getProject().getLocation() + "/evosuite-tests",
-			        "-evosuiteCP", TestGenerationAction.getEvoSuiteJar(), "-projectCP",
-			        classPath, "-base_dir",
-			        target.getProject().getLocation().toOSString(),
-			        "-Dshow_progress=false", "-Dstopping_condition=MaxTime",
-			        "-Dtest_comments=false", "-Dpure_inspectors=true" })); //  "-Dsandbox_mode=IO", -Djava.rmi.server.codebase=file:///Users/gordon/Documents/Workspace/evosuite-eclipse/lib/evosuite-0.1-SNAPSHOT-jar-minimal.jar
+					"-generateSuite",
+					"-class",
+					targetClass,
+					"-Dtest_dir=" + target.getProject().getLocation()
+							+ "/evosuite-tests", "-evosuiteCP",
+					TestGenerationAction.getEvoSuiteJar(), "-projectCP",
+					classPath, "-base_dir", baseDir, "-Dshow_progress=false",
+					"-Dstopping_condition=MaxTime", "-Dtest_comments=false",
+					"-Dpure_inspectors=true" })); //  "-Dsandbox_mode=IO", -Djava.rmi.server.codebase=file:///Users/gordon/Documents/Workspace/evosuite-eclipse/lib/evosuite-0.1-SNAPSHOT-jar-minimal.jar
 			commands.addAll(getAdditionalParameters());
 			String cp = "";
 			first = true;
@@ -192,25 +206,30 @@ public class TestGenerationJob extends Job {
 						cp += File.pathSeparator;
 					else
 						first = false;
-					cp += target.getWorkspace().getRoot().getLocation().toOSString()
-					        + path.toOSString();
+					cp += target.getWorkspace().getRoot().getLocation()
+							.toOSString()
+							+ path.toOSString();
 				}
 			}
 
-			String budget = target.getProject().getPersistentProperty(EvosuitePropertyPage.TIME_PROP_KEY);
+			String budget = target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.TIME_PROP_KEY);
 			if (budget == null) {
 				commands.add("-Dsearch_budget=120");
 			} else {
 				commands.add("-Dsearch_budget=" + budget);
 			}
 			;
-			if ("false".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.ASSERTION_PROP_KEY))) {
+			if ("false".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.ASSERTION_PROP_KEY))) {
 				commands.add("-Dassertions=false");
 			}
-			if ("false".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.MINIMIZE_TESTS_PROP_KEY))) {
+			if ("false".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.MINIMIZE_TESTS_PROP_KEY))) {
 				commands.add("-Dminimize=false");
 			}
-			if (!"false".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.MINIMIZE_VALUES_PROP_KEY))) {
+			if (!"false".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.MINIMIZE_VALUES_PROP_KEY))) {
 				commands.add("-Dminimize_values=true");
 			}
 			// if (!"true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.RUNNER_PROP_KEY))) {
@@ -218,37 +237,55 @@ public class TestGenerationJob extends Job {
 			//	commands.add("-Dreplace_calls=false");
 			//} else {
 			//	commands.add("-Djunit_runner=true");
-			if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.DETERMINISTIC_PROP_KEY))) {
+			if ("true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.DETERMINISTIC_PROP_KEY))) {
 				commands.add("-Dreplace_calls=true");
 				commands.add("-Dreset_static_fields=true");
 				commands.add("-Dvirtual_fs=true");
 			}
 			//	}
 
-			if (!"true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.REPORT_PROP_KEY))) {
+			if (!"true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.REPORT_PROP_KEY))) {
 				commands.add("-Dhtml=false");
 				//commands.add("-Dstatistics_backend=none");
 			} else {
-				if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.PLOT_PROP_KEY))) {
+				if ("true".equals(target.getProject().getPersistentProperty(
+						EvosuitePropertyPage.PLOT_PROP_KEY))) {
 					commands.add("-Dplot=true");
 				}
 			}
-			if ("false".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.SANDBOX_PROP_KEY))) {
+			if ("false".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.SANDBOX_PROP_KEY))) {
 				commands.add("-Dsandbox=false");
 			}
-			if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.CONTRACTS_PROP_KEY))) {
+			if ("true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.CONTRACTS_PROP_KEY))) {
 				commands.add("-Dcheck_contracts=true");
 			}
-			if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.ERROR_BRANCHES_PROP_KEY))) {
+			if ("true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.ERROR_BRANCHES_PROP_KEY))) {
 				commands.add("-Derror_branches=true");
 			}
-			if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.DSE_PROP_KEY))) {
-				commands.add("-Dlocal_search_rate=3");
-				commands.add("-Dlocal_search_selective=true");
-				commands.add("-Dlocal_search_selective_primitives=true");
-				commands.add("-Dlocal_search_dse=statement");
+			if ("true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.DSE_PROP_KEY))) {
+				commands.add("-Dlocal_search_rate=1");
+				commands.add("-Dlocal_search_probability=1.0");
+				commands.add("-Dlocal_search_adaptation_rate=1.0");
+				commands.add("-Dlocal_search_selective=false");
+				commands.add("-Dlocal_search_selective_primitives=false");
+				commands.add("-Dlocal_search_dse=suite");
+				commands.add("-Dlocal_search_budget_type=time");
+				commands.add("-Dlocal_search_budget=15");
 			}
-			String criterion = target.getProject().getPersistentProperty(EvosuitePropertyPage.CRITERION_PROP_KEY);
+			String seed = target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.SEED_PROP_KEY);
+			if (seed != null) {
+				commands.add("-seed");
+				commands.add(seed);
+			}
+			String criterion = target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.CRITERION_PROP_KEY);
 			if (criterion != null) {
 				commands.add("-criterion");
 				commands.add(criterion);
@@ -268,20 +305,26 @@ public class TestGenerationJob extends Job {
 						String subTask = "";
 
 						while (percent != -1 && !isInterrupted()) {
-							MasterNodeLocal masterNode = MasterServices.getInstance().getMasterNode();
+							MasterNodeLocal masterNode = MasterServices
+									.getInstance().getMasterNode();
 							if (masterNode != null) {
-								Collection<ClientStateInformation> currentStates = MasterServices.getInstance().getMasterNode().getCurrentStateInformation();
+								Collection<ClientStateInformation> currentStates = MasterServices
+										.getInstance().getMasterNode()
+										.getCurrentStateInformation();
 								if (currentStates.size() == 1) {
-									ClientStateInformation currentState = currentStates.iterator().next();
+									ClientStateInformation currentState = currentStates
+											.iterator().next();
 									lastState = currentState;
 
 									percent = currentState.getOverallProgress();
 									if (percent >= 100
-									        && currentState.getState() == ClientState.NOT_STARTED)
+											&& currentState.getState() == ClientState.NOT_STARTED)
 										continue;
 
-									String currentTask = currentState.getState().getDescription();
-									if (percent > last || !subTask.equals(currentTask)) {
+									String currentTask = currentState
+											.getState().getDescription();
+									if (percent > last
+											|| !subTask.equals(currentTask)) {
 										subTask = currentTask;
 										monitor.worked(percent - last);
 										monitor.subTask(subTask);
@@ -294,8 +337,9 @@ public class TestGenerationJob extends Job {
 
 					} catch (Exception e) {
 
-						System.err.println("Exception while reading output of client process "
-						        + e);
+						System.err
+								.println("Exception while reading output of client process "
+										+ e);
 					}
 
 				}
@@ -303,8 +347,9 @@ public class TestGenerationJob extends Job {
 			};
 
 			progressMonitor.start();
-			List<List<TestGenerationResult>> results = (List<List<TestGenerationResult>>) evosuite.parseCommandLine(command);
-			System.out.println("Results: "+results.size());
+			launchProcess(baseDir, command);
+
+			//			System.out.println("Results: "+results.size());
 			// for(TestGenerationResult result : results) {
 			//	System.out.println("Covered lines: "+result.getCoveredLines());
 			// }
@@ -331,7 +376,8 @@ public class TestGenerationJob extends Job {
 		}
 		try {
 			target.getProject().refreshLocal(IProject.DEPTH_INFINITE, null);
-			if ("true".equals(target.getProject().getPersistentProperty(EvosuitePropertyPage.REPORT_PROP_KEY))) {
+			if ("true".equals(target.getProject().getPersistentProperty(
+					EvosuitePropertyPage.REPORT_PROP_KEY))) {
 				syncWithUi(target);
 			}
 		} catch (CoreException e) {
@@ -343,13 +389,55 @@ public class TestGenerationJob extends Job {
 		return Status.OK_STATUS;
 	}
 
+	private boolean isFinished(Process process) {
+		try {
+			process.exitValue();
+			return true;
+		} catch (IllegalThreadStateException ex) {
+			return false;
+		}
+	}
+
+	private void launchProcess(String baseDir, String[] evoSuiteOptions) throws IOException {
+		//		EvoSuite evosuite = new EvoSuite();
+		//		evosuite.parseCommandLine(command);
+
+		Vector<String> javaCmd = new Vector<String>();
+		javaCmd.add("java");
+		javaCmd.add("-jar");
+		javaCmd.add(TestGenerationAction.getEvoSuiteJar());
+		Collections.addAll(javaCmd, evoSuiteOptions);
+
+		String[] command = javaCmd.toArray(new String[] {});
+		ProcessBuilder builder = new ProcessBuilder(command);
+		builder.directory(new File(baseDir));
+		builder.redirectErrorStream(true);
+
+		Process process = builder.start();
+		InputStream stdout = process.getInputStream();
+		do {
+			readInputStream("EvoSuite process output - ", stdout);
+		} while (!isFinished(process));
+
+	}
+
+	private void readInputStream(String prefix, InputStream in) throws IOException {
+		InputStreamReader is = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(is);
+		String read = br.readLine();
+		while (read != null) {
+			System.out.println(prefix + read);
+			read = br.readLine();
+		}
+	}
+
 	private void syncWithUi(final IResource target) {
 		Display.getDefault().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				GenerationResult result = new GenerationResult(shell, SWT.DIALOG_TRIM
-				        | SWT.APPLICATION_MODAL, target.getProject().getLocation(),
-				        target);
+				GenerationResult result = new GenerationResult(shell,
+						SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL, target
+								.getProject().getLocation(), target);
 				result.open();
 				//MessageDialog.openInformation(shell, "Your Popup ",
 				//                              "Your job has finished.");
@@ -363,8 +451,8 @@ public class TestGenerationJob extends Job {
 		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
 		IWorkbenchPage page = win.getActivePage();
 		try {
-			IDE.openEditor(page,
-			               ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(targetFile));
+			IDE.openEditor(page, ResourcesPlugin.getWorkspace().getRoot()
+					.getFileForLocation(targetFile));
 		} catch (PartInitException e2) {
 			//Put your exception handler here if you wish to.
 			System.out.println("Error: " + e2);
