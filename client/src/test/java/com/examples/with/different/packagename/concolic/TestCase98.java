67986305ff20cac6e7e02aaf8abcbf51ce29324d
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-07-21 12:38:15 +0200

    bugfix VM handling of concrete matrixes

---
+++ client/src/test/java/com/examples/with/different/packagename/concolic/TestCase98.java
@@ -0,0 +1,9 @@
+package com.examples.with.different.packagename.concolic;
+
+public class TestCase98 {
+
+	public static char[][] test() {
+		char[][] matrix = new char[][] { { '7', '8', '9' }, { '0', '1', '2' } };
+		return matrix;
+	}
+}
