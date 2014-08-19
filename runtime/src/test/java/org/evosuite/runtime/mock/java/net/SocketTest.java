edc41fb833800376b1a9d6239f8351b03ae4ee3f
arcuri <none@none>
2014-08-19 16:07:57 +0200

    setting pom for deploy

--- runtime/src/test/java/org/evosuite/runtime/mock/java/net/SocketTest.java
+++ runtime/src/test/java/org/evosuite/runtime/mock/java/net/SocketTest.java
@@ -1,6 +1,11 @@
 package org.evosuite.runtime.mock.java.net;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+import org.junit.Assert;
 
 import org.evosuite.runtime.vnet.VirtualNetwork;
 import org.junit.Before;
@@ -14,7 +19,7 @@ public class SocketTest {
 	}
 	
 	@Test
-	public void testConnect() throws IOException{
+	public void testNullConnect() throws IOException{
 		
 		MockSocket s = new MockSocket();
 		try {
@@ -23,4 +28,22 @@ public class SocketTest {
 			//expected
 		}
 	}
+	
+	@Test
+	public void testConnectNoAnswer() throws IOException{
+		
+		InetAddress addr = InetAddress.getByName("127.42.42.42");
+		SocketAddress saddr = new InetSocketAddress(addr, 12345);
+		MockSocket s = new MockSocket();
+		
+		/* FIXME
+		try{
+			s.connect(saddr);
+			Assert.fail();
+		} catch(IOException e){
+			//expected, as no listener
+		}
+		*/
+	}
+	
 }
