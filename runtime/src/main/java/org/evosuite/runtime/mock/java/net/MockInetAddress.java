9e477277fc87937c3289d5e3e7fe9800f96ab32b
arcuri <none@none>
2014-08-30 22:53:37 +0200

    VNET: handling DNS resolution

--- runtime/src/main/java/org/evosuite/runtime/mock/java/net/MockInetAddress.java
+++ runtime/src/main/java/org/evosuite/runtime/mock/java/net/MockInetAddress.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.List;
 
 import org.evosuite.runtime.mock.StaticReplacementMock;
+import org.evosuite.runtime.vnet.EvoIPAddressUtil;
+import org.evosuite.runtime.vnet.NetworkInterfaceState;
+import org.evosuite.runtime.vnet.VirtualNetwork;
 
 /**
  * We need to mock this class mainly to handle hostnames resolutions,
@@ -18,123 +22,177 @@ public class MockInetAddress implements StaticReplacementMock{
 	public String getMockedClassName() {
 		return InetAddress.class.getName();
 	} 
-	
+
 	//-----  public instance methods -----------------
 	/*
 	 * Note: as we create instances with Inet4AddressUtil,
 	 * here we do not need to mock these methods
 	 */
-	
+
 	public static boolean isMulticastAddress(InetAddress addr){
 		return addr.isMulticastAddress();
 	}
-	
+
 	public static boolean isAnyLocalAddress(InetAddress addr){
 		return addr.isAnyLocalAddress();
 	}
-	
+
 	public static boolean isLoopbackAddress(InetAddress addr){
 		return addr.isLoopbackAddress();
 	}
-	
+
 	public static boolean isLinkLocalAddress(InetAddress addr){
 		return addr.isLinkLocalAddress();
 	}
-	
+
 	public static boolean isSiteLocalAddress(InetAddress addr){
 		return addr.isSiteLocalAddress();
 	}
-	
+
 	public static boolean isMCGlobal(InetAddress addr){
 		return addr.isMCGlobal();
 	}
-	
+
 	public static boolean isMCNodeLocal(InetAddress addr){
 		return addr.isMCNodeLocal();
 	}
-	
+
 	public static boolean isMCLinkLocal(InetAddress addr){
 		return addr.isMCLinkLocal();
 	}
-	
+
 	public static boolean isMCSiteLocal(InetAddress addr){
 		return addr.isMCSiteLocal();
 	}
-	
+
 	public static boolean isMCOrgLocal(InetAddress addr){
 		return addr.isMCOrgLocal();
 	}
-	
-    
-    public static byte[] getAddress(InetAddress addr) {
-    		return addr.getAddress();
-    }
-    
-    public static String getHostAddress(InetAddress addr) {
-    		return addr.getHostAddress();
-     }
 
-    public static int hashCode(InetAddress addr) {
-        return addr.hashCode(); 
-    }
 
-    public static boolean equals(InetAddress addr, Object obj) {
-        return addr.equals(obj); 
-    }
+	public static byte[] getAddress(InetAddress addr) {
+		return addr.getAddress();
+	}
 
-    
-    public static String toString(InetAddress addr) {
-    		return addr.toString(); 
-    }
-    
+	public static String getHostAddress(InetAddress addr) {
+		return addr.getHostAddress();
+	}
+
+	public static int hashCode(InetAddress addr) {
+		return addr.hashCode(); 
+	}
+
+	public static boolean equals(InetAddress addr, Object obj) {
+		return addr.equals(obj); 
+	}
+
+
+	public static String toString(InetAddress addr) {
+		return addr.toString(); 
+	}
+
 
 	// ----- public instance methods depending on virtual network -----
-	
+
 	public static boolean isReachable(InetAddress addr, int timeout) throws IOException{
 		return isReachable(addr, null, 0 , timeout);
 	}
 
 	public static boolean isReachable(InetAddress addr, NetworkInterface netif, int ttl,
-            int timeout) throws IOException {
-    		//TODO
-    		return false;
-    }
-    
-    public static String getHostName(InetAddress addr) {
-    		//TODO
-    		return null;
-    }
-    
-    public static String getCanonicalHostName(InetAddress addr) {
-    		//TODO
-    		return null;
-    }
-    //------ static methods in mocked ---------
-    
-    public static InetAddress getByAddress(String host, byte[] addr)
-            throws UnknownHostException {
-    		return null; //TODO
-    }
-    
-    public static InetAddress getByName(String host){
-    		return null; //TODO 
-    }
+			int timeout) throws IOException {
+		//TODO
+		return false;
+	}
 
-    public static InetAddress[] getAllByName(String host){
-    		return null; //TODO
-    }
-    
-    public static InetAddress getLoopbackAddress() {
-    		return null; //TODO
-    }
-    
-    public static InetAddress getByAddress(byte[] addr)
-    		throws UnknownHostException {
-    		return null; //TODO
-    }
+	public static String getHostName(InetAddress addr) {
+		//TODO
+		return null;
+	}
 
-    public static InetAddress getLocalHost() throws UnknownHostException {
-    		return null; //TODO
-    }
+	public static String getCanonicalHostName(InetAddress addr) {
+		//TODO
+		return null;
+	}
+	//------ static methods in mocked ---------
+
+	public static InetAddress getByAddress(byte[] addr)
+			throws UnknownHostException {
+		return null; //TODO
+	}
+
+	public static InetAddress getByAddress(String host, byte[] addr)
+			throws UnknownHostException {
+		return null; //TODO
+	}
+
+	public static InetAddress getByName(String host)
+			throws UnknownHostException{
+		return getAllByName(host)[0]; 
+	}
+
+	public static InetAddress[] getAllByName(String host)
+			throws UnknownHostException{
+
+		//if no specified host, return loopback
+		if (host == null || host.length() == 0) {
+			InetAddress[] ret = new InetAddress[1];
+			ret[0] = getLoopbackAddress();
+			return ret;
+		}
+
+		// if host is an IPv4 address, we won't do further lookup
+		if (Character.digit(host.charAt(0), 16) != -1) {
+
+			// see if it is IPv4 address
+			byte[] addr = EvoIPAddressUtil.textToNumericFormatV4(host); 
+
+			if(addr != null && addr.length == Inet4AddressUtil.INADDRSZ) {
+				InetAddress[] ret = new InetAddress[1];
+				ret[0] = Inet4AddressUtil.createNewInstance(null, addr); 
+				return ret;
+			}
+		} 
+
+		String resolved = VirtualNetwork.getInstance().dnsResolve(host);
+		if(resolved == null){
+			throw new UnknownHostException("Cannot resolve: "+resolved);
+		}
+		
+		byte[] addr = EvoIPAddressUtil.textToNumericFormatV4(resolved);
+		InetAddress[] ret = new InetAddress[1];
+		ret[0] = Inet4AddressUtil.createNewInstance(host, addr); 
+		return ret;		
+	}
+
+
+	public static InetAddress getLoopbackAddress() {
+
+		List<NetworkInterfaceState> list = 
+				VirtualNetwork.getInstance().getAllNetworkInterfaceStates();
+
+		for(NetworkInterfaceState nis : list){
+			if(!nis.isLoopback()){
+				continue;
+			}
+
+			List<InetAddress> addresses = nis.getLocalAddresses();
+			if(addresses==null || addresses.isEmpty()){
+				continue;
+			}
+
+			return addresses.get(0);
+		}
+
+		return null; //nothing  found
+	}
+
+	public static InetAddress getLocalHost() throws UnknownHostException {
+		/* 
+		 * for simplicity, just return the loopback address.
+		 * TODO: this ll need to be changed if we mock the name
+		 * of the machine
+		 */ 
+		return getLoopbackAddress(); 
+	}
 
 }	
--- runtime/src/main/java/org/evosuite/runtime/vnet/VirtualNetwork.java
+++ runtime/src/main/java/org/evosuite/runtime/vnet/VirtualNetwork.java
@@ -27,7 +27,7 @@ import org.evosuite.runtime.mock.java.net.MockInetAddress;
 public class VirtualNetwork {
 
 	public enum ConnectionType {UDP,TCP};
-	
+
 	/**
 	 * Singleton instance
 	 */
@@ -40,21 +40,21 @@ public class VirtualNetwork {
 	 * overhead/complication
 	 */
 	private static final int START_OF_REMOTE_EPHEMERAL_PORTS = 40000; 
-	
+
 	/**
 	 * Set of listening ports locally opened by the SUTs.
 	 * Eg, when the SUTs work as a server, we keep track
 	 * of which local ports/interfaces they listen to.
 	 */
 	private final Set<EndPointInfo> localListeningPorts;
-	
+
 	/**
 	 * Set of addresses/ports the SUT tried to contact.
 	 */
 	private final Set<EndPointInfo> remoteContactedPorts;
-	
+
 	private final Map<EndPointInfo, Queue<RemoteTcpServer>> remoteCurrentServers;
-	
+
 	/**
 	 * Buffer of incoming connections.
 	 * 
@@ -64,62 +64,67 @@ public class VirtualNetwork {
 	 * Value -> queue of foreign addresses/ports waiting to connect to the given local address (key)
 	 */
 	private final Map<EndPointInfo,Queue<NativeTcp>> incomingConnections;
-	
+
 	/**
 	 * Keep track of all TCP connections opened during the tests.
 	 * This is for example useful to check what data the SUT sent.
 	 */
 	private final Set<NativeTcp> openedTcpConnections;
-	
+
 	/**
 	 * Current remote port number that can be opened
 	 */
 	private final AtomicInteger remotePortIndex;
-	
+
 	/**
 	 * Define with interfaces are available:
 	 * eg, a loopback one and a wifi
 	 */
 	private final List<NetworkInterfaceState> networkInterfaces;
-	
-	
+
+	private DNS dns;
+
 	/**
 	 * private, singleton constructor
 	 */
 	private VirtualNetwork(){
-        localListeningPorts = new CopyOnWriteArraySet<>();
-        incomingConnections = new ConcurrentHashMap<>();
-        openedTcpConnections = new CopyOnWriteArraySet<>();
-        remotePortIndex = new AtomicInteger(START_OF_REMOTE_EPHEMERAL_PORTS);
-        remoteContactedPorts = new CopyOnWriteArraySet<>();
-        remoteCurrentServers = new ConcurrentHashMap<>();
-        networkInterfaces = new CopyOnWriteArrayList<>();
+		localListeningPorts = new CopyOnWriteArraySet<>();
+		incomingConnections = new ConcurrentHashMap<>();
+		openedTcpConnections = new CopyOnWriteArraySet<>();
+		remotePortIndex = new AtomicInteger(START_OF_REMOTE_EPHEMERAL_PORTS);
+		remoteContactedPorts = new CopyOnWriteArraySet<>();
+		remoteCurrentServers = new ConcurrentHashMap<>();
+		networkInterfaces = new CopyOnWriteArrayList<>();
+		dns = new DNS();
 	}
-	
+
 	public static VirtualNetwork getInstance(){
 		return instance;
 	}
-	
+
 	//------------------------------------------
 
-	public void reset(){
+	
+	public void reset(){		
+		dns = new DNS();
+
 		localListeningPorts.clear();
 		incomingConnections.clear();
 		remotePortIndex.set(START_OF_REMOTE_EPHEMERAL_PORTS);
 		remoteCurrentServers.clear();
 		networkInterfaces.clear();
-		
+
 		//TODO most likely it ll need different handling, as needed after the search
 		openedTcpConnections.clear();
 		remoteContactedPorts.clear();
 	}
-	
+
 	public void init(){
 		reset(); //just to be sure
-		
+
 		initNetworkInterfaces();
 	}
-	
+
 	/**
 	 * Create new port to open on remote host
 	 * 
@@ -128,7 +133,7 @@ public class VirtualNetwork {
 	public int getNewRemoteEphemeralPort(){
 		return remotePortIndex.getAndIncrement();
 	}
-	
+
 	/**
 	 * 
 	 * @param name
@@ -142,7 +147,7 @@ public class VirtualNetwork {
 		}
 		return null; 
 	}
-	
+
 	/**
 	 * Get a copy of all available interfaces
 	 * @return
@@ -150,8 +155,16 @@ public class VirtualNetwork {
 	public List<NetworkInterfaceState> getAllNetworkInterfaceStates(){
 		return new ArrayList<NetworkInterfaceState>(networkInterfaces); 
 	}
-	
-	
+
+	/**
+	 * Use mocked DNS to resolve host
+	 * @param host
+	 * @return 
+	 */
+	public String dnsResolve(String host){
+		return dns.resolve(host);
+	}
+
 	/**
 	 * Simulate an incoming connection. The connection is put on a buffer till
 	 * the SUT open a listening port
@@ -164,16 +177,16 @@ public class VirtualNetwork {
 	public synchronized NativeTcp registerIncomingTcpConnection(
 			String originAddr, int originPort,
 			String destAddr, int destPort){
-		
+
 		EndPointInfo origin = new EndPointInfo(originAddr,originPort,ConnectionType.TCP);
 		EndPointInfo dest = new EndPointInfo(destAddr,destPort,ConnectionType.TCP);
-		
+
 		Queue<NativeTcp> queue = incomingConnections.get(dest);
 		if(queue == null){
 			queue = new ConcurrentLinkedQueue<>();
 			incomingConnections.put(dest, queue);
 		}
-		
+
 		NativeTcp connection = new NativeTcp(dest,origin);		
 		queue.add(connection);
 		return connection;
@@ -192,14 +205,14 @@ public class VirtualNetwork {
 		if(queue == null || queue.isEmpty()){
 			return null;
 		}
-		
+
 		NativeTcp connection = queue.poll();
 		openedTcpConnections.add(connection);
-		
+
 		return connection; 
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @param addr
@@ -207,81 +220,86 @@ public class VirtualNetwork {
 	 */
 	public synchronized boolean  openTcpServer(String addr, int port){
 		EndPointInfo info = new EndPointInfo(addr,port,ConnectionType.TCP);
-		
+
 		if(localListeningPorts.contains(info)){
 			//there is already an existing opened port
 			return false;
 		}
-		
+
 		if(! isValidLocalServer(info)){
 			return false;
 		}
-		
+
 		localListeningPorts.add(info);
-		
+
 		return true;
 	}
-	
+
 	public Set<NativeTcp> getViewOfOpenedTcpConnections(){
 		return  Collections.unmodifiableSet(openedTcpConnections);
 	}
-	
+
 	/**
 	 *  Register a remote server that can reply to SUT's connection requests
 	 */
 	public synchronized void addRemoteTcpServer(RemoteTcpServer server){
-		
+
 		Queue<RemoteTcpServer> queue = remoteCurrentServers.get(server.getAddress());
 		if(queue==null){
 			queue = new ConcurrentLinkedQueue<>();
 			remoteCurrentServers.put(server.getAddress(), queue);
 		}
-		
+
 		queue.add(server);
 	}
-	
-	
-	
+
+
+
 	public synchronized NativeTcp connectToRemoteAddress(EndPointInfo localOrigin, EndPointInfo remoteTarget)
-		throws IllegalArgumentException, IOException{
-		
+			throws IllegalArgumentException, IOException{
+
 		if(localOrigin==null || remoteTarget==null){
 			throw new IllegalArgumentException("Null input");
 		}
-		
+
 		if(!isValidLocalServer(localOrigin)){
 			throw new IllegalArgumentException("Invalid local address: "+localOrigin);
 		}
-		
+
 		remoteContactedPorts.add(remoteTarget);
-		
+
 		Queue<RemoteTcpServer> queue = remoteCurrentServers.get(remoteTarget);
 		if(queue==null || queue.isEmpty()){
 			throw new IOException("Remote address/port is not opened: "+remoteTarget);
 		}
-		
+
 		RemoteTcpServer server = queue.poll();
 		NativeTcp connection = server.connect(localOrigin);
 		return connection;
 	}
-	
+
 	//------------------------------------------
 
-	
+
 	private boolean isValidLocalServer(EndPointInfo info){
 		return true; //TODO
 	}
 
 	private void initNetworkInterfaces() {
 
-		NetworkInterfaceState loopback = new NetworkInterfaceState(
-				"Evo_lo0", 1, null, 16384, true, MockInetAddress.getByName("127.0.0.1"));
-		networkInterfaces.add(loopback);
-		
-		NetworkInterfaceState wifi = new NetworkInterfaceState(
-				"Evo_en0", 5, new byte[]{0, 42, 0, 42, 0, 42}, 
-				1500, false, MockInetAddress.getByName("142.42.42.42"));
-		networkInterfaces.add(wifi);
+		try{
+			NetworkInterfaceState loopback = new NetworkInterfaceState(
+					"Evo_lo0", 1, null, 16384, true, MockInetAddress.getByName("127.0.0.1"));
+			networkInterfaces.add(loopback);
+
+			NetworkInterfaceState wifi = new NetworkInterfaceState(
+					"Evo_en0", 5, new byte[]{0, 42, 0, 42, 0, 42}, 
+					1500, false, MockInetAddress.getByName("142.42.42.42"));
+			networkInterfaces.add(wifi);
+		} catch(Exception e){
+			//this should never happen
+			throw new RuntimeException("EvoSuite error: "+e.getMessage());
+		}
 	}
 
 	//------------------------------------------
