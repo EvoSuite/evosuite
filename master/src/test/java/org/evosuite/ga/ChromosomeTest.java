5c83440478b6686c542672b89c8c0f087ccc5e21
Jose Miguel Rojas <jmrs84@gmail.com>
2014-09-23 21:12:09 +0100

    update tests

--- master/src/test/java/org/evosuite/ga/ChromosomeTest.java
+++ master/src/test/java/org/evosuite/ga/ChromosomeTest.java
@@ -3,41 +3,30 @@
  */
 package org.evosuite.ga;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
-import org.evosuite.SystemTest;
 import org.evosuite.Properties.Algorithm;
-import org.evosuite.Properties.Criterion;
-import org.evosuite.Properties.NoSuchParameterException;
-import org.evosuite.TestSuiteGenerator;
 import org.evosuite.coverage.branch.BranchCoverageSuiteFitness;
+import org.evosuite.coverage.exception.ExceptionCoverageSuiteFitness;
+import org.evosuite.coverage.method.MethodTraceCoverageSuiteFitness;
+import org.evosuite.coverage.method.MethodNoExceptionCoverageSuiteFitness;
+import org.evosuite.coverage.output.OutputCoverageSuiteFitness;
 import org.evosuite.coverage.statement.StatementCoverageSuiteFitness;
-import org.evosuite.ga.comparators.SortByFitness;
-import org.evosuite.ga.localsearch.LocalSearchObjective;
-import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
-import org.evosuite.ga.metaheuristics.SteadyStateGA;
-import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testsuite.TestSuiteChromosome;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import com.examples.with.different.packagename.Calculator;
 
 /**
  * @author Jose Miguel Rojas
  *
  */
-public class ChromosomeTest extends SystemTest {
-	 
+public class ChromosomeTest {
+
+	private final double ANY_DOUBLE_1 = 2.0;
+	private final double ANY_DOUBLE_2 = 5.0;
+	private final double ANY_DOUBLE_3 = 6.0;
+	private final double ANY_DOUBLE_4 = 3.0;
+
 	@Test(expected=AssertionError.class)
 	public void testGetFitnessShouldNotWorkForNSGAII() {
 		Properties.ALGORITHM = Algorithm.NSGAII;
@@ -49,11 +38,9 @@ public class ChromosomeTest extends SystemTest {
 		Properties.ALGORITHM = Algorithm.STEADYSTATEGA;
 		Properties.COMPOSITIONAL_FITNESS = false;
         TestSuiteChromosome c = new TestSuiteChromosome();
-        double ANY_DOUBLE = 2.0;
-        double ANY_DOUBLE2 = 5.0;
-        c.addFitness(new StatementCoverageSuiteFitness(), ANY_DOUBLE);
-		c.addFitness(new BranchCoverageSuiteFitness(), ANY_DOUBLE2);
-		assertEquals(ANY_DOUBLE, c.getFitness(), 0.001);
+        c.addFitness(new StatementCoverageSuiteFitness(), ANY_DOUBLE_1);
+		c.addFitness(new BranchCoverageSuiteFitness(), ANY_DOUBLE_2);
+		assertEquals(ANY_DOUBLE_1, c.getFitness(), 0.001);
 	}
 	
 	@Test
@@ -65,7 +52,7 @@ public class ChromosomeTest extends SystemTest {
 	}
 	
 	@Test
-	public void testCompositionalFitnessForNoFunction() {
+	public void testCompositionalGetFitnessForNoFunction() {
 		Properties.ALGORITHM = Algorithm.STEADYSTATEGA;
 		Properties.COMPOSITIONAL_FITNESS = true;
         TestSuiteChromosome c = new TestSuiteChromosome();
@@ -73,27 +60,48 @@ public class ChromosomeTest extends SystemTest {
 	}
 	
 	@Test
-	public void testCompositionalFitnessForOneFunction() {
+	public void testCompositionalGetFitnessForOneFunction() {
 		Properties.ALGORITHM = Algorithm.STEADYSTATEGA;
 		Properties.COMPOSITIONAL_FITNESS = true;
         TestSuiteChromosome c = new TestSuiteChromosome();
         StatementCoverageSuiteFitness f1 = new StatementCoverageSuiteFitness();
         c.addFitness(f1);
-        c.setFitness(f1, 0.2);
-		assertEquals(0.2, c.getFitness(), 0.001);
+        c.setFitness(f1, ANY_DOUBLE_1);
+		assertEquals(ANY_DOUBLE_1, c.getFitness(), 0.001);
 	}
 	
 	@Test
-	public void testCompositionalFitnessForSeveralFunctions() {
+	public void testCompositionalGetFitnessForTwoFunctions() {
 		Properties.ALGORITHM = Algorithm.STEADYSTATEGA;
 		Properties.COMPOSITIONAL_FITNESS = true;
         TestSuiteChromosome c = new TestSuiteChromosome();
         StatementCoverageSuiteFitness f1 = new StatementCoverageSuiteFitness();
         c.addFitness(f1);
-        c.setFitness(f1, 0.2);
+        c.setFitness(f1, ANY_DOUBLE_1);
 		BranchCoverageSuiteFitness f2 = new BranchCoverageSuiteFitness(); 
 		c.addFitness(f2);
-		c.setFitness(f2, 0.4);
-		assertEquals(0.6, c.getFitness(), 0.001);
+		c.setFitness(f2, ANY_DOUBLE_2);
+		assertEquals(ANY_DOUBLE_1 + ANY_DOUBLE_2, c.getFitness(), 0.001);
+	}
+	
+	@Test
+	public void testCompositionalGetFitnessForSeveralFunctions() {
+		Properties.ALGORITHM = Algorithm.STEADYSTATEGA;
+		Properties.COMPOSITIONAL_FITNESS = true;
+        TestSuiteChromosome c = new TestSuiteChromosome();
+        MethodTraceCoverageSuiteFitness f1 = new MethodTraceCoverageSuiteFitness();
+        c.addFitness(f1);
+        c.setFitness(f1, ANY_DOUBLE_1);
+		MethodNoExceptionCoverageSuiteFitness f2 = new MethodNoExceptionCoverageSuiteFitness(); 
+		c.addFitness(f2);
+		c.setFitness(f2, ANY_DOUBLE_2);
+		OutputCoverageSuiteFitness f3 = new OutputCoverageSuiteFitness(); 
+		c.addFitness(f3);
+		c.setFitness(f3, ANY_DOUBLE_3);
+		ExceptionCoverageSuiteFitness f4 = new ExceptionCoverageSuiteFitness(); 
+		c.addFitness(f4);
+		c.setFitness(f4, ANY_DOUBLE_4);
+		double sum = ANY_DOUBLE_1 + ANY_DOUBLE_2 + ANY_DOUBLE_3 + ANY_DOUBLE_4;
+		assertEquals(sum, c.getFitness(), 0.001);
 	}
 }
