3499b2b160a65b04c3e65028b704e5f677395308
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-10-05 23:21:22 +0200

    Option Z3StrSolver

--- client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
+++ client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
@@ -6,6 +6,9 @@ import org.evosuite.symbolic.solver.search.EvoSuiteSolver;
 public class SolverFactory {
 
 	private static final String Z3_SOLVER_CLASS_NAME = "org.evosuite.symbolic.z3.Z3Solver";
+
+	private static final String Z3STR_SOLVER_CLASS_NAME = "org.evosuite.symbolic.z3str.Z3StrSolver";
+
 	private static final SolverFactory instance = new SolverFactory();
 
 	public static SolverFactory getInstance() {
@@ -16,9 +19,26 @@ public class SolverFactory {
 		switch (Properties.DSE_SOLVER) {
 		case Z3_SOLVER:
 			return createZ3Solver();
+		case Z3STR_SOLVER:
+			return createZ3StrSolver();
 		case EVOSUITE_SOLVER:
 		default:
 			return new EvoSuiteSolver();
+		}
+	}
+
+	private Solver createZ3StrSolver() {
+		try {
+			Class<?> clazz = Class.forName(Z3STR_SOLVER_CLASS_NAME);
+			Solver z3Solver = (Solver) clazz.newInstance();
+			return z3Solver;
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Z3 Solver was not found", e);
+		} catch (InstantiationException e) {
+			throw new RuntimeException("Z3 Solver could not be created", e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(
+					"Illegal Access when creating new Z3 Solver", e);
 		}
 	}
 
