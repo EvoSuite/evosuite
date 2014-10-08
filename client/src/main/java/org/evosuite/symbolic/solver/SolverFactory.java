76f22302253d670a51c1bd7906dc90a5acee9476
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-10-08 10:49:25 +0200

    SolverFactory does not use reflection any more

--- client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
+++ client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
@@ -2,12 +2,10 @@ package org.evosuite.symbolic.solver;
 
 import org.evosuite.Properties;
 import org.evosuite.symbolic.solver.search.EvoSuiteSolver;
+import org.evosuite.symbolic.solver.z3.Z3Solver;
+import org.evosuite.symbolic.solver.z3str.Z3StrSolver;
 
 public class SolverFactory {
-
-	private static final String Z3_SOLVER_CLASS_NAME = "org.evosuite.symbolic.z3.Z3Solver";
-
-	private static final String Z3STR_SOLVER_CLASS_NAME = "org.evosuite.symbolic.z3str.Z3StrSolver";
 
 	private static final SolverFactory instance = new SolverFactory();
 
@@ -18,42 +16,12 @@ public class SolverFactory {
 	public Solver buildNewSolver() {
 		switch (Properties.DSE_SOLVER) {
 		case Z3_SOLVER:
-			return createZ3Solver();
+			return new Z3Solver();
 		case Z3_STR_SOLVER:
-			return createZ3StrSolver();
+			return new Z3StrSolver();
 		case EVOSUITE_SOLVER:
 		default:
 			return new EvoSuiteSolver();
-		}
-	}
-
-	private Solver createZ3StrSolver() {
-		try {
-			Class<?> clazz = Class.forName(Z3STR_SOLVER_CLASS_NAME);
-			Solver z3Solver = (Solver) clazz.newInstance();
-			return z3Solver;
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Z3 Solver was not found", e);
-		} catch (InstantiationException e) {
-			throw new RuntimeException("Z3 Solver could not be created", e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(
-					"Illegal Access when creating new Z3 Solver", e);
-		}
-	}
-
-	private static Solver createZ3Solver() {
-		try {
-			Class<?> clazz = Class.forName(Z3_SOLVER_CLASS_NAME);
-			Solver z3Solver = (Solver) clazz.newInstance();
-			return z3Solver;
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Z3 Solver was not found", e);
-		} catch (InstantiationException e) {
-			throw new RuntimeException("Z3 Solver could not be created", e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(
-					"Illegal Access when creating new Z3 Solver", e);
 		}
 	}
 
