08a7000746e08d1d9455cb747b1485739f586a19
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-12-06 02:09:08 +0100

    CVC4 model parser should support parsing Strings

--- client/src/main/java/org/evosuite/symbolic/solver/cvc4/CVC4ModelParser.java
+++ client/src/main/java/org/evosuite/symbolic/solver/cvc4/CVC4ModelParser.java
@@ -19,88 +19,94 @@ class CVC4ModelParser {
 	public Map<String, Object> parse(String cvc4ResultStr) {
 		Map<String, Object> solution = new HashMap<String, Object>();
 
+		String token;
 		StringTokenizer tokenizer = new StringTokenizer(cvc4ResultStr,
-				"() \n\t");
-		tokenizer.nextToken(); // sat
-		tokenizer.nextToken(); // model
+				"() \n\t", true);
+		token = tokenizer.nextToken(); // sat
+		token = tokenizer.nextToken(); // 
+		token = tokenizer.nextToken(); // (
+		token = tokenizer.nextToken(); // model
+		token = tokenizer.nextToken(); // \n
 
 		while (tokenizer.hasMoreTokens()) {
-			String token = tokenizer.nextToken();
+			token = tokenizer.nextToken(); //(
+			if (token.equals(")")) {
+				break;
+			}
+			token = tokenizer.nextToken();
 			if (token.equals("define-fun")) {
+				token = tokenizer.nextToken(); // 
 				String funcName = tokenizer.nextToken();
-				if (funcName.equals(CVC4Solver.STR_LENGTH)) {
+				token = tokenizer.nextToken(); // 
+				token = tokenizer.nextToken(); // (
+				token = tokenizer.nextToken(); // )
+				token = tokenizer.nextToken(); // 
 
-					tokenizer.nextToken(); // x!1
-					tokenizer.nextToken(); // Array
-					tokenizer.nextToken(); // Int
-					tokenizer.nextToken(); // Int
-					tokenizer.nextToken(); // Int
-
-					tokenizer.nextToken(); // value
-				} else {
-					String typeName = tokenizer.nextToken();
-					if (typeName.equals("Int")) {
-						String integerValueStr = tokenizer.nextToken();
-						Long value;
-						if (integerValueStr.equals("-")) {
-							String absoluteIntegerValue = tokenizer.nextToken();
-							value = Long.parseLong("-" + absoluteIntegerValue);
-						} else {
-							value = Long.parseLong(integerValueStr);
-						}
-						solution.put(funcName, value);
-					} else if (typeName.equals("Real")) {
-						String realValueStr = tokenizer.nextToken();
-						Double value;
-						if (realValueStr.equals("-")) {
-							String absoluteValueStr = tokenizer.nextToken();
-							if (absoluteValueStr.equals("/")) {
-								String numeratorStr = tokenizer.nextToken();
-								String denominatorStr = tokenizer.nextToken();
-
-								double numerator = Double
-										.parseDouble(numeratorStr);
-								double denominator = Double
-										.parseDouble(denominatorStr);
-
-								value = -(numerator / denominator);
-							} else {
-								value = Double.parseDouble("-"
-										+ absoluteValueStr);
-							}
-						} else {
-
-							if (realValueStr.equals("/")) {
-								String numeratorStr = tokenizer.nextToken();
-								String denominatorStr = tokenizer.nextToken();
-
-								double numerator = Double
-										.parseDouble(numeratorStr);
-								double denominator = Double
-										.parseDouble(denominatorStr);
-
-								value = (numerator / denominator);
-							} else {
-
-								value = Double.parseDouble(realValueStr);
-							}
-						}
-						solution.put(funcName, value);
-					} else if (typeName.equals("String")) {
-						String stringValue = tokenizer.nextToken();
-						String stringNoQuotes = stringValue.substring(1,
-								stringValue.length() - 1);
-						solution.put(funcName, stringNoQuotes);
-					} else if (typeName.equals("x!1")) {
-
+				String typeName = tokenizer.nextToken();
+				if (typeName.equals("Int")) {
+					String integerValueStr = tokenizer.nextToken();
+					Long value;
+					if (integerValueStr.equals("-")) {
+						String absoluteIntegerValue = tokenizer.nextToken();
+						value = Long.parseLong("-" + absoluteIntegerValue);
 					} else {
-						//						throw new IllegalArgumentException(
-						//								"Must implement this production");
+						value = Long.parseLong(integerValueStr);
 					}
+					solution.put(funcName, value);
+				} else if (typeName.equals("Real")) {
+					String realValueStr = tokenizer.nextToken();
+					Double value;
+					if (realValueStr.equals("-")) {
+						String absoluteValueStr = tokenizer.nextToken();
+						if (absoluteValueStr.equals("/")) {
+							String numeratorStr = tokenizer.nextToken();
+							String denominatorStr = tokenizer.nextToken();
+
+							double numerator = Double.parseDouble(numeratorStr);
+							double denominator = Double
+									.parseDouble(denominatorStr);
+
+							value = -(numerator / denominator);
+						} else {
+							value = Double.parseDouble("-" + absoluteValueStr);
+						}
+					} else {
+
+						if (realValueStr.equals("/")) {
+							String numeratorStr = tokenizer.nextToken();
+							String denominatorStr = tokenizer.nextToken();
+
+							double numerator = Double.parseDouble(numeratorStr);
+							double denominator = Double
+									.parseDouble(denominatorStr);
+
+							value = (numerator / denominator);
+						} else {
+
+							value = Double.parseDouble(realValueStr);
+						}
+					}
+					solution.put(funcName, value);
+				} else if (typeName.equals("String")) {
+					token = tokenizer.nextToken();
+					StringBuffer value = new StringBuffer();
+
+					String stringToken;
+					do {
+						stringToken = tokenizer.nextToken();
+						value.append(stringToken);
+					} while (!stringToken.endsWith("\""));
+
+					String stringWithQuotes = value.toString();
+					String stringWithoutQuotes = stringWithQuotes.substring(1,
+							stringWithQuotes.length() - 1);
+					solution.put(funcName, stringWithoutQuotes);
+					token = tokenizer.nextToken(); // )
+					token = tokenizer.nextToken(); // \n
+				} else {
+					//						throw new IllegalArgumentException(
+					//								"Must implement this production");
 				}
-			} else {
-				//				throw new IllegalArgumentException(
-				//						"Must implement this production");
 			}
 		}
 
