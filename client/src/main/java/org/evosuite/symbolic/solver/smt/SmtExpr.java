1398157284287f85eb2a3932d6170a6f36b147f2
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-12-09 17:30:05 +0100

    SMT expressions are no longer strings but ASTs

---
+++ client/src/main/java/org/evosuite/symbolic/solver/smt/SmtExpr.java
@@ -0,0 +1,7 @@
+package org.evosuite.symbolic.solver.smt;
+
+public abstract class SmtExpr {
+
+	public abstract <K, V> K accept(SmtExprVisitor<K, V> v, V arg);
+
+}
---
+++ client/src/main/java/org/evosuite/symbolic/solver/smt/SmtVariable.java
@@ -0,0 +1,39 @@
+package org.evosuite.symbolic.solver.smt;
+
+public abstract class SmtVariable extends SmtExpr {
+
+	protected final String varName;
+
+	public SmtVariable(String varName) {
+		this.varName = varName;
+	}
+
+	public String getName() {
+		return varName;
+	}
+
+	@Override
+	public final int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((varName == null) ? 0 : varName.hashCode());
+		return result;
+	}
+
+	@Override
+	public final boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SmtVariable other = (SmtVariable) obj;
+		if (varName == null) {
+			if (other.varName != null)
+				return false;
+		} else if (!varName.equals(other.varName))
+			return false;
+		return true;
+	}
+}
