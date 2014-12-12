4c40e2565dec368f4414f7fd71c82ad2d228abfa
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-12-12 17:32:10 +0100

    handling more cases of String.valueOf()

--- client/src/main/java/org/evosuite/symbolic/vm/string/ValueOf.java
+++ client/src/main/java/org/evosuite/symbolic/vm/string/ValueOf.java
@@ -1,5 +1,7 @@
 package org.evosuite.symbolic.vm.string;
 
+import org.evosuite.symbolic.expr.bv.IntegerValue;
+import org.evosuite.symbolic.expr.str.IntegerToStringCast;
 import org.evosuite.symbolic.expr.str.StringValue;
 import org.evosuite.symbolic.vm.NonNullReference;
 import org.evosuite.symbolic.vm.Reference;
@@ -7,14 +9,48 @@ import org.evosuite.symbolic.vm.SymbolicEnvironment;
 import org.evosuite.symbolic.vm.SymbolicFunction;
 import org.evosuite.symbolic.vm.SymbolicHeap;
 
-public abstract class ValueOf {
+public abstract class ValueOf extends SymbolicFunction {
+
 	private static final String VALUE_OF = "valueOf";
 
-	public static final class ValueOf_O extends SymbolicFunction {
+	public static abstract class ValueOf_Int extends ValueOf {
+
+		public ValueOf_Int(SymbolicEnvironment env, String desc) {
+			super(env, desc);
+		}
+
+		@Override
+		public final Object executeFunction() {
+
+			IntegerValue symb_arg = this.getSymbIntegerArgument(0);
+
+			Reference symb_ret_val = this.getSymbRetVal();
+			String conc_ret_val = (String) this.getConcRetVal();
+
+			if (symb_arg.containsSymbolicVariable()) {
+				StringValue symbExpr = new IntegerToStringCast(symb_arg,
+						conc_ret_val);
+
+				NonNullReference symb_non_null_ret_val = (NonNullReference) symb_ret_val;
+
+				env.heap.putField(Types.JAVA_LANG_STRING,
+						SymbolicHeap.$STRING_VALUE, conc_ret_val,
+						symb_non_null_ret_val, symbExpr);
+			}
+			return this.getSymbRetVal();
+
+		}
+
+	}
+
+	public ValueOf(SymbolicEnvironment env, String desc) {
+		super(env, Types.JAVA_LANG_STRING, VALUE_OF, desc);
+	}
+
+	public static final class ValueOf_O extends ValueOf {
 
 		public ValueOf_O(SymbolicEnvironment env) {
-			super(env, Types.JAVA_LANG_STRING, VALUE_OF,
-					Types.OBJECT_TO_STR_DESCRIPTOR);
+			super(env, Types.OBJECT_TO_STR_DESCRIPTOR);
 		}
 
 		@Override
@@ -46,4 +82,33 @@ public abstract class ValueOf {
 		}
 
 	}
+
+	public static final class ValueOf_J extends ValueOf_Int {
+
+		public ValueOf_J(SymbolicEnvironment env) {
+			super(env, Types.LONG_TO_STR_DESCRIPTOR);
+		}
+	}
+
+	public static final class ValueOf_I extends ValueOf_Int {
+
+		public ValueOf_I(SymbolicEnvironment env) {
+			super(env, Types.INT_TO_STR_DESCRIPTOR);
+		}
+	}
+
+	public static final class ValueOf_C extends ValueOf_Int {
+
+		public ValueOf_C(SymbolicEnvironment env) {
+			super(env, Types.CHAR_TO_STR_DESCRIPTOR);
+		}
+	}
+
+	public static final class ValueOf_B extends ValueOf_Int {
+
+		public ValueOf_B(SymbolicEnvironment env) {
+			super(env, Types.BOOLEAN_TO_STR_DESCRIPTOR);
+		}
+	}
+
 }
