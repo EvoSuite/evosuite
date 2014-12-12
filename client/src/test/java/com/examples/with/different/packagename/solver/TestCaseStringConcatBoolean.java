f84ef335c9e5a2de57b5c311f16abb1611131cf4
Juan Galeotti <galeotti@cs.uni-saarland.de>
2014-12-12 15:44:44 +0100

    append char and append boolean

---
+++ client/src/test/java/com/examples/with/different/packagename/solver/TestCaseStringConcatBoolean.java
@@ -0,0 +1,12 @@
+package com.examples.with.different.packagename.solver;
+
+public class TestCaseStringConcatBoolean {
+
+	public static boolean test(boolean boolean0) {
+		String concat1 = "" + boolean0;
+		if (concat1.equals("true")) {
+			return true;
+		}
+		return true;
+	}
+}
