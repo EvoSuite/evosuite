59108f9a2208122b8630c0b8f207a51d6f31cacf
arcuri <none@none>
2014-12-26 11:38:43 +0100

    added a system test for multi coverage criteria

---
+++ master/src/test/java/com/examples/with/different/packagename/coverage/BooleanOneLine.java
@@ -0,0 +1,11 @@
+package com.examples.with.different.packagename.coverage;
+
+/**
+ * Created by arcuri on 12/24/14.
+ */
+public class BooleanOneLine {
+
+    public boolean check(String s){
+        return s.equals("42");
+    }
+}
