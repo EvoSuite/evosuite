ff5afaa4ff38934db2a383d57d2872e9c3496ec8
arcuri <none@none>
2015-01-16 16:25:59 +0100

    improved handling of URLs

--- master/src/test/java/org/evosuite/mock/java/net/MockUrlSystemTest.java
+++ master/src/test/java/org/evosuite/mock/java/net/MockUrlSystemTest.java
@@ -1,6 +1,7 @@
 package org.evosuite.mock.java.net;
 
 import com.examples.with.different.packagename.agent.GetURL;
+import com.examples.with.different.packagename.mock.java.net.ReadFromInputURL;
 import com.examples.with.different.packagename.mock.java.net.ReadFromURL;
 import com.examples.with.different.packagename.mock.java.net.ReceiveTcp;
 import org.evosuite.EvoSuite;
@@ -48,5 +49,30 @@ public class MockUrlSystemTest extends SystemTest{
         Assert.assertEquals("Wrong number of goals: ", 3, goals);
         Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
     }
+
+    @Test
+    public void testUrlAsInput(){
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = ReadFromInputURL.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.SEARCH_BUDGET = 20000;
+        Properties.VIRTUAL_NET = true;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        Assert.assertTrue(result != null);
+
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        int goals = TestSuiteGenerator.getFitnessFactory().get(0).getCoverageGoals().size(); // assuming single fitness function
+        Assert.assertEquals("Wrong number of goals: ", 5, goals);
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+
 
 }
