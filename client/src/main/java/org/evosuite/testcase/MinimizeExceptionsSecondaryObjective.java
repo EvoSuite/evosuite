b782a138b0e65a2a12ec2b8e6ca5991511e15c3b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-01-22 16:29:41 +0000

    Generic type added

--- client/src/main/java/org/evosuite/testcase/MinimizeExceptionsSecondaryObjective.java
+++ client/src/main/java/org/evosuite/testcase/MinimizeExceptionsSecondaryObjective.java
@@ -20,7 +20,6 @@
  */
 package org.evosuite.testcase;
 
-import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.SecondaryObjective;
 import org.evosuite.testcase.execution.ExecutionResult;
 
@@ -29,12 +28,12 @@ import org.evosuite.testcase.execution.ExecutionResult;
  *
  * @author Gordon Fraser
  */
-public class MinimizeExceptionsSecondaryObjective extends SecondaryObjective {
+public class MinimizeExceptionsSecondaryObjective extends SecondaryObjective<TestChromosome> {
 
 	private static final long serialVersionUID = -4405276303273532040L;
 
-	private int getNumExceptions(Chromosome chromosome) {
-		ExecutionResult result = ((ExecutableChromosome) chromosome).getLastExecutionResult();
+	private int getNumExceptions(TestChromosome chromosome) {
+		ExecutionResult result = chromosome.getLastExecutionResult();
 		if (result != null)
 			return result.getNumberOfThrownExceptions();
 		else
@@ -46,7 +45,7 @@ public class MinimizeExceptionsSecondaryObjective extends SecondaryObjective {
 	 */
 	/** {@inheritDoc} */
 	@Override
-	public int compareChromosomes(Chromosome chromosome1, Chromosome chromosome2) {
+	public int compareChromosomes(TestChromosome chromosome1, TestChromosome chromosome2) {
 		return getNumExceptions(chromosome1) - getNumExceptions(chromosome2);
 	}
 
@@ -55,8 +54,8 @@ public class MinimizeExceptionsSecondaryObjective extends SecondaryObjective {
 	 */
 	/** {@inheritDoc} */
 	@Override
-	public int compareGenerations(Chromosome parent1, Chromosome parent2,
-	        Chromosome child1, Chromosome child2) {
+	public int compareGenerations(TestChromosome parent1, TestChromosome parent2,
+			TestChromosome child1, TestChromosome child2) {
 		return Math.min(getNumExceptions(parent1), getNumExceptions(parent2))
 		        - Math.min(getNumExceptions(child1), getNumExceptions(child2));
 	}
