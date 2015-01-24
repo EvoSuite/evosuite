7d8ee7dc2c7651e643160c7e280d184dfecdb101
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-01-24 20:51:48 +0000

    Fix for resourcelist: anonymous classes have numbers, $ can also be a member class

--- client/src/main/java/org/evosuite/classpath/ResourceList.java
+++ client/src/main/java/org/evosuite/classpath/ResourceList.java
@@ -239,7 +239,7 @@ public class ResourceList {
 				if(!className.startsWith(prefix)){
 					continue;
 				}
-				if(!includeAnonymousClasses && className.contains("$")){
+				if(!includeAnonymousClasses && className.matches(".*\\$\\d+$")) {
 					continue;
 				}
 
