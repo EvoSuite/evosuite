36b9ab0c5e5750018069df6bb56595e2454f0499
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-02-06 21:34:22 +0000

    Remove unused imports

--- client/src/main/java/org/evosuite/coverage/line/LineCoverageFactory.java
+++ client/src/main/java/org/evosuite/coverage/line/LineCoverageFactory.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.evosuite.Properties;
 import org.evosuite.graphs.cfg.BytecodeInstruction;
 import org.evosuite.instrumentation.LinePool;
 import org.evosuite.testsuite.AbstractFitnessFactory;
