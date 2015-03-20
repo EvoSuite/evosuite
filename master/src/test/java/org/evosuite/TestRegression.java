982ee0d2243b601d5b19bfaf6ef8cd1ca792e67c
Andrea Arcuri <arcuri82@gmail.com>
2015-03-20 11:11:19 +0100

    fix for failing test

--- master/src/test/java/org/evosuite/TestRegression.java
+++ master/src/test/java/org/evosuite/TestRegression.java
@@ -88,13 +88,19 @@ public class TestRegression extends SystemTest {
 		testCovered(CallTest.class.getCanonicalName(), 3);
 	}
 
-	@Test
-	public void testDependency() {
-		testCovered(DepTest.class.getCanonicalName(), 2);
-		// TODO: Re-run with deprecated active
-	}
+    @Test
+    public void testDependency_noDeprecated() {
+        Properties.USE_DEPRECATED = false;
+        testCovered(DepTest.class.getCanonicalName(), 2);
+    }
 
-	@Test
+    @Test
+    public void testDependency_withDeprecated() {
+        Properties.USE_DEPRECATED = true;
+        testCovered(DepTest.class.getCanonicalName(), 3);
+    }
+
+    @Test
 	public void testEmpty() {
 		testCovered(EmptyTest.class.getCanonicalName(), 2);
 	}
