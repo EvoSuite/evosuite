445bf57510ab6daaa57db66d2a11b5c050f4c571
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-05-02 17:42:22 +0100

    Fix private method reflection statements

--- client/src/main/java/org/evosuite/testcase/statements/reflection/PrivateFieldStatement.java
+++ client/src/main/java/org/evosuite/testcase/statements/reflection/PrivateFieldStatement.java
@@ -1,19 +1,19 @@
 package org.evosuite.testcase.statements.reflection;
 
+import org.evosuite.ga.ConstructionFailedException;
 import org.evosuite.runtime.PrivateAccess;
 import org.evosuite.testcase.TestFactory;
-import org.evosuite.testcase.statements.ClassPrimitiveStatement;
 import org.evosuite.testcase.variable.ConstantValue;
-import org.evosuite.testcase.variable.FieldReference;
 import org.evosuite.testcase.TestCase;
 import org.evosuite.testcase.variable.VariableReference;
 import org.evosuite.testcase.statements.MethodStatement;
 import org.evosuite.utils.GenericClass;
-import org.evosuite.utils.GenericField;
 import org.evosuite.utils.GenericMethod;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Statement representing the setting of a private field, which is done through reflection in the
@@ -23,7 +23,9 @@ import java.util.Arrays;
  */
 public class PrivateFieldStatement extends MethodStatement{
 
-    private static Method setVariable;
+	private static final long serialVersionUID = 5152490398872348493L;
+
+	private static Method setVariable;
 
     static {
         try {
@@ -36,19 +38,22 @@ public class PrivateFieldStatement extends MethodStatement{
     }
 
     public PrivateFieldStatement(TestCase tc, Class<?> klass , String fieldName, VariableReference callee, VariableReference param)
-            throws NoSuchFieldException {
+            throws NoSuchFieldException, IllegalArgumentException, ConstructionFailedException {
         super(
                 tc,
                 new GenericMethod(setVariable,PrivateAccess.class),
                 null, //it is static
                 Arrays.asList(  // setVariable(Class<T> klass, T instance, String fieldName, Object value)
-                        new ConstantValue(tc,new GenericClass(Class.class),klass.getName()+".class"),  // Class<T> klass
+                        new ConstantValue(tc,new GenericClass(Class.class),klass),  // Class<T> klass
                         //new ClassPrimitiveStatement(tc,klass).getReturnValue(),  // Class<T> klass
                         callee, // T instance
                         new ConstantValue(tc,new GenericClass(String.class),fieldName),  // String fieldName
                         param // Object value
                 )
         );
+        List<GenericClass> parameterTypes = new ArrayList<GenericClass>();
+        parameterTypes.add(new GenericClass(klass));
+        this.method.setTypeParameters(parameterTypes);
     }
 
     @Override
