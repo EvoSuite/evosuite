e4fc8fecb029409df97f2ecd0ca8e690d7879bfd
Andrea Arcuri <arcuri82@gmail.com>
2015-05-20 11:50:18 +0200

    improved html

--- release_results/script/generateHtml.R
+++ release_results/script/generateHtml.R
@@ -49,6 +49,13 @@ html <- function(zipFile){
 	version = gsub("_",".",VERSION)
 	budget = unique(dt$search_budget) / 60
 
+	line = formatC(100 * mean(dt$LineCoverage),digits=1,format="f")
+	branch = formatC(100 * mean(dt$BranchCoverage),digits=1,format="f")
+	mutation = formatC(100 * mean(dt$WeakMutationScore),digits=1,format="f")
+	output = formatC(100 * mean(dt$OutputCoverage),digits=1,format="f")
+	exception = formatC(mean(dt$Implicit_MethodExceptions),digits=1,format="f")
+	tests = formatC(mean(dt$Size),digits=1,format="f")
+
 	PAGE = paste(GENERATED_FILES,"/results.html",sep="")
 	unlink(PAGE)
 	sink(PAGE, append=TRUE, split=FALSE)
@@ -70,6 +77,15 @@ html <- function(zipFile){
 	cat("			<img src='",FIGURE_CLASSES,"' alt='Coverage per class' />\n", sep="")
 	cat("			<img src='",FIGURE_PROJECTS,"' alt='Coverage per project' /> \n", sep="")
 	cat("		</div> \n")
+	cat("		<p>Average values for the different testing results: </p> \n")
+	cat("		<ul> \n")
+	cat("		<li>","Line:       ",line,"%</li> \n",sep="")
+	cat("		<li>","Branch:     ",branch,"%</li> \n",sep="")
+	cat("		<li>","Mutation:   ",mutation,"%</li> \n",sep="")
+	cat("		<li>","Output:     ",output,"%</li> \n",sep="")
+	cat("		<li>","Exceptions: ",exception,"</li> \n",sep="")
+	cat("		<li>","# of Tests: ",tests,"</li> \n",sep="")
+	cat("		</ul> \n")
 	cat("		<p>In detail results: </p> \n")
 	table(dt)
 	cat("	</body>\n")
@@ -95,7 +111,7 @@ table <- function(dt){
 		cat("					<div data-role='collapsible'>\n", sep="")
 		cat("						<h4> ",projName," (",length(projClasses)," classes): average ",projCov,"% line coverage</h4>\n", sep="")
 		cat("						<table data-role='table'  class='ui-responsive table-stripe'> \n")
-		cat("							<thead><tr><th>Class Name</th><th>Line</th><th>Branch</th><th>Mutation</th><th>Output</th><th>Exception</th><th># Tests</th></tr></thead> \n")
+		cat("							<thead><tr><th>Class Name</th><th>Line</th><th>Branch</th><th>Mutation</th><th>Output</th><th>Exceptions</th><th># Tests</th></tr></thead> \n")
 		cat("							<tbody> \n")
 		for(class in projClasses){
 			line = formatC(100 * mean(dt$LineCoverage[dt$group_id==p & dt$TARGET_CLASS==class]),digits=1,format="f")
