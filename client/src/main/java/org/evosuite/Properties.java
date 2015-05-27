b37da56c579ec8b1bf4ebac5838c707197fd1368
Sina Shamshiri <sina6002@gmail.com>
2015-05-27 13:22:10 +0100

    reverting statistics

--- client/src/main/java/org/evosuite/Properties.java
+++ client/src/main/java/org/evosuite/Properties.java
@@ -936,7 +936,7 @@ public class Properties {
 	public static boolean SERIALIZE_RESULT = false;
 
 	@Parameter(key = "new_statistics", group = "Output", description = "Use the new statistics backend on the master")
-	public static boolean NEW_STATISTICS = false;
+	public static boolean NEW_STATISTICS = true;
 
 	//@Parameter(key = "old_statistics", group = "Output", description = "Use the old statistics backend on the master")
 	//public static boolean OLD_STATISTICS = false;
