e2f3da8c593fb8d2cb034f73880d42eec9a57569
Andrea Arcuri <arcuri82@gmail.com>
2015-06-01 11:58:56 +0200

    trying with Spring. seems to work, but problem with Delete of tables

--- runtime/src/main/java/org/evosuite/runtime/javaee/db/DBManager.java
+++ runtime/src/main/java/org/evosuite/runtime/javaee/db/DBManager.java
@@ -3,15 +3,28 @@ package org.evosuite.runtime.javaee.db;
 import org.hibernate.internal.SessionImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -44,7 +57,11 @@ public class DBManager {
         //     Maybe not needed during test generation, but likely in runtime when JUnit are run in isolation
         //TODO wrapper for EntityManagerFactory: goal of keeping track of what is called on EntityManager.
         //TODO Also keep track of created managers
-        factory = Persistence.createEntityManagerFactory(EVOSUITE_DB);
+
+        //factory = Persistence.createEntityManagerFactory(EVOSUITE_DB);
+        factory = createEMFWithSpring();
+
+
         em = factory.createEntityManager();
     }
 
@@ -78,7 +95,9 @@ public class DBManager {
             }
             rs.close();
             for (String table : tables) {
-                s.executeUpdate("DELETE FROM " + table);
+                String delete = "DELETE FROM " + table;
+                s.executeUpdate(delete);
+                logger.debug("SQL executed: "+delete);
             }
             s.execute("SET DATABASE REFERENTIAL INTEGRITY TRUE");
             s.close();
@@ -99,5 +118,88 @@ public class DBManager {
             em.getTransaction().rollback();
         }
         clearDatabase();
+    }
+
+
+    private EntityManagerFactory createEMFWithSpring(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem:.");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("**.*"); //search everything on classpath
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.setProperty("hibernate.connection.shutdown", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        em.setJpaProperties(properties);
+
+        em.afterPropertiesSet();
+
+        return em.getObject();
+    }
+
+
+    @Configuration
+    @EnableTransactionManagement
+    private class StandaloneDataJpaConfig {
+
+        @Bean
+        public DataSource dataSource() {
+
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+            dataSource.setUrl("jdbc:hsqldb:mem:.");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("");
+            return dataSource;
+            /*
+            return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
+                    .addScript("classpath:sql/schema.sql")
+                    .addScript("classpath:sql/import-users.sql")
+                    .build();
+                    */
+        }
+
+        @Bean
+        public PlatformTransactionManager transactionManager() {
+
+            JpaTransactionManager txManager = new JpaTransactionManager();
+            txManager.setEntityManagerFactory(entityManagerFactory());
+            return txManager;
+        }
+
+        @Bean
+        public HibernateExceptionTranslator hibernateExceptionTranslator() {
+            return new HibernateExceptionTranslator();
+        }
+
+        @Bean
+        public EntityManagerFactory entityManagerFactory() {
+
+            // will set the provider to 'org.hibernate.ejb.HibernatePersistence'
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            // will set hibernate.show_sql to 'true'
+            vendorAdapter.setShowSql(true);
+            // if set to true, will set hibernate.hbm2ddl.auto to 'update'
+            vendorAdapter.setGenerateDdl(false);
+
+            LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+            factory.setJpaVendorAdapter(vendorAdapter);
+            factory.setPackagesToScan("com.geowarin.standalonedatajpa.model");
+            factory.setDataSource(dataSource());
+
+            // This will trigger the creation of the entity manager factory
+            factory.afterPropertiesSet();
+
+            return factory.getObject();
+        }
     }
 }
