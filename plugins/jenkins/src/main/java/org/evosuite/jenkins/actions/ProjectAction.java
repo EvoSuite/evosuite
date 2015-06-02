f2b81096286dac3e56e1c5f80037770ee7d9bf3e
Jose Campos <jose.campos@sheffield.ac.uk>
2015-06-02 18:47:35 +0100

    improve build report of Jenkins plugin

--- plugins/jenkins/src/main/java/org/evosuite/jenkins/actions/ProjectAction.java
+++ plugins/jenkins/src/main/java/org/evosuite/jenkins/actions/ProjectAction.java
@@ -12,10 +12,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.evosuite.jenkins.plot.CoveragePlot;
 import org.kohsuke.stapler.StaplerRequest;
@@ -24,7 +28,7 @@ import org.kohsuke.stapler.StaplerResponse;
 public class ProjectAction implements Action {
 
 	private AbstractProject<?, ?> project;
-	private List<ModuleAction> modules = new ArrayList<ModuleAction>();
+	private Map<String, ModuleAction> modules = new LinkedHashMap<String, ModuleAction>();
 
 	public ProjectAction(AbstractProject<?, ?> project) {
 		this.project = (AbstractProject<?, ?>) project;
@@ -38,7 +42,7 @@ public class ProjectAction implements Action {
 					+ ".evosuite" + File.separator + "project_info.xml");
 
 			if (Files.exists(path)) {
-				this.modules.add(new ModuleAction(module.getName(), path));
+				this.modules.put(module.getName(), new ModuleAction(module.getName(), path));
 			}
 		}
 	}
@@ -47,7 +51,7 @@ public class ProjectAction implements Action {
 		return this.project;
 	}
 
-	public List<ModuleAction> getModules() {
+	public Map<String, ModuleAction> getModules() {
 		return this.modules;
 	}
 
@@ -67,7 +71,7 @@ public class ProjectAction implements Action {
 		}
 
 		double coverage = 0.0;
-		for (ModuleAction module : this.modules) {
+		for (ModuleAction module : this.modules.values()) {
 			coverage += module.getOverallCoverage();
 		}
 
@@ -77,7 +81,7 @@ public class ProjectAction implements Action {
 	public Map<String, List<Double>> getCoverageValues() {
 		Map<String, List<Double>> coverageValues = new LinkedHashMap<String, List<Double>>();
 
-		for (ModuleAction module : this.modules) {
+		for (ModuleAction module : this.modules.values()) {
 			for (String criterion : module.getCoverageValues().keySet()) {
 				List<Double> coverages = new ArrayList<Double>();
 				if (coverageValues.containsKey(criterion)) {
@@ -119,10 +123,42 @@ public class ProjectAction implements Action {
 		}
 
 		int count = 0;
-		for (ModuleAction module : this.modules) {
+		for (ModuleAction module : this.modules.values()) {
 			count += module.getNumberOfTestableClasses();
 		}
 
 		return count;
+	}
+
+	public Map<String, String> getCriterion() {
+		if (this.modules.isEmpty()) {
+			return null;
+		}
+
+		Set<String> names = new LinkedHashSet<String>();
+		for (ModuleAction module : this.modules.values()) {
+			names.addAll(module.getCriterion());
+		}
+
+		Map<String, String> coverageValues = new LinkedHashMap<String, String>();
+		for (String criterionName : names) {
+			NumberFormat formatter = new DecimalFormat("#0.00");
+			coverageValues.put(criterionName, formatter.format(this.getCriterionCoverage(criterionName)));
+		}
+
+		return coverageValues;
+	}
+
+	private double getCriterionCoverage(String criterion) {
+		if (this.modules.isEmpty()) {
+			return 0.0;
+		}
+
+		double coverage = 0.0;
+		for (ModuleAction module : this.modules.values()) {
+			coverage += module.getCriterionCoverage(criterion);
+		}
+
+		return coverage / this.modules.size();
 	}
 }
