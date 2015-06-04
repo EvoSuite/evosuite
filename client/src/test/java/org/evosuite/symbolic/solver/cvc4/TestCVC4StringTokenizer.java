8733d09e96d780c4b07e088da4b0b893423b1bb9
Juan Galeotti <galeotti@cs.uni-saarland.de>
2015-06-04 12:20:36 -0300

    test cases cvc4

---
+++ client/src/test/java/org/evosuite/symbolic/solver/cvc4/TestCVC4StringTokenizer.java
@@ -0,0 +1,41 @@
+package org.evosuite.symbolic.solver.cvc4;
+
+import org.evosuite.Properties;
+import org.evosuite.symbolic.solver.ConstraintSolverTimeoutException;
+import org.evosuite.symbolic.solver.TestSolverTokenizer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCVC4StringTokenizer {
+
+	private static final String DEFAULT_CVC4_PATH = Properties.CVC4_PATH;
+
+	@BeforeClass
+	public static void configureCVC4Path() {
+		String cvc4Path = System.getenv("cvc4_path");
+		if (cvc4Path != null) {
+			Properties.CVC4_PATH = cvc4Path;
+		}
+	}
+
+	@AfterClass
+	public static void restoreCVC4Path() {
+		Properties.CVC4_PATH = DEFAULT_CVC4_PATH;
+	}
+
+	@Test
+	public void testStringTokenizer() throws SecurityException,
+			NoSuchMethodException, ConstraintSolverTimeoutException {
+
+		if (Properties.CVC4_PATH == null) {
+			System.out
+					.println("Warning: cvc4_path should be configured to execute this test case");
+			return;
+		}
+
+		CVC4Solver solver = new CVC4Solver();
+		TestSolverTokenizer.testStringTokenizer(solver);
+	}
+
+}
