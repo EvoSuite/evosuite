19126802ed5e50294095e05cc733503cdd919831
Juan Galeotti <galeotti@cs.uni-saarland.de>
2015-06-05 15:37:15 -0300

    refactor z3str -> z3str2

--- client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
+++ client/src/main/java/org/evosuite/symbolic/solver/SolverFactory.java
@@ -4,7 +4,7 @@ import org.evosuite.Properties;
 import org.evosuite.symbolic.solver.cvc4.CVC4Solver;
 import org.evosuite.symbolic.solver.search.EvoSuiteSolver;
 import org.evosuite.symbolic.solver.z3.Z3Solver;
-import org.evosuite.symbolic.solver.z3str.Z3StrSolver;
+import org.evosuite.symbolic.solver.z3str2.Z3Str2Solver;
 
 public class SolverFactory {
 
@@ -18,8 +18,8 @@ public class SolverFactory {
 		switch (Properties.DSE_SOLVER) {
 		case Z3_SOLVER:
 			return new Z3Solver();
-		case Z3_STR_SOLVER:
-			return new Z3StrSolver();
+		case Z3_STR2_SOLVER:
+			return new Z3Str2Solver();
 		case CVC4_SOLVER:
 			return new CVC4Solver();
 		case EVOSUITE_SOLVER:
