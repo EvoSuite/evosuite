9d2b9091de918dbb541b5cbb73e2bba2129c3ee8
Andrea Arcuri <arcuri82@gmail.com>
2015-06-09 11:11:00 +0200

    - hopefully (need more testing) fixed showstopper in CTG - seeding from last run is now always done regardless of schedule strategy, as test retention mechanism was  seriously flawed

---
+++ client/src/main/java/org/evosuite/utils/Inputs.java
@@ -0,0 +1,24 @@
+package org.evosuite.utils;
+
+/**
+ * Created by Andrea Arcuri on 09/06/15.
+ */
+public class Inputs {
+
+    /**
+     *  Check for NPE but using IAE with proper error message
+     *
+     * @param inputs
+     * @throws IllegalArgumentException
+     */
+    public static void checkNull(Object... inputs) throws IllegalArgumentException{
+        if(inputs == null){
+            throw new IllegalArgumentException("No inputs to check");
+        }
+        for(Object obj : inputs){
+            if(obj==null){
+                throw new IllegalArgumentException("Null input");
+            }
+        }
+    }
+}
