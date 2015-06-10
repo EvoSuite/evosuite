5c21ad10082bf8c3087aa54e51d8df0b8b637917
Sina Shamshiri <sina6002@gmail.com>
2015-06-10 16:50:33 +0100

    fix for resourcelist tests

--- master/src/test/java/org/evosuite/classpath/TestResourceList.java
+++ master/src/test/java/org/evosuite/classpath/TestResourceList.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 
 import org.evosuite.SystemTest;
+import org.evosuite.TestGenerationContext;
 import org.junit.Test;
 
 import com.examples.with.different.packagename.listclasses.AbstractClass;
@@ -17,27 +18,27 @@ public class TestResourceList extends SystemTest {
 
 	@Test
 	public void testCanAccessAbstractClass() throws IOException {
-		assertTrue(ResourceList.isClassTestable(AbstractClass.class.getCanonicalName()));
+		assertTrue(ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).isClassTestable(AbstractClass.class.getCanonicalName()));
 	}
 	
 	@Test
 	public void testCanAccessAClassWithDefaultMethods() throws IOException {
-		assertTrue(ResourceList.isClassTestable(ClassWithDefaultMethods.class.getCanonicalName()));
+		assertTrue(ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).isClassTestable(ClassWithDefaultMethods.class.getCanonicalName()));
 	}
 
 	@Test
 	public void testCanAccessClassWithoutPublicMethods() throws IOException {
-		assertFalse(ResourceList.isClassTestable(ClassWithoutPublicMethods.class.getCanonicalName()));
+		assertFalse(ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).isClassTestable(ClassWithoutPublicMethods.class.getCanonicalName()));
 	}
 
 	@Test
 	public void testCanAccessClassWithProtectedMethods() throws IOException {
-		assertTrue(ResourceList.isClassTestable(ClassWithProtectedMethods.class.getCanonicalName()));
+		assertTrue(ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).isClassTestable(ClassWithProtectedMethods.class.getCanonicalName()));
 	}
 
 	@Test
 	public void testCanAccessNonPublicClass() throws IOException {
-		assertTrue(ResourceList.isClassTestable("com.examples.with.different.packagename.listclasses.NonPublicClass"));
+		assertTrue(ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).isClassTestable("com.examples.with.different.packagename.listclasses.NonPublicClass"));
 	}
 
 }
