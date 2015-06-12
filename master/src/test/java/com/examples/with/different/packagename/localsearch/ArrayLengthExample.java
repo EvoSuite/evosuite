99d59b36024010d9bb716046780d8bff5f60a87e
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-06-12 21:46:11 +0100

    Local search tests and fixes

---
+++ master/src/test/java/com/examples/with/different/packagename/localsearch/ArrayLengthExample.java
@@ -0,0 +1,12 @@
+package com.examples.with.different.packagename.localsearch;
+
+public class ArrayLengthExample {
+
+	public boolean testMe(int[] x) {
+		if(x.length == 4) 
+			return true;
+		else
+			return false;
+	}
+			
+}
