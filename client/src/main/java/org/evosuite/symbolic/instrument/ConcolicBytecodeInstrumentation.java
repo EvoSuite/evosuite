6b2df35aad713545c7b977d41975173d25c65475
Juan Galeotti <galeotti@cs.uni-saarland.de>
2015-06-24 17:11:41 -0300

    static reset with Concolic execution

--- client/src/main/java/org/evosuite/symbolic/instrument/ConcolicBytecodeInstrumentation.java
+++ client/src/main/java/org/evosuite/symbolic/instrument/ConcolicBytecodeInstrumentation.java
@@ -1,6 +1,11 @@
 package org.evosuite.symbolic.instrument;
 
+import org.evosuite.Properties;
+import org.evosuite.instrumentation.ExitClassInitAdapter;
+import org.evosuite.instrumentation.PutStaticClassAdapter;
 import org.evosuite.junit.writer.TestSuiteWriterUtils;
+import org.evosuite.runtime.classhandling.ResetManager;
+import org.evosuite.runtime.instrumentation.CreateClassResetClassAdapter;
 import org.evosuite.runtime.instrumentation.MethodCallReplacementClassAdapter;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
@@ -30,11 +35,31 @@ public class ConcolicBytecodeInstrumentation {
 
 		ClassVisitor cv = writer;
 
+        if (Properties.RESET_STATIC_FIELDS) {
+            cv = new PutStaticClassAdapter(cv, className);
+        }
+
 		// Apply transformations to class under test and its owned
 		// classes
 		// cv = new TraceClassVisitor(cv, new PrintWriter(System.err));
 		cv = new ConcolicClassAdapter(cv, className);
 
+        // If we need to reset static constructors, make them explicit methods
+        if (Properties.RESET_STATIC_FIELDS) {
+            // Create a __STATIC_RESET() cloning the original <clinit> method or create one by default
+            CreateClassResetClassAdapter resetClassAdapter = new CreateClassResetClassAdapter(cv, className);
+            if (ResetManager.getInstance().getResetFinalFields()) {
+                resetClassAdapter.setRemoveFinalModifierOnStaticFields(true);
+            } else {
+                resetClassAdapter.setRemoveFinalModifierOnStaticFields(false);
+            }
+            cv = resetClassAdapter;
+            // Add a callback before leaving the <clinit> method
+
+            ExitClassInitAdapter exitClassInitAdapter = new ExitClassInitAdapter(cv, className);
+            cv = exitClassInitAdapter;
+        }
+		
         // Mock instrumentation (eg File and TCP).
         if (TestSuiteWriterUtils.needToUseAgent()) {
             cv = new MethodCallReplacementClassAdapter(cv, className);
