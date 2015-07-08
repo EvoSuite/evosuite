5a36912b1551e2033a2bda2e60527bfd61a9c125
Andrea Arcuri <arcuri82@gmail.com>
2015-07-08 17:16:01 +0200

    handled PostConstruct

--- master/src/test/java/org/evosuite/jee/InjectionSystemTest.java
+++ master/src/test/java/org/evosuite/jee/InjectionSystemTest.java
@@ -3,6 +3,7 @@ package org.evosuite.jee;
 import com.examples.with.different.packagename.DependencyLibrary;
 import com.examples.with.different.packagename.jee.EntityManagerInjection;
 import com.examples.with.different.packagename.jee.MultipleDefaultInjections;
+import com.examples.with.different.packagename.jee.PostConstructorInjection;
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.SystemTest;
@@ -17,6 +18,28 @@ import org.junit.Test;
 public class InjectionSystemTest extends SystemTest{
 
     @Test
+    public void testPostConstructor(){
+
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = PostConstructorInjection.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
+        Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+
+    @Test
     public void testEntityManager(){
 
         EvoSuite evosuite = new EvoSuite();
@@ -26,6 +49,7 @@ public class InjectionSystemTest extends SystemTest{
         Properties.TARGET_CLASS = targetClass;
         Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
         Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
 
         String[] command = new String[] { "-generateSuite", "-class", targetClass };
 
@@ -48,6 +72,7 @@ public class InjectionSystemTest extends SystemTest{
         Properties.TARGET_CLASS = targetClass;
         Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
         Properties.JEE = true;
+        Properties.P_REFLECTION_ON_PRIVATE = 0.0;
 
         String[] command = new String[] { "-generateSuite", "-class", targetClass };
 
