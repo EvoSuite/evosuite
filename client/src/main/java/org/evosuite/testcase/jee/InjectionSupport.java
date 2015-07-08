3779e2bd1c35aa66607c3374031e74bba862eee7
arcuri82 <none@none>
2015-07-08 17:01:26 +0200

    handling default injection

--- client/src/main/java/org/evosuite/testcase/jee/InjectionSupport.java
+++ client/src/main/java/org/evosuite/testcase/jee/InjectionSupport.java
@@ -15,6 +15,9 @@ public class InjectionSupport {
     private static final Logger logger = LoggerFactory.getLogger(InjectionSupport.class);
 
     private static volatile GenericMethod entityManager;
+    private static volatile GenericMethod entityManagerFactory;
+    private static volatile GenericMethod userTransaction;
+    private static volatile GenericMethod event;
 
     public static GenericMethod getInjectorForEntityManager(){
         if(entityManager == null){
@@ -28,6 +31,48 @@ public class InjectionSupport {
             }
         }
         return entityManager;
+    }
+
+    public static GenericMethod getInjectorForEntityManagerFactory(){
+        if(entityManagerFactory == null){
+            try {
+                entityManagerFactory = new GenericMethod(
+                        Injector.class.getDeclaredMethod("injectEntityManagerFactory",Object.class,Class.class)
+                        , Injector.class);
+            } catch (NoSuchMethodException e) {
+                logger.error("Reflection failed in InjectionSupport: "+e.getMessage());
+                return null;
+            }
+        }
+        return entityManagerFactory;
+    }
+
+    public static GenericMethod getInjectorForUserTransaction(){
+        if(userTransaction == null){
+            try {
+                userTransaction = new GenericMethod(
+                        Injector.class.getDeclaredMethod("injectUserTransaction",Object.class,Class.class)
+                        , Injector.class);
+            } catch (NoSuchMethodException e) {
+                logger.error("Reflection failed in InjectionSupport: "+e.getMessage());
+                return null;
+            }
+        }
+        return userTransaction;
+    }
+
+    public static GenericMethod getInjectorForEvent(){
+        if(event == null){
+            try {
+                event = new GenericMethod(
+                        Injector.class.getDeclaredMethod("injectEvent",Object.class,Class.class)
+                        , Injector.class);
+            } catch (NoSuchMethodException e) {
+                logger.error("Reflection failed in InjectionSupport: "+e.getMessage());
+                return null;
+            }
+        }
+        return event;
     }
 
 }
--- master/src/test/java/org/evosuite/jee/InjectionSystemTest.java
+++ master/src/test/java/org/evosuite/jee/InjectionSystemTest.java
@@ -2,6 +2,7 @@ package org.evosuite.jee;
 
 import com.examples.with.different.packagename.DependencyLibrary;
 import com.examples.with.different.packagename.jee.EntityManagerInjection;
+import com.examples.with.different.packagename.jee.MultipleDefaultInjections;
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.SystemTest;
@@ -21,6 +22,28 @@ public class InjectionSystemTest extends SystemTest{
         EvoSuite evosuite = new EvoSuite();
 
         String targetClass = EntityManagerInjection.class.getCanonicalName();
+
+        Properties.TARGET_CLASS = targetClass;
+        Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
+        Properties.JEE = true;
+
+        String[] command = new String[] { "-generateSuite", "-class", targetClass };
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        System.out.println("EvolvedTestSuite:\n" + best);
+
+        Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+    }
+
+
+    @Test
+    public void testMultipleDefaultInjections(){
+
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = MultipleDefaultInjections.class.getCanonicalName();
 
         Properties.TARGET_CLASS = targetClass;
         Properties.CRITERION = new Properties.Criterion[]{Properties.Criterion.LINE};
