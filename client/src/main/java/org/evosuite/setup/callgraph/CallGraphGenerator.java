3f288e221af888c45da8b48e80204ba0fd906d50
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-07-24 20:17:52 +0100

    Remove unused imports

--- client/src/main/java/org/evosuite/setup/callgraph/CallGraphGenerator.java
+++ client/src/main/java/org/evosuite/setup/callgraph/CallGraphGenerator.java
@@ -20,15 +20,8 @@
  */
 package org.evosuite.setup.callgraph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.evosuite.Properties;
 import org.evosuite.instrumentation.BytecodeInstrumentation;
