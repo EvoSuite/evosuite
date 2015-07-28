1889a493c0c63aa3a0c5b0ac8e18e1ca1b62a844
Andrea Arcuri <arcuri82@gmail.com>
2015-07-28 14:22:56 +0200

    working on Mockito listener

---
+++ runtime/src/test/java/org/evosuite/runtime/fm/EvoInvocationListenerTest.java
@@ -0,0 +1,58 @@
+package org.evosuite.runtime.fm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+/**
+ * Created by Andrea Arcuri on 27/07/15.
+ */
+public class EvoInvocationListenerTest {
+
+    public interface Foo{
+        public int parseString(String s);
+        public int parseString(String s, boolean flag);
+        public int parseString(String s, Object obj);
+        public int parseString(String s, Foo foo);
+    }
+
+    @Test
+    public void testBase(){
+
+        EvoInvocationListener listener = new EvoInvocationListener();
+        Foo foo = mock(Foo.class, withSettings().invocationListeners(listener));
+
+        when(foo.parseString(any())).thenReturn(1);
+        when(foo.parseString(any(), anyBoolean())).thenReturn(2);
+        when(foo.parseString(any(), any(Object.class))).thenReturn(3);
+        when(foo.parseString(any(), any(Foo.class))).thenReturn(4);
+
+        int res = foo.parseString("foo");
+        Assert.assertEquals(1, res);
+
+        res = foo.parseString("bar",true);
+        Assert.assertEquals(2, res);
+
+        res = foo.parseString("foo");
+        Assert.assertEquals(1, res);
+
+        res = foo.parseString("bar",new Object());
+        Assert.assertEquals(3, res);
+
+        res = foo.parseString("bar", (Foo) null);
+        Assert.assertEquals(4, res);
+
+        List<MethodDescriptor> list = listener.getViewOfMethodDescriptors();
+        //TODO: as it is now, in Mockito we cannot distinguish between overloaded methods with same cardinality
+        Assert.assertEquals(2, list.size());
+    }
+}
