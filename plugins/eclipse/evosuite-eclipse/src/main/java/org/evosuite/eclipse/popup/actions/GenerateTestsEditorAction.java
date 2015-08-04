d57f2f1dedafbc7598bf69a4687d0497dad69541
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-08-04 17:42:52 +0100

    Change eclipse setup to newer version, likely broken but untestable due to Java 8

--- plugins/eclipse/evosuite-eclipse/src/main/java/org/evosuite/eclipse/popup/actions/GenerateTestsEditorAction.java
+++ plugins/eclipse/evosuite-eclipse/src/main/java/org/evosuite/eclipse/popup/actions/GenerateTestsEditorAction.java
@@ -3,10 +3,13 @@
  */
 package org.evosuite.eclipse.popup.actions;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
@@ -18,9 +21,11 @@ import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * @author Gordon Fraser
@@ -29,16 +34,41 @@ import org.eclipse.ui.PlatformUI;
 @SuppressWarnings("restriction")
 public class GenerateTestsEditorAction extends TestGenerationAction {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
 	@Override
-	public void run(IAction action) {
-		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		IEditorPart activeEditor = page.getActiveEditor();
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
 
 		String SUT = "";
-		if (activeEditor instanceof JavaEditor) {
+		IResource target = null;
+		System.out.println("Current selection of type "+selection.getClass().getName()+": "+selection);
+		if(selection instanceof TreeSelection) {
+			TreeSelection treeSelection = (TreeSelection) selection;
+			IAdaptable firstElement = (IAdaptable) treeSelection.getFirstElement();
+
+			// Relies on an internal API, bad juju
+			if (firstElement instanceof org.eclipse.jdt.internal.core.CompilationUnit) {
+				try {
+					org.eclipse.jdt.internal.core.CompilationUnit compilationUnit = (org.eclipse.jdt.internal.core.CompilationUnit) firstElement;									
+					String packageName = "";
+					if (compilationUnit.getPackageDeclarations().length > 0) {
+						System.out.println("Package: "
+								+ compilationUnit.getPackageDeclarations()[0].getElementName());
+						packageName = compilationUnit.getPackageDeclarations()[0].getElementName();
+					}
+					String targetSuite = compilationUnit.getElementName().replace(".java", "");
+					if (!packageName.isEmpty())
+						targetSuite = packageName + "." + targetSuite;
+					System.out.println("Selected class: " + targetSuite);
+					SUT = targetSuite;
+					target = compilationUnit.getResource();
+				} catch (JavaModelException e) {
+					
+				}
+			}
+		}
+		else if (activeEditor instanceof JavaEditor) {
 			ITypeRoot root = EditorUtility.getEditorInputJavaElement(activeEditor, false);
 			ITextSelection sel = (ITextSelection) ((JavaEditor) activeEditor).getSelectionProvider().getSelection();
 			int offset = sel.getOffset();
@@ -46,7 +76,11 @@ public class GenerateTestsEditorAction extends TestGenerationAction {
 
 			try {
 				element = root.getElementAt(offset);
-				if (element.getElementType() == IJavaElement.METHOD) {
+				if(element == null) {
+					ISelection sel2 = HandlerUtil.getCurrentSelection(event);
+					System.out.println("Selected element of type "+sel2.getClass().getName()+": "+sel2.toString());
+				}
+				else if (element.getElementType() == IJavaElement.METHOD) {
 					IJavaElement pDeclaration = element.getAncestor(IJavaElement.PACKAGE_FRAGMENT);
 					IPackageFragment pFragment = (IPackageFragment) pDeclaration;
 					String packageName = "";
@@ -65,27 +99,33 @@ public class GenerateTestsEditorAction extends TestGenerationAction {
 					System.out.println("Selected class: " + type.getFullyQualifiedName());
 					SUT = type.getFullyQualifiedName();
 				}
+				
+				IWorkspaceRoot wroot = ResourcesPlugin.getWorkspace().getRoot();
+				target = wroot.findMember(EditorUtility.getEditorInputJavaElement(activeEditor, false).getPath());
 
-				if (!SUT.isEmpty()) {
-					IWorkspaceRoot wroot = ResourcesPlugin.getWorkspace().getRoot();
-					IResource res = wroot.findMember(root.getPath());
-					IProject proj = res.getProject();
-					fixJUnitClassPath(JavaCore.create(proj));
-					generateTests(res);
-				}
 			} catch (JavaModelException e) {
 
 			}
+
+
 		}
+		
+		if (!SUT.isEmpty() && target != null) {
+			IProject proj = target.getProject();
+			fixJUnitClassPath(JavaCore.create(proj));
+			generateTests(target);
+		}
+		
+		return null;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
 	 */
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-
-	}
+//	@Override
+//	public void selectionChanged(IAction action, ISelection selection) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
