eea7c6273659efdc074f00aa9c801c65ee492d24
Jose Rojas <jmrs84@email.com>
2015-09-07 09:47:21 +0200

    fix for non-compiling extended suites due to missing imports; pitfall: concrete import declarations turn into onDemand declarations

---
+++ plugins/eclipse/evosuite-eclipse/src/main/java/org/evosuite/eclipse/popup/actions/ImportDeclarationVisitor.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2010-2015 Gordon Fraser, Andrea Arcuri and EvoSuite
+ * contributors
+ *
+ * This file is part of EvoSuite.
+ *
+ * EvoSuite is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser Public License as published by the
+ * Free Software Foundation, either version 3.0 of the License, or (at your
+ * option) any later version.
+ *
+ * EvoSuite is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License along
+ * with EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package org.evosuite.eclipse.popup.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+
+/**
+ * @author Jose Miguel Rojas
+ * 
+ */
+public class ImportDeclarationVisitor extends ASTVisitor {
+
+	public String result = "";
+	
+	private List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.MethodDeclaration)
+	 */
+	@Override
+	public boolean visit(ImportDeclaration node) {
+		imports.add(node);
+		return super.visit(node);
+	}
+	
+	public List<ImportDeclaration> getImports() {
+		return imports;
+	}
+}
