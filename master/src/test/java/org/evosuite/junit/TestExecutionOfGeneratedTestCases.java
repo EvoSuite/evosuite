25b95374e223e6299b2755bbb49221415d955d4f
Andrea Arcuri <arcuri82@gmail.com>
2015-09-09 23:10:59 +0200

    fix for failing test

--- master/src/test/java/org/evosuite/junit/TestExecutionOfGeneratedTestCases.java
+++ master/src/test/java/org/evosuite/junit/TestExecutionOfGeneratedTestCases.java
@@ -5,19 +5,26 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.evosuite.SystemTest;
+import org.evosuite.runtime.instrumentation.MethodCallReplacementClassAdapter;
 import org.junit.Test;
 
 import com.examples.with.different.packagename.Euclidean_ESTest;
 
-public class TestExecutionOfGeneratedTestCases extends SystemTest {
+public class TestExecutionOfGeneratedTestCases  {
 
 	@Test
 	public void test() {
-		JUnitRunner jR = new JUnitRunner(Euclidean_ESTest.class);
-	    jR.run();
+		MethodCallReplacementClassAdapter.dirtyHack_applyUIDTransformation = false;
 
-	    List<JUnitResult> results = jR.getTestResults();
-	    assertTrue(results.size() == 1);
-	    assertTrue(results.get(0).wasSuccessful());
+		try {
+			JUnitRunner jR = new JUnitRunner(Euclidean_ESTest.class);
+			jR.run();
+
+			List<JUnitResult> results = jR.getTestResults();
+			assertTrue(results.size() == 1);
+			assertTrue(results.get(0).wasSuccessful());
+		} finally {
+			MethodCallReplacementClassAdapter.dirtyHack_applyUIDTransformation = true;
+		}
 	}
 }
