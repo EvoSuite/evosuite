39edbc4c4f674ada63fa9dcce5c3696f9fc176ec
Andrea Arcuri <arcuri82@gmail.com>
2015-09-12 20:42:50 +0200

    use own logger instead of one of superclass

--- client/src/main/java/org/evosuite/testsuite/AbstractTestSuiteChromosome.java
+++ client/src/main/java/org/evosuite/testsuite/AbstractTestSuiteChromosome.java
@@ -51,11 +51,15 @@ import org.evosuite.ga.ConstructionFailedException;
 import org.evosuite.ga.localsearch.LocalSearchObjective;
 import org.evosuite.testcase.ExecutableChromosome;
 import org.evosuite.utils.Randomness;
-public abstract class AbstractTestSuiteChromosome<T extends ExecutableChromosome> extends
-        Chromosome {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractTestSuiteChromosome<T extends ExecutableChromosome> extends Chromosome {
+
+
 	private static final long serialVersionUID = 1L;
 
-
+	private static final Logger logger = LoggerFactory.getLogger(AbstractTestSuiteChromosome.class);
 	
 	protected List<T> tests = new ArrayList<T>();
 	protected ChromosomeFactory<T> testChromosomeFactory;
