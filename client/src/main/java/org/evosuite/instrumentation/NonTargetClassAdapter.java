078edf74c9f12b4deb8e4a5e6b476c41e29a8c88
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2015-09-30 00:53:39 +0100

    Workaround to let mocking return final modifier on classes that is otherwise removed to make mocking possible

--- client/src/main/java/org/evosuite/instrumentation/NonTargetClassAdapter.java
+++ client/src/main/java/org/evosuite/instrumentation/NonTargetClassAdapter.java
@@ -22,6 +22,7 @@
  */
 package org.evosuite.instrumentation;
 
+import org.evosuite.runtime.instrumentation.RemoveFinalClassAdapter;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
@@ -49,6 +50,10 @@ public class NonTargetClassAdapter extends ClassVisitor {
 
 	@Override
 	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+		if((access & Opcodes.ACC_FINAL) == Opcodes.ACC_FINAL) {
+			RemoveFinalClassAdapter.finalClasses.add(name.replace('/', '.'));
+		}
+
 		// We are removing final access to allow mocking
 		super.visit(version, access & ~Opcodes.ACC_FINAL, name, signature, superName, interfaces);
 	}
@@ -67,6 +72,9 @@ public class NonTargetClassAdapter extends ClassVisitor {
 	
 	@Override
 	public void visitInnerClass(String name, String outerName, String innerName, int access) {
+		if((access & Opcodes.ACC_FINAL) == Opcodes.ACC_FINAL) {
+			RemoveFinalClassAdapter.finalClasses.add(name.replace('/', '.'));
+		}
 		// We are removing final access to allow mocking
 		super.visitInnerClass(name, outerName, innerName, access & ~Opcodes.ACC_FINAL);
 	}
