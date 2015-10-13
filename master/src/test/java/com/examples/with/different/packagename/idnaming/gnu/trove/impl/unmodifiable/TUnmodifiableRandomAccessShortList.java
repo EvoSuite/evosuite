ef2c5bd9471c021dff4ee6e860999ee3f1eb2fb4
Jose Rojas <jmrs84@email.com>
2015-10-13 22:11:39 +0100

    replace package with code that actually compiles

---
+++ master/src/test/java/com/examples/with/different/packagename/idnaming/gnu/trove/impl/unmodifiable/TUnmodifiableRandomAccessShortList.java
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2008, Robert D. Eden All Rights Reserved.
+// Copyright (c) 2009, Jeff Randall All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+///////////////////////////////////////////////////////////////////////////////
+
+package com.examples.with.different.packagename.idnaming.gnu.trove.impl.unmodifiable;
+
+
+//////////////////////////////////////////////////
+// THIS IS A GENERATED CLASS. DO NOT HAND EDIT! //
+//////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// THIS IS AN IMPLEMENTATION CLASS. DO NOT USE DIRECTLY!  //
+// Access to these methods should be through TCollections //
+////////////////////////////////////////////////////////////
+
+
+import com.examples.with.different.packagename.idnaming.gnu.trove.iterator.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.procedure.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.set.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.list.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.function.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.map.*;
+import com.examples.with.different.packagename.idnaming.gnu.trove.*;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map;
+import java.util.RandomAccess;
+import java.util.Random;
+import java.io.Serializable;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+
+public class TUnmodifiableRandomAccessShortList extends TUnmodifiableShortList
+	implements RandomAccess {
+
+	private static final long serialVersionUID = -2542308836966382001L;
+
+	public TUnmodifiableRandomAccessShortList( TShortList list ) {
+		super( list );
+	}
+
+	public TShortList subList( int fromIndex, int toIndex ) {
+		return new TUnmodifiableRandomAccessShortList( list.subList( fromIndex, toIndex ) );
+	}
+
+	/**
+	 * Allows instances to be deserialized in pre-1.4 JREs (which do
+	 * not have UnmodifiableRandomAccessList).  UnmodifiableList has
+	 * a readResolve method that inverts this transformation upon
+	 * deserialization.
+	 */
+	private Object writeReplace() {
+		return new TUnmodifiableShortList( list );
+	}
+}
