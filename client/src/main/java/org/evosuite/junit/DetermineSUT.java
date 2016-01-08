c16e23596b249783081da4f34c2e6da811bf408b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-01-08 17:58:09 +0000

    DetermineSUT updates

--- client/src/main/java/org/evosuite/junit/DetermineSUT.java
+++ client/src/main/java/org/evosuite/junit/DetermineSUT.java
@@ -41,6 +41,7 @@ import org.evosuite.classpath.ClassPathHacker;
 import org.evosuite.classpath.ResourceList;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
@@ -138,6 +139,13 @@ public class DetermineSUT {
 			ClassReader reader = new ClassReader(is);
 			ClassNode classNode = new ClassNode();
 			reader.accept(classNode, ClassReader.SKIP_FRAMES);
+
+            // If we only look for pure classes
+            if(!isPureTestClass(classNode)) {
+                calledClasses.add("<NOTATEST>");
+                return calledClasses;
+            }
+
 			superClasses = getSuperClasses(classNode);
 
 			if (isJUnitTest(classNode)) {
@@ -155,6 +163,96 @@ public class DetermineSUT {
 		return calledClasses;
 	}
 
+    private boolean isPureTestClass(String className) throws ClassNotFoundException {
+        try {
+
+            InputStream is = ClassLoader.getSystemResourceAsStream(className + ".class");
+            if (is == null) {
+                throw new ClassNotFoundException("Class '" + className + ".class"
+                        + "' should be in target project, but could not be found!");
+            }
+            ClassReader reader = new ClassReader(is);
+            ClassNode classNode = new ClassNode();
+            reader.accept(classNode, ClassReader.SKIP_FRAMES);
+            return isPureTestClass(classNode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean isPureTestClass(ClassNode cn) {
+        if ((cn.access & Opcodes.ACC_ABSTRACT) == Opcodes.ACC_ABSTRACT) {
+            System.err.println("Abstract class: "+cn.name);
+            return false;
+        }
+
+        if(!cn.superName.equals("java/lang/Object") && !cn.superName.equals("junit/framework/TestCase")) {
+            System.err.println("Wrong superclass: "+cn.superName);
+            return false;
+        }
+
+        boolean junit3 = false;
+        if(cn.superName.equals("junit/framework/TestCase"))
+            junit3 = true;
+
+        System.err.println("JUnit3: "+junit3);
+        List<MethodNode> methods = cn.methods;
+        for (MethodNode mn : methods) {
+            if(mn.name.startsWith("<init>"))
+                continue;
+            if(mn.name.startsWith("<clinit>"))
+                continue;
+            if(mn.name.equals("main"))
+                continue;
+            if(mn.name.equals("suite"))
+                continue;
+            if(mn.name.equals("class$"))
+                continue;
+
+            System.err.println("Checking method: "+mn.name);
+            if(junit3) {
+                if(mn.name.startsWith("setUp") || mn.name.startsWith("tearDown"))
+                    continue;
+                // Test
+                if(!mn.name.startsWith("test")) {
+                    System.err.println("Not a test method: "+mn.name);
+                    return false;
+                }
+
+                // No arguments
+                if(Type.getArgumentTypes(mn.desc).length > 0) {
+                    System.err.println("Has arguments: "+mn.name);
+                    return false;
+                }
+
+
+                // public
+                if((Opcodes.ACC_PUBLIC & mn.access) != Opcodes.ACC_PUBLIC) {
+                    System.err.println("Not public: "+mn.name);
+                    return false;
+                }
+            } else {
+                boolean hasTestAnnotation = false;
+                List<AnnotationNode> annotations = mn.visibleAnnotations;
+                if (annotations != null) {
+                    for (AnnotationNode an : annotations) {
+                        if (an.desc.equals("Lorg/junit/Test;") || an.desc.equals("Lorg/junit/Before;") || an.desc.equals("Lorg/junit/After;") ||
+                                an.desc.equals("L" + PackageInfo.getNameWithSlash(EvoSuiteTest.class) + ";")) {
+                            hasTestAnnotation = true;
+                            break;
+                        }
+                    }
+                }
+                if(!hasTestAnnotation) {
+                    System.err.println("Has no annotation : "+mn.name);
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
 	@SuppressWarnings("unchecked")
 	private void handleClassNode(Set<String> calledClasses, ClassNode cn,
