54407a51d24ddff0306e05ca23135c9c4fe5299e
Jose Miguel Rojas <jmrs84@gmail.com>
2016-03-08 03:14:44 +0000

    updated naming-related tests

--- master/src/test/java/org/evosuite/idnaming/IDNamingSystemTest.java
+++ master/src/test/java/org/evosuite/idnaming/IDNamingSystemTest.java
@@ -20,19 +20,29 @@
 package org.evosuite.idnaming;
 
 import com.examples.with.different.packagename.Calculator;
-import com.examples.with.different.packagename.sette.L4_Collections;
-import com.examples.with.different.packagename.sette.SnippetInputContainer;
+import com.examples.with.different.packagename.coverage.BOMInputStreamTest;
+import com.examples.with.different.packagename.coverage.MethodWithSeveralInputArguments;
+import com.examples.with.different.packagename.coverage.TestMethodWithSeveralInputArguments;
+import com.examples.with.different.packagename.idnaming.BOMInputStream;
 import com.examples.with.different.packagename.idnaming.naturalize.StringExample;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.SystemTestBase;
-import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.junit.naming.methods.CoverageGoalTestNameGenerationStrategy;
 import org.evosuite.junit.writer.TestSuiteWriter;
+import org.evosuite.junit.writer.TestSuiteWriterUtils;
+import org.evosuite.statistics.SearchStatistics;
 import org.evosuite.strategy.TestGenerationStrategy;
+import org.evosuite.testcarver.testcase.CarvedTestCase;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.execution.TestCaseExecutor;
+import org.evosuite.testcase.factories.JUnitTestCarvedChromosomeFactory;
 import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.xsd.TestSuite;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,9 +51,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
-
-import static org.junit.Assert.assertEquals;
+import java.util.List;
+import java.util.Map;
 
 public class IDNamingSystemTest extends SystemTestBase {
 
@@ -129,188 +140,50 @@ public class IDNamingSystemTest extends SystemTestBase {
 	}
 
 
-//	@Test
-//	public void testCarvedTestNames() {
-//
-//		EvoSuite evosuite = new EvoSuite();
-//
-//		String targetClass = MethodWithSeveralInputArguments.class.getCanonicalName();
-//		String testClass = TestMethodWithSeveralInputArguments.class.getCanonicalName();
-//
-//		Properties.TARGET_CLASS = targetClass;
-//		Properties.JUNIT = testClass;
-//		Properties.SELECTED_JUNIT = testClass;
-//
-//		Properties.TEST_NAMING = true;
-//		Properties.JUNIT_TESTS = true;
-//		Properties.WRITE_COVERED_GOALS_FILE = true;
-//		Properties.WRITE_TEST_NAMES_FILE = true;
-//
-//		Properties.CRITERION = new Properties.Criterion[] { Properties.Criterion.INPUT,
-//				Properties.Criterion.OUTPUT, Properties.Criterion.METHOD};
-//
-//		String[] command = new String[] {
-//				"-class", targetClass,
-//				"-Djunit=" + testClass,
-//				"-Dselected_junit=" + testClass,
-//				"-measureCoverage"
-//		};
-//
-//		SearchStatistics result = (SearchStatistics)evosuite.parseCommandLine(command);
-//		Assert.assertNotNull(result);
-//
-//		Map<TestCase,String> testNamesMap = TestNameGenerator.getResults();
-//		Assert.assertEquals("Incorrect number of carved tests", 2, testNamesMap.size());
-//
-//		Object[] tests = testNamesMap.keySet().toArray();
-//		TestCase tc0 = (TestCase)tests[0];
-//		TestCase tc1 = (TestCase)tests[1];
-//
-//		Assert.assertEquals("Unexpected number of covered goals", 7, tc0.getCoveredGoals().size());
-//		Assert.assertEquals("Unexpected number of covered goals", 7, tc1.getCoveredGoals().size());
-//
-//		String carvedName0 = ((CarvedTestCase)tc0).getName();
-//		String generatedName0 = testNamesMap.get(tc0);
-//
-//		String carvedName1 = ((CarvedTestCase)tc1).getName();
-//		String generatedName1 = testNamesMap.get(tc1);
-//
-//		String[] carved = { carvedName0, carvedName1 };
-//		Arrays.sort(carved);
-//		String[] carvedExpected = {"testWithNull", "testWithArray"};
-//		Arrays.sort(carvedExpected);
-//		String[] generated = { generatedName0, generatedName1 };
-//		Arrays.sort(generated);
-//		String[] generatedExpected = {"testTestFooWithNonemptyInput", "testTestFooWithNullInput"};
-//		Arrays.sort(generatedExpected);
-//
-//		Assert.assertArrayEquals("Unexpected carved test names", carvedExpected, carved);
-//		Assert.assertArrayEquals("Unexpected generated test names", generatedExpected, generated);
-//	}
-
-//	@Test
-//	public void testCarvedTestNamesBOMInputStream() {
-//
-//		EvoSuite evosuite = new EvoSuite();
-//
-//		String targetClass = BOMInputStream.class.getCanonicalName();
-//		String testClass = BOMInputStreamTest.class.getCanonicalName();
-//
-//		Properties.TARGET_CLASS = targetClass;
-//		Properties.JUNIT = testClass;
-//		Properties.SELECTED_JUNIT = testClass;
-//
-//		Properties.TEST_NAMING = true;
-//		Properties.JUNIT_TESTS = true;
-//		Properties.WRITE_COVERED_GOALS_FILE = true;
-//		Properties.WRITE_TEST_NAMES_FILE = true;
-//
-//		Properties.CRITERION = new Properties.Criterion[] { Properties.Criterion.INPUT,
-//				Properties.Criterion.OUTPUT, Properties.Criterion.METHOD, Properties.Criterion.BRANCH, Properties.Criterion.EXCEPTION};
-//
-//		String[] command = new String[] {
-//				"-class", targetClass,
-//				"-Djunit=" + testClass,
-//				"-Dselected_junit=" + testClass,
-//				"-measureCoverage"
-//		};
-//
-//		SearchStatistics result = (SearchStatistics)evosuite.parseCommandLine(command);
-//		Assert.assertNotNull(result);
-//
-//		Map<TestCase,String> testNamesMap = TestNameGenerator.getResults();
-//		Assert.assertEquals("Incorrect number of carved tests", 2, testNamesMap.size());
-//
-//		Object[] tests = testNamesMap.keySet().toArray();
-//		TestCase tc0 = (TestCase)tests[0];
-//		TestCase tc1 = (TestCase)tests[1];
-//
-//		Assert.assertEquals("Unexpected number of covered goals", 7, tc0.getCoveredGoals().size());
-//		Assert.assertEquals("Unexpected number of covered goals", 7, tc1.getCoveredGoals().size());
-//
-//		String carvedName0 = ((CarvedTestCase)tc0).getName();
-//		String generatedName0 = testNamesMap.get(tc0);
-//
-//		String carvedName1 = ((CarvedTestCase)tc1).getName();
-//		String generatedName1 = testNamesMap.get(tc1);
-//
-//		String[] carved = { carvedName0, carvedName1 };
-//		Arrays.sort(carved);
-//		String[] carvedExpected = {"testWithNull", "testWithArray"};
-//		Arrays.sort(carvedExpected);
-//		String[] generated = { generatedName0, generatedName1 };
-//		Arrays.sort(generated);
-//		String[] generatedExpected = {"testTestFooWithNonemptyInput", "testTestFooWithNullInput"};
-//		Arrays.sort(generatedExpected);
-//
-//		Assert.assertArrayEquals("Unexpected carved test names", carvedExpected, carved);
-//		Assert.assertArrayEquals("Unexpected generated test names", generatedExpected, generated);
-//	}
-
 	@Test
-	public void testIdWithSameMethod() {
+	public void testCarvedTestNames() {
+
 		EvoSuite evosuite = new EvoSuite();
 
-		String targetClass = Calculator.class.getCanonicalName();
+		String targetClass = MethodWithSeveralInputArguments.class.getCanonicalName();
+		String testClass = TestMethodWithSeveralInputArguments.class.getCanonicalName();
 
 		Properties.TARGET_CLASS = targetClass;
-		Properties.JUNIT_TESTS = true;
+		Properties.JUNIT = testClass;
+		Properties.SELECTED_JUNIT = testClass;
 
-		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+		Properties.CRITERION = new Properties.Criterion[] { Properties.Criterion.INPUT,
+				Properties.Criterion.OUTPUT, Properties.Criterion.METHOD};
 
-		Object result = evosuite.parseCommandLine(command);
-		GeneticAlgorithm<?> ga = getGAFromResult(result);
-		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
-		System.out.println("EvolvedTestSuite:\n" + best);
+		String[] command = new String[] {
+				"-class", targetClass,
+				"-Djunit=" + testClass,
+				"-Dselected_junit=" + testClass,
+				"-measureCoverage"
+		};
 
-		int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
+		SearchStatistics result = (SearchStatistics)evosuite.parseCommandLine(command);
+		Assert.assertNotNull(result);
 
-		Assert.assertEquals("Wrong number of goals: ", 5, goals);
-		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
+		JUnitTestCarvedChromosomeFactory factory = new JUnitTestCarvedChromosomeFactory(null);
+		Assert.assertTrue(factory.hasCarvedTestCases());
+		Assert.assertEquals("Incorrect number of carved tests", 2, factory.getNumCarvedTestCases());
+
+		List<TestCase> tests = factory.getCarvedTestCases();
+
+		Assert.assertEquals("Unexpected number of covered goals", 7, tests.get(0).getCoveredGoals().size());
+		Assert.assertEquals("Unexpected number of covered goals", 7, tests.get(1).getCoveredGoals().size());
+
+		String carvedName0 = TestSuiteWriterUtils.getNameOfTest(tests, 0);
+		String carvedName1 = TestSuiteWriterUtils.getNameOfTest(tests, 1);
+		Assert.assertEquals("Unexpected carved test name", "testWithNull", carvedName0);
+		Assert.assertEquals("Unexpected carved test name", "testWithArray", carvedName1);
+
+		CoverageGoalTestNameGenerationStrategy naming = new CoverageGoalTestNameGenerationStrategy(tests);
+		String generatedName0 = naming.getName(tests.get(0));
+		String generatedName1 = naming.getName(tests.get(1));
+		Assert.assertEquals("Unexpected generated test name", "testTestFooWithNull", generatedName0);
+		Assert.assertEquals("Unexpected generated test name", "testTestFooWithNonEmptyArray", generatedName1);
 	}
-
-	@Test
-	public void testIdWithSameOutput() {
-		EvoSuite evosuite = new EvoSuite();
-
-		String targetClass = L4_Collections.class.getCanonicalName();
-
-		Properties.TARGET_CLASS = targetClass;
-		Properties.JUNIT_TESTS = true;
-
-		String[] command = new String[] { "-generateSuite", "-class", targetClass };
-
-		Object result = evosuite.parseCommandLine(command);
-		GeneticAlgorithm<?> ga = getGAFromResult(result);
-		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
-		System.out.println("EvolvedTestSuite:\n" + best);
-
-		int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
-		//	System.out.println(best.getCoverage()+"-"+1d);
-		Assert.assertEquals("Wrong number of goals: ", 11, goals);
-		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.443);
-	}
-
-	@Test
-	public void testMethodTrace() {
-		EvoSuite evosuite = new EvoSuite();
-
-		String targetClass = SnippetInputContainer.class.getCanonicalName();
-
-		Properties.TARGET_CLASS = targetClass;
-		Properties.JUNIT_TESTS = true;
-
-		String[] command = new String[] { "-generateSuite", "-class", targetClass };
-
-		Object result = evosuite.parseCommandLine(command);
-		GeneticAlgorithm<?> ga = getGAFromResult(result);
-		TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
-		System.out.println("EvolvedTestSuite:\n" + best);
-
-		int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
-		Assert.assertEquals("Wrong number of goals: ", 15, goals);
-		Assert.assertEquals("Non-optimal coverage: ", 1d, best.getCoverage(), 0.001);
-	}
-
 
 }
