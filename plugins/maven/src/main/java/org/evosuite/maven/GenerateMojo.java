daf0a0fb72f4b788d1effb57deab55a65624914b
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-04-04 18:12:46 +0100

    Execute compile lifecyclephase before generate in maven plugin to ensure code is compiled

--- plugins/maven/src/main/java/org/evosuite/maven/GenerateMojo.java
+++ plugins/maven/src/main/java/org/evosuite/maven/GenerateMojo.java
@@ -19,23 +19,12 @@
  */
 package org.evosuite.maven;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
 import org.eclipse.aether.RepositorySystemSession;
@@ -45,10 +34,19 @@ import org.evosuite.maven.util.FileUtils;
 import org.evosuite.maven.util.HistoryChanges;
 import org.evosuite.utils.SpawnProcessKeepAliveChecker;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Generate JUnit tests
  */
 @Mojo( name = "generate" , requiresDependencyResolution = ResolutionScope.TEST, requiresDependencyCollection = ResolutionScope.TEST)
+@Execute(phase = LifecyclePhase.COMPILE)
 public class GenerateMojo extends AbstractMojo {
 
 	/**
