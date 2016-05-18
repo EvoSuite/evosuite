72c9c0d6122f20989f86baf02f0a3f685510f64a
arcuri82 <arcuri82@gmail.com>
2016-05-18 09:23:18 +0200

    investigating test execution issue on Jenkins

--- master/src/test/java/org/evosuite/SystemTestBase.java
+++ master/src/test/java/org/evosuite/SystemTestBase.java
@@ -132,8 +132,8 @@ public class SystemTestBase {
 		ResetManager.getInstance().clearManager();
 
 		//change seed every month
-		long seed = new GregorianCalendar().get(Calendar.MONTH);
-//		long seed = getSeed();
+//		long seed = new GregorianCalendar().get(Calendar.MONTH);
+		long seed = getSeed();
 		Randomness.setSeed(seed);
 
 		currentProperties = (java.util.Properties) System.getProperties().clone();
