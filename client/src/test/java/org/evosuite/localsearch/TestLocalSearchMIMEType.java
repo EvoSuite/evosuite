3ce33327386eda44ddc6e8800917f4f692b16f6f
Juan Galeotti <jgaleotti@gmail.com>
2016-06-15 16:34:23 -0300

    testfix: LS optimises compound fitness, not one per fitness function

--- client/src/test/java/org/evosuite/localsearch/TestLocalSearchMIMEType.java
+++ client/src/test/java/org/evosuite/localsearch/TestLocalSearchMIMEType.java
@@ -241,17 +241,15 @@ public class TestLocalSearchMIMEType {
 		fitnessFunctions.add(outputCoverage);
 		fitnessFunctions.add(methodCoverage);
 		fitnessFunctions.add(methodNoExceptionCoverage);
-		//fitnessFunctions.add(cbranchCoverage);
+		fitnessFunctions.add(cbranchCoverage);
 
 		for (TestSuiteFitnessFunction ff : fitnessFunctions) {
 			suite.addFitness(ff);
 		}
 
-		Map<TestSuiteFitnessFunction,Double> oldFitnesses = new HashMap<TestSuiteFitnessFunction,Double>();
 		for (TestSuiteFitnessFunction ff : fitnessFunctions) {
 			double oldFitness = ff.getFitness(suite);
 			System.out.println(ff.toString() + "->" + oldFitness);
-			oldFitnesses.put(ff, oldFitness);
 		}
 		double oldFitness = suite.getFitness();
 		System.out.println("oldFitness->" + oldFitness);
@@ -265,24 +263,16 @@ public class TestLocalSearchMIMEType {
 
 		System.out.println("hasImproved=" + hasImproved);
 
-		Map<TestSuiteFitnessFunction,Double> newFitnesses = new HashMap<TestSuiteFitnessFunction,Double>();
 
 		for (TestSuiteFitnessFunction ff : fitnessFunctions) {
 			double newFitness = ff.getFitness(suite);
 			System.out.println(ff.toString() + "->" + newFitness);
-			newFitnesses.put(ff, newFitness);
 
 		}
 		double newFitness = suite.getFitness();
 		System.out.println("newFitness->" + newFitness);
 		System.out.println("newSize->" + suite.getTests().size());
 
-		for (TestSuiteFitnessFunction testSuiteFitnessFunction : fitnessFunctions) {
-			double oldValue = oldFitnesses.get(testSuiteFitnessFunction);
-			double newValue = newFitnesses.get(testSuiteFitnessFunction);
-			assertTrue(newValue<=oldValue);
-		}
-		
 		assertTrue(newFitness<=oldFitness);
 	}
 
