c508fe0a7ffc85b724fa46c40067744a2c289799
Juan Galeotti <jgaleotti@gmail.com>
2016-06-30 13:30:21 -0300

    changes to CVC4 configuration

--- client/src/main/java/org/evosuite/symbolic/solver/cvc4/CVC4QueryPrinter.java
+++ client/src/main/java/org/evosuite/symbolic/solver/cvc4/CVC4QueryPrinter.java
@@ -28,7 +28,7 @@ import org.evosuite.symbolic.solver.smt.SmtFunctionDefinition;
 
 class CVC4QueryPrinter {
 
-	private static final String CVC4_LOGIC = "QF_SLIRA";
+	private static final String CVC4_LOGIC = "SLIRA"; // previously QF_SLIRA
 
 	public String print(SmtCheckSatQuery smtQuery) {
 		StringBuffer buff = new StringBuffer();
