f1be8a3a37fe210405ee4b79da3ceeeeca5cf6a9
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-11-26 19:15:13 +0000

    Delete old observers when resetting test executor

--- client/src/main/java/org/evosuite/TestGenerationContext.java
+++ client/src/main/java/org/evosuite/TestGenerationContext.java
@@ -154,6 +154,7 @@ public class TestGenerationContext {
 			DBManager.getInstance().setSutClassLoader(classLoader);
 		}
 
+		TestCaseExecutor.getInstance().newObservers();
 		TestCaseExecutor.pullDown();
 
 		ExecutionTracer.getExecutionTracer().clear();
