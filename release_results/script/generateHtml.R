ba19123c63a5668ddc8cdadef66a33d527113dfe
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-12-29 01:14:44 +0000

    Update release experiment scripts

--- release_results/script/generateHtml.R
+++ release_results/script/generateHtml.R
@@ -1,6 +1,6 @@
 source("util.R")
 
-VERSION = "1_0_3"
+VERSION = "1_0_4"
 
 GENERATED_FILES = paste("../generated_files","/",VERSION,sep="")
 SELECTION_FILE = paste("../generated_files","/","selection.txt",sep="")
