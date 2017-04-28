b53059a27d921d5c751abf8a79fb9e010d6dc8a9
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2017-04-28 15:07:45 +0100

    Give wildfly test more time

--- master/src/test/java/org/evosuite/jee/WildflySystemTest.java
+++ master/src/test/java/org/evosuite/jee/WildflySystemTest.java
@@ -3,15 +3,12 @@ package org.evosuite.jee;
 import com.examples.with.different.packagename.jee.injection.wildfly.TransactionServlet;
 import org.evosuite.Properties;
 import org.evosuite.SystemTestBase;
-import org.evosuite.utils.Randomness;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class WildflySystemTest extends SystemTestBase{
 
 
-    @Test(timeout = 60_000) //@Ignore
+    @Test(timeout = 120_000) //@Ignore
     public void testTransactionServlet(){
         Properties.JEE = true;
         Properties.SEARCH_BUDGET = 100_000;
