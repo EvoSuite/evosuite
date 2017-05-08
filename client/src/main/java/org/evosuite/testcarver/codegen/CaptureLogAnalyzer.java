5da7bab2966b05bf3f0dcdfbb2bea5fbebc3cbcd
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2017-05-08 22:22:21 +0100

    Remove redundant condition

--- client/src/main/java/org/evosuite/testcarver/codegen/CaptureLogAnalyzer.java
+++ client/src/main/java/org/evosuite/testcarver/codegen/CaptureLogAnalyzer.java
@@ -227,7 +227,7 @@ public final class CaptureLogAnalyzer implements ICaptureLogAnalyzer
 			{
 				return Short.TYPE;
 			}
-			else if(type.equals("String") ||type.equals("Boolean") ||type.equals("Boolean") || type.equals("Short") ||type.equals("Long") ||
+			else if(type.equals("String") ||type.equals("Boolean") || type.equals("Short") ||type.equals("Long") ||
 					type.equals("Integer") || type.equals("Float") || type.equals("Double") ||type.equals("Byte") || 
 					type.equals("Character") )
 			{
