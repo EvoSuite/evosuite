c47bc317dc18f3e40d31d7aee55e00ad7bcbb67a
apanichella <annibale.panichella@uni.lu>
2017-09-06 17:05:59 +0200

    Fixing NPE in InputCoverageTestFitness

--- client/src/main/java/org/evosuite/coverage/io/input/InputCoverageGoal.java
+++ client/src/main/java/org/evosuite/coverage/io/input/InputCoverageGoal.java
@@ -20,9 +20,12 @@
 package org.evosuite.coverage.io.input;
 
 
+import org.evosuite.ga.metaheuristics.mosa.DynaMOSA;
 import org.evosuite.testcase.TestFitnessFunction;
 import org.evosuite.testcase.statements.EntityWithParametersStatement;
 import org.objectweb.asm.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.lang.reflect.Array;
@@ -40,247 +43,257 @@ import static org.evosuite.coverage.io.IOCoverageConstants.STRING_NONEMPTY;
  */
 public class InputCoverageGoal implements Serializable, Comparable<InputCoverageGoal> {
 
-    private static final long serialVersionUID = -2917009638438833179L;
+	private static final long serialVersionUID = -2917009638438833179L;
 
-    private final String className;
-    private final String methodName;
-    private final int    argIndex;
-    private final String type;
-    private final String valueDescriptor;
-    private final Number numericValue;
+	private static final Logger logger = LoggerFactory.getLogger(DynaMOSA.class);
 
-    /**
-     * Can be used to create an arbitrary {@code InputCoverageGoal} trying to cover the
-     * method such that it returns a given {@code value}
-     * <p/>
-     * <p/>
-     * If the method returns a boolean, this goal will try to cover the method with either {@code true} or {@code false}
-     * If the given branch is {@code null}, this goal will try to cover the root branch
-     * of the method identified by the given name - meaning it will just try to
-     * call the method at hand
-     * <p/>
-     * <p/>
-     * Otherwise this goal will try to reach the given branch and if value is
-     * true, make the branchInstruction jump and visa versa
-     *
-     * @param className       a {@link String} object.
-     * @param methodName      a {@link String} object.
-     * @param argIndex        an argument index.
-     * @param type            a {@link Type} object.
-     * @param valueDescriptor a value descriptor.
-     */
-    public InputCoverageGoal(String className, String methodName, int argIndex, Type type, String valueDescriptor) {
-        this(className, methodName, argIndex, type, valueDescriptor, null);
-    }
+	private final String className;
+	private final String methodName;
+	private final int    argIndex;
+	private final String type;
+	private final String valueDescriptor;
+	private final Number numericValue;
 
-    public InputCoverageGoal(String className, String methodName, int argIndex, Type type, String valueDescriptor, Number numericValue) {
-        if (className == null || methodName == null)
-            throw new IllegalArgumentException("null given");
+	/**
+	 * Can be used to create an arbitrary {@code InputCoverageGoal} trying to cover the
+	 * method such that it returns a given {@code value}
+	 * <p/>
+	 * <p/>
+	 * If the method returns a boolean, this goal will try to cover the method with either {@code true} or {@code false}
+	 * If the given branch is {@code null}, this goal will try to cover the root branch
+	 * of the method identified by the given name - meaning it will just try to
+	 * call the method at hand
+	 * <p/>
+	 * <p/>
+	 * Otherwise this goal will try to reach the given branch and if value is
+	 * true, make the branchInstruction jump and visa versa
+	 *
+	 * @param className       a {@link String} object.
+	 * @param methodName      a {@link String} object.
+	 * @param argIndex        an argument index.
+	 * @param type            a {@link Type} object.
+	 * @param valueDescriptor a value descriptor.
+	 */
+	public InputCoverageGoal(String className, String methodName, int argIndex, Type type, String valueDescriptor) {
+		this(className, methodName, argIndex, type, valueDescriptor, null);
+	}
 
-        this.className = className;
-        this.methodName = methodName;
-        this.argIndex = argIndex;
-        this.type = type.toString();
-        this.valueDescriptor = valueDescriptor;
-        this.numericValue = numericValue;
-    }
+	public InputCoverageGoal(String className, String methodName, int argIndex, Type type, String valueDescriptor, Number numericValue) {
+		if (className == null || methodName == null)
+			throw new IllegalArgumentException("null given");
+
+		this.className = className;
+		this.methodName = methodName;
+		this.argIndex = argIndex;
+		this.type = type.toString();
+		this.valueDescriptor = valueDescriptor;
+		this.numericValue = numericValue;
+	}
 
 
 
-    /**
-     * @return the className
-     */
-    public String getClassName() {
-        return className;
-    }
+	/**
+	 * @return the className
+	 */
+	public String getClassName() {
+		return className;
+	}
 
-    /**
-     * @return the methodName
-     */
-    public String getMethodName() {
-        return methodName;
-    }
+	/**
+	 * @return the methodName
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
 
-    /**
-     * @return the argument index
-     */
-    public int getArgIndex() {
-        return argIndex;
-    }
+	/**
+	 * @return the argument index
+	 */
+	public int getArgIndex() {
+		return argIndex;
+	}
 
-    /**
-     * @return the type
-     */
-    public Type getType() {
-        return Type.getType(type);
-    }
+	/**
+	 * @return the type
+	 */
+	public Type getType() {
+		return Type.getType(type);
+	}
 
-    /**
-     * @return the value
-     */
-    public String getValueDescriptor() {
-        return valueDescriptor;
-    }
+	/**
+	 * @return the value
+	 */
+	public String getValueDescriptor() {
+		return valueDescriptor;
+	}
 
-    public Number getNumericValue() { return numericValue; }
+	public Number getNumericValue() { return numericValue; }
 
-    // inherited from Object
+	// inherited from Object
 
-    /**
-     * {@inheritDoc}
-     * <p/>
-     * Readable representation
-     */
-    @Override
-    public String toString() {
-        return className + "." + methodName + "[" + argIndex + "]:" + valueDescriptor;
-    }
+	/**
+	 * {@inheritDoc}
+	 * <p/>
+	 * Readable representation
+	 */
+	@Override
+	public String toString() {
+		return className + "." + methodName + "[" + argIndex + "]:" + valueDescriptor;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + className.hashCode();
-        result = prime * result + methodName.hashCode();
-        result = prime * result + argIndex;
-        result = prime * result + (type == null ? 0 : type.hashCode());
-        result = prime * result + (valueDescriptor == null ? 0 : valueDescriptor.hashCode());
-        return result;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + className.hashCode();
+		result = prime * result + methodName.hashCode();
+		result = prime * result + argIndex;
+		result = prime * result + (type == null ? 0 : type.hashCode());
+		result = prime * result + (valueDescriptor == null ? 0 : valueDescriptor.hashCode());
+		return result;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 
-        InputCoverageGoal other = (InputCoverageGoal) obj;
+		InputCoverageGoal other = (InputCoverageGoal) obj;
 
-        if (this.argIndex != other.argIndex)
-            return false;
+		if (this.argIndex != other.argIndex)
+			return false;
 
-        if (!this.methodName.equals(other.methodName) && this.className.equals(other.className))
-            return false;
+		if (!this.methodName.equals(other.methodName) && this.className.equals(other.className))
+			return false;
 
-        if ((this.type == null && other.type != null) || (this.type != null && other.type == null))
-            return false;
+		if ((this.type == null && other.type != null) || (this.type != null && other.type == null))
+			return false;
 
-        if (this.type != null && !this.type.equals(other.type))
-            return false;
+		if (this.type != null && !this.type.equals(other.type))
+			return false;
 
-        if ((this.valueDescriptor == null && other.valueDescriptor != null) || (this.valueDescriptor != null && other.valueDescriptor == null))
-            return false;
+		if ((this.valueDescriptor == null && other.valueDescriptor != null) || (this.valueDescriptor != null && other.valueDescriptor == null))
+			return false;
 
-        if (this.valueDescriptor != null && !this.valueDescriptor.equals(other.valueDescriptor))
-            return false;
+		if (this.valueDescriptor != null && !this.valueDescriptor.equals(other.valueDescriptor))
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public int compareTo(InputCoverageGoal o) {
+	@Override
+	public int compareTo(InputCoverageGoal o) {
 
-        int diff = className.compareTo(o.className);
-        if (diff == 0) {
-            int diff2 = methodName.compareTo(o.methodName);
-            if (diff2 == 0) {
-                if (argIndex == o.argIndex) {
-                    int diff3 = type.compareTo(o.type);
-                    if (diff3 == 0)
-                        return this.valueDescriptor.compareTo(o.valueDescriptor);
-                    else
-                        return diff3;
-                } else
-                    return Integer.compare(argIndex, o.argIndex);
-            } else
-                return diff2;
-        } else
-            return diff;
-    }
+		int diff = className.compareTo(o.className);
+		if (diff == 0) {
+			int diff2 = methodName.compareTo(o.methodName);
+			if (diff2 == 0) {
+				if (argIndex == o.argIndex) {
+					int diff3 = type.compareTo(o.type);
+					if (diff3 == 0)
+						return this.valueDescriptor.compareTo(o.valueDescriptor);
+					else
+						return diff3;
+				} else
+					return Integer.compare(argIndex, o.argIndex);
+			} else
+				return diff2;
+		} else
+			return diff;
+	}
 
-    public static Set<InputCoverageGoal> createCoveredGoalsFromParameters(String className, String methodName, String methodDesc, List<Object> argumentsValues) {
-        Set<InputCoverageGoal> goals = new LinkedHashSet<>();
+	public static Set<InputCoverageGoal> createCoveredGoalsFromParameters(String className, String methodName, String methodDesc, List<Object> argumentsValues) {
+		Set<InputCoverageGoal> goals = new LinkedHashSet<>();
 
-        Type[] argTypes = Type.getArgumentTypes(methodDesc);
+		Type[] argTypes = Type.getArgumentTypes(methodDesc);
 
-        for (int i=0;i<argTypes.length;i++) {
-            Type argType = argTypes[i];
-            Object argValue = argumentsValues.get(i);
-            String argValueDesc = "";
-            Number numberValue = null;
-            switch (argType.getSort()) {
-                case Type.BOOLEAN:
-                    argValueDesc = (((boolean) argValue)) ? BOOL_TRUE : BOOL_FALSE;
-                    break;
-                case Type.CHAR:
-                    char c = (char) argValue;
-                    numberValue = (int)c; // Suite fitness uses the numeric representation to estimate distances
-                    if (Character.isAlphabetic(c))
-                        argValueDesc = CHAR_ALPHA;
-                    else if (Character.isDigit(c))
-                        argValueDesc = CHAR_DIGIT;
-                    else
-                        argValueDesc = CHAR_OTHER;
-                    break;
-                case Type.BYTE:
-                case Type.SHORT:
-                case Type.INT:
-                case Type.FLOAT:
-                case Type.LONG:
-                case Type.DOUBLE:
-                    // assert (argValue instanceof Number); // not always true: char can be assigned to integers
-                    double value;
+		for (int i=0;i<argTypes.length;i++) {
+			Type argType = argTypes[i];
+			Object argValue = argumentsValues.get(i);
+			String argValueDesc = "";
+			Number numberValue = null;
+			switch (argType.getSort()) {
+			case Type.BOOLEAN:
+				argValueDesc = (((boolean) argValue)) ? BOOL_TRUE : BOOL_FALSE;
+				break;
+			case Type.CHAR:
+				if (argValue == null){
+					argValueDesc = REF_NULL;
+				} else {
+					char c = (char) argValue;
+					numberValue = (int) c; // Suite fitness uses the numeric representation to estimate distances
+					if (Character.isAlphabetic(c))
+						argValueDesc = CHAR_ALPHA;
+					else if (Character.isDigit(c))
+						argValueDesc = CHAR_DIGIT;
+					else
+						argValueDesc = CHAR_OTHER;
+				}
+				break;
+			case Type.BYTE:
+			case Type.SHORT:
+			case Type.INT:
+			case Type.FLOAT:
+			case Type.LONG:
+			case Type.DOUBLE:
+				// assert (argValue instanceof Number); // not always true: char can be assigned to integers
+				double value;
 
-                    if (argValue instanceof Character) {
-                        value = ((Number) ((int) (char) argValue)).doubleValue();
-                    } else {
-                        value = ((Number) argValue).doubleValue();
-                    }
-                    numberValue = value;
-                    argValueDesc = (value < 0) ? NUM_NEGATIVE : (value == 0) ? NUM_ZERO : NUM_POSITIVE;
-                    break;
-                case Type.ARRAY:
-                    if (argValue == null)
-                        argValueDesc = REF_NULL;
-                    else
-                        argValueDesc = (Array.getLength(argValue) == 0) ? ARRAY_EMPTY : ARRAY_NONEMPTY;
-                    break;
-                case Type.OBJECT:
-                    if (argValue == null)
-                        argValueDesc = REF_NULL;
-                    else {
-                        if (argType.getClassName().equals("java.lang.String")) {
-                            argValueDesc = ((String) argValue).isEmpty() ? STRING_EMPTY : STRING_NONEMPTY;
-                        }
-                        else if(argValue instanceof List) {
-                            argValueDesc = ((List) argValue).isEmpty() ? LIST_EMPTY : LIST_NONEMPTY;
-                        }
-                        else if(argValue instanceof Set) {
-                            argValueDesc = ((Set) argValue).isEmpty() ? SET_EMPTY : SET_NONEMPTY;
-                        }
-                        else if(argValue instanceof Map) {
-                            argValueDesc = ((Map) argValue).isEmpty() ? MAP_EMPTY : MAP_NONEMPTY;
-                        }
-                        else
-                            argValueDesc = REF_NONNULL;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            if (!argValueDesc.isEmpty())
-                goals.add(new InputCoverageGoal(className, methodName+methodDesc, i, argType, argValueDesc, numberValue));
-        }
+				if (argValue == null){
+					argValueDesc = REF_NULL;
+				} else {
+					if (argValue instanceof Character) {
+						value = ((Number) ((int) (char) argValue)).doubleValue();
+					} else {
+						value = ((Number) argValue).doubleValue();
+					}
+					numberValue = value;
+					argValueDesc = (value < 0) ? NUM_NEGATIVE : (value == 0) ? NUM_ZERO : NUM_POSITIVE;
+				} 
+				break;
+			case Type.ARRAY:
+				if (argValue == null)
+					argValueDesc = REF_NULL;
+				else
+					argValueDesc = (Array.getLength(argValue) == 0) ? ARRAY_EMPTY : ARRAY_NONEMPTY;
+				break;
+			case Type.OBJECT:
+				if (argValue == null)
+					argValueDesc = REF_NULL;
+				else {
+					if (argType.getClassName().equals("java.lang.String")) {
+						argValueDesc = ((String) argValue).isEmpty() ? STRING_EMPTY : STRING_NONEMPTY;
+					}
+					else if(argValue instanceof List) {
+						argValueDesc = ((List) argValue).isEmpty() ? LIST_EMPTY : LIST_NONEMPTY;
+					}
+					else if(argValue instanceof Set) {
+						argValueDesc = ((Set) argValue).isEmpty() ? SET_EMPTY : SET_NONEMPTY;
+					}
+					else if(argValue instanceof Map) {
+						argValueDesc = ((Map) argValue).isEmpty() ? MAP_EMPTY : MAP_NONEMPTY;
+					}
+					else
+						argValueDesc = REF_NONNULL;
+				}
+				break;
+			default:
+				break;
+			}
+			if (!argValueDesc.isEmpty())
+				goals.add(new InputCoverageGoal(className, methodName+methodDesc, i, argType, argValueDesc, numberValue));
+		}
 
-        return goals;
-    }
+		return goals;
+	}
 }
