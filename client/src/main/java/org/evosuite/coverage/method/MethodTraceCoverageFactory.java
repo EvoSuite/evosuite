1022b4b1083c3548bda2f0585abfd3309da8df27
jose <zecapistolas@gmail.com>
2018-01-17 13:33:20 +0000

    Ignore methods of enum classes.

--- client/src/main/java/org/evosuite/coverage/method/MethodTraceCoverageFactory.java
+++ client/src/main/java/org/evosuite/coverage/method/MethodTraceCoverageFactory.java
@@ -108,6 +108,13 @@ public class MethodTraceCoverageFactory extends
 		Method[] allMethods = clazz.getDeclaredMethods();
 		for (Method m : allMethods) {
 			if (TestUsageChecker.canUse(m)) {
+				if(clazz.isEnum()) {
+					if (m.getName().equals("valueOf") || m.getName().equals("values")
+							|| m.getName().equals("ordinal")) {
+						logger.debug("Excluding valueOf for Enum " + m.toString());
+						continue;
+					}
+				}
 				String methodName = m.getName() + Type.getMethodDescriptor(m);
 				logger.info("Adding goal for method " + className + "." + methodName);
 				goals.add(new MethodTraceCoverageTestFitness(className, methodName));
