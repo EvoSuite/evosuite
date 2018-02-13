1255f619e008e4c3b3219689def7960aa9881d44
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-02-13 19:19:29 +0100

    Fix updating of variables in container assertion

--- client/src/main/java/org/evosuite/assertion/ContainsAssertion.java
+++ client/src/main/java/org/evosuite/assertion/ContainsAssertion.java
@@ -6,6 +6,7 @@ import org.evosuite.testcase.execution.Scope;
 import org.evosuite.testcase.variable.VariableReference;
 
 import java.util.Collection;
+import java.util.Set;
 
 public class ContainsAssertion extends Assertion {
 
@@ -56,5 +57,19 @@ public class ContainsAssertion extends Assertion {
         } catch (CodeUnderTestException e) {
             throw new UnsupportedOperationException();
         }
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.evosuite.assertion.Assertion#getReferencedVariables()
+     */
+    /** {@inheritDoc} */
+    @Override
+    public Set<VariableReference> getReferencedVariables() {
+        Set<VariableReference> vars = super.getReferencedVariables();
+        vars.add(source);
+        vars.add(containedVariable);
+        return vars;
     }
 }
