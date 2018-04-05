d34a18e2ec3770470fc048b4535546ed936040dd
Gordon Fraser <gordon.fraser@uni-passau.de>
2018-04-05 12:33:16 +0200

    Release result scripts

--- release_results/script/generateHtml.R
+++ release_results/script/generateHtml.R
@@ -1,6 +1,6 @@
 source("util.R")
 
-VERSION = "1_0_5"
+VERSION = "1_0_6"
 
 GENERATED_FILES = paste("../generated_files","/",VERSION,sep="")
 SELECTION_FILE = paste("../generated_files","/","selection.txt",sep="")
