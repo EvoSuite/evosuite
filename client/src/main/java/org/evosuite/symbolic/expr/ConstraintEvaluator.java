35c8b10cd5a7073da38a574348404b54096c42b7
Juan Pablo Galeotti <jgaleotti@dc.uba.ar>
2018-08-07 16:12:01 -0300

    adopted evosuite eclipse code style

--- client/src/main/java/org/evosuite/symbolic/expr/ConstraintEvaluator.java
+++ client/src/main/java/org/evosuite/symbolic/expr/ConstraintEvaluator.java
@@ -1,21 +1,18 @@
 /**
- * Copyright (C) 2010-2018 Gordon Fraser, Andrea Arcuri and EvoSuite
- * contributors
+ * Copyright (C) 2010-2018 Gordon Fraser, Andrea Arcuri and EvoSuite contributors
  *
  * This file is part of EvoSuite.
  *
- * EvoSuite is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3.0 of the License, or
- * (at your option) any later version.
+ * EvoSuite is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3.0 of
+ * the License, or (at your option) any later version.
  *
- * EvoSuite is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * EvoSuite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with EvoSuite. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 package org.evosuite.symbolic.expr;
 
@@ -39,87 +36,87 @@ import org.slf4j.LoggerFactory;
 
 public class ConstraintEvaluator implements ConstraintVisitor<Object, Void> {
 
-	static Logger log = LoggerFactory.getLogger(ConstraintEvaluator.class);
+  static Logger log = LoggerFactory.getLogger(ConstraintEvaluator.class);
 
-	@Override
-	public Object visit(IntegerConstraint n, Void arg) {
+  @Override
+  public Object visit(IntegerConstraint n, Void arg) {
 
-		ExpressionEvaluator visitor = new ExpressionEvaluator();
-		long left = (Long) n.getLeftOperand().accept(visitor, null);
-		long right = (Long) n.getRightOperand().accept(visitor, null);
+    ExpressionEvaluator visitor = new ExpressionEvaluator();
+    long left = (Long) n.getLeftOperand().accept(visitor, null);
+    long right = (Long) n.getRightOperand().accept(visitor, null);
 
-		Comparator cmpr = n.getComparator();
+    Comparator cmpr = n.getComparator();
 
-		switch (cmpr) {
-		case EQ:
-			return left == right;
-		case NE:
-			return left != right;
-		case LT:
-			return left < right;
-		case LE:
-			return left <= right;
-		case GT:
-			return left > right;
-		case GE:
-			return left >= right;
-		default:
-			throw new IllegalArgumentException("unimplemented comparator");
-		}
-	}
+    switch (cmpr) {
+      case EQ:
+        return left == right;
+      case NE:
+        return left != right;
+      case LT:
+        return left < right;
+      case LE:
+        return left <= right;
+      case GT:
+        return left > right;
+      case GE:
+        return left >= right;
+      default:
+        throw new IllegalArgumentException("unimplemented comparator");
+    }
+  }
 
-	@Override
-	public Object visit(RealConstraint n, Void arg) {
-		ExpressionEvaluator visitor = new ExpressionEvaluator();
-		double left = (Double) n.getLeftOperand().accept(visitor, null);
-		double right = (Double) n.getRightOperand().accept(visitor, null);
+  @Override
+  public Object visit(RealConstraint n, Void arg) {
+    ExpressionEvaluator visitor = new ExpressionEvaluator();
+    double left = (Double) n.getLeftOperand().accept(visitor, null);
+    double right = (Double) n.getRightOperand().accept(visitor, null);
 
-		Comparator cmpr = n.getComparator();
+    Comparator cmpr = n.getComparator();
 
-		switch (cmpr) {
-		case EQ:
-			return left == right;
-		case NE:
-			return left != right;
-		case LT:
-			return left < right;
-		case LE:
-			return left <= right;
-		case GT:
-			return left > right;
-		case GE:
-			return left >= right;
-		default:
-			throw new IllegalArgumentException("unimplemented comparator");
-		}
+    switch (cmpr) {
+      case EQ:
+        return left == right;
+      case NE:
+        return left != right;
+      case LT:
+        return left < right;
+      case LE:
+        return left <= right;
+      case GT:
+        return left > right;
+      case GE:
+        return left >= right;
+      default:
+        throw new IllegalArgumentException("unimplemented comparator");
+    }
 
-	}
+  }
 
-	@Override
-	public Object visit(StringConstraint n, Void arg) {
+  @Override
+  public Object visit(StringConstraint n, Void arg) {
 
-		ExpressionEvaluator visitor = new ExpressionEvaluator();
-		long left = (Long) n.getLeftOperand().accept(visitor, null);
-		long right = (Long) n.getRightOperand().accept(visitor, null);
-		Comparator cmpr = n.getComparator();
+    ExpressionEvaluator visitor = new ExpressionEvaluator();
+    long left = (Long) n.getLeftOperand().accept(visitor, null);
+    long right = (Long) n.getRightOperand().accept(visitor, null);
+    Comparator cmpr = n.getComparator();
 
-		switch (cmpr) {
-		case EQ:
-			return left == right;
-		case NE:
-			return left != right;
-		case LT:
-			return left < right;
-		case LE:
-			return left <= right;
-		case GT:
-			return left > right;
-		case GE:
-			return left >= right;
-		default:
-			throw new IllegalArgumentException("unimplemented comparator");
-		}
+    switch (cmpr) {
+      case EQ:
+        return left == right;
+      case NE:
+        return left != right;
+      case LT:
+        return left < right;
+      case LE:
+        return left <= right;
+      case GT:
+        return left > right;
+      case GE:
+        return left >= right;
+      default:
+        throw new IllegalArgumentException("unimplemented comparator");
+    }
 
-	}
+  }
 
 }
