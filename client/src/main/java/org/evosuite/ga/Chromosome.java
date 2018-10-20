4dd83ab607c7e75d440166a3940c4386d03c891a
VoglSebastian <VoglSebastian@users.noreply.github.com>
2018-10-20 10:28:38 +0200

    Update Chromosome.java

--- client/src/main/java/org/evosuite/ga/Chromosome.java
+++ client/src/main/java/org/evosuite/ga/Chromosome.java
@@ -24,7 +24,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import net.bytebuddy.pool.TypePool;
 import org.evosuite.Properties;
 import org.evosuite.ga.localsearch.LocalSearchObjective;
 import org.evosuite.utils.PublicCloneable;
