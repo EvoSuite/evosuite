4030952ccd4629348ed3257e006b8deff4adec4a
Sebastian Schweikl <schweikl@posteo.de>
2019-09-05 15:23:58 +0200

    GuidedInsertion: implement singleton design pattern
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
+++ client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
@@ -3,8 +3,20 @@ package org.evosuite.testcase.mutation;
 import org.evosuite.testcase.TestCase;
 
 public class GuidedInsertion implements InsertionStrategy {
+
+    // singleton design pattern, use getInstance() instead
+    private GuidedInsertion() { }
+
+    public static GuidedInsertion getInstance() {
+        return SingletonContainer.instance;
+    }
+
     @Override
     public int insertStatement(TestCase test, int lastPosition) {
-        return 0;
+        throw new RuntimeException("not implemented");
+    }
+
+    private static final class SingletonContainer {
+        private static final GuidedInsertion instance = new GuidedInsertion();
     }
 }
