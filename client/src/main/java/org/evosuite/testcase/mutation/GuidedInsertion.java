5f6a65e542ac2f46546091df397c151dc596931b
Sebastian Schweikl <schweikl@posteo.de>
2019-09-05 15:27:04 +0200

    GuidedInsertion: add method for updating currently targeted coverage goals
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
+++ client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
@@ -1,14 +1,34 @@
 package org.evosuite.testcase.mutation;
 
 import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.TestFitnessFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
 
 public class GuidedInsertion implements InsertionStrategy {
+
+    private static final Logger logger = LoggerFactory.getLogger(GuidedInsertion.class);
+
+    private Set<TestFitnessFunction> goals = Collections.emptySet();
 
     // singleton design pattern, use getInstance() instead
     private GuidedInsertion() { }
 
     public static GuidedInsertion getInstance() {
         return SingletonContainer.instance;
+    }
+
+    /**
+     * Sets the goals to be targeted during the insertion process.
+     *
+     * @param goals the goals intended for covering
+     */
+    public void setGoals(final Set<TestFitnessFunction> goals) {
+        this.goals = Objects.requireNonNull(goals);
     }
 
     @Override
