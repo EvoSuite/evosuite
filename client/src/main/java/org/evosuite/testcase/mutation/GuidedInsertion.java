f16ee826aaf9f6d71b539ed35b27771c28d54dfb
Sebastian Schweikl <schweikl@posteo.de>
2019-09-05 15:22:25 +0200

    Add scaffolding class for guided insertion strategy
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

---
+++ client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
@@ -0,0 +1,10 @@
+package org.evosuite.testcase.mutation;
+
+import org.evosuite.testcase.TestCase;
+
+public class GuidedInsertion implements InsertionStrategy {
+    @Override
+    public int insertStatement(TestCase test, int lastPosition) {
+        return 0;
+    }
+}
