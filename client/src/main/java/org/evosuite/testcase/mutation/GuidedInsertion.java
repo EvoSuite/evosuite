f28c5c5f3e2ca4d4967b117f097b9d28c60bc8b2
Sebastian Schweikl <schweikl@posteo.de>
2019-09-05 15:35:57 +0200

    insert call to UUT, choosing targeted goal via roulette wheel selection
    Signed-off-by: Sebastian Schweikl <schweikl@posteo.de>

--- client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
+++ client/src/main/java/org/evosuite/testcase/mutation/GuidedInsertion.java
@@ -1,12 +1,21 @@
 package org.evosuite.testcase.mutation;
 
+import org.evosuite.Properties;
+import org.evosuite.TestGenerationContext;
 import org.evosuite.setup.TestCluster;
+import org.evosuite.symbolic.instrument.ClassLoaderUtils;
 import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.TestFactory;
 import org.evosuite.testcase.TestFitnessFunction;
 import org.evosuite.utils.Randomness;
+import org.evosuite.utils.generic.GenericConstructor;
+import org.evosuite.utils.generic.GenericExecutable;
+import org.evosuite.utils.generic.GenericMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Objects;
@@ -34,6 +43,34 @@ public class GuidedInsertion implements InsertionStrategy {
      */
     public void setGoals(final Set<TestFitnessFunction> goals) {
         this.goals = Objects.requireNonNull(goals);
+    }
+
+    boolean insertUUT(final TestCase test, final int lastPosition) {
+        Objects.requireNonNull(test, "mutation: test case to modify must not be null");
+
+        if (!test.isEmpty() && lastPosition < 0) {
+            throw new IllegalArgumentException("illegal position for statement insertion");
+        }
+
+        if (goals.isEmpty()) { // all goals have been covered
+            info("mutation: no more goals to cover, returning");
+            return false; // TODO return true or false?
+        }
+
+        if (!Properties.PURE_INSPECTORS) {
+            warn("Purity analysis is disabled, use the switch -Dpure_inspectors=true to enable");
+        }
+
+        /*
+         * Biased random selection of a goal based on the inverse cyclomatic complexity of its
+         * target method. The rationale is to prefer "easier" (i.e., less complex) targets over
+         * "harder" ones since easier targets are often easier to cover. Hopefully, this increases
+         * coverage fast by covering easy targets first, and reduces the number of cases where we
+         * spend too much search budget on infeasible goals.
+         */
+        final TestFitnessFunction currentGoal = rouletteWheelSelect(goals);
+
+        return insertCallFor(test, currentGoal, lastPosition); // TODO: lastPosition + 1???
     }
 
     /**
@@ -151,6 +188,191 @@ public class GuidedInsertion implements InsertionStrategy {
             }
             return lastIndex;
         }
+    }
+
+    /**
+     * Tries to append a statement calling the target method or target constructor of the given
+     * coverage goal to the end of specified test case. If everything succeeds, a {@code
+     * VariableReference} to the return value of aforementioned statement is returned. Otherwise,
+     * if there is an error, {@code null} is returned.
+     *
+     * @param test the test case to which the call of the target method should be appended
+     * @param goal the coverage goal (target class and method) that should be attempted to reach
+     * @param lastPos the position of the last valid statement of {@code test}
+     * @return a reference to the return value of the statement calling the target method, or
+     * {@code null} if unsuccessful
+     */
+    private boolean insertCallFor(final TestCase test, final TestFitnessFunction goal,
+                                  final int lastPos) {
+        // Tries to reflect the target class.
+        final Class<?> clazz = getTargetClass(goal);
+        if (clazz == null) { // unable to reflect target class
+            return false;
+        }
+
+        // Tries to reflect the target executable (either method or constructor).
+        final GenericExecutable<?, ?> executable = getTargetExecutable(goal, clazz);
+        if (executable == null) { // unable to reflect target executable
+            return false;
+        }
+
+        return TestFactory.getInstance().insertCallFor(test, executable, lastPos + 1);
+
+        /*
+         * In the given test case, tries to find an object which acts as callee for the goal's
+         * target executable. If the latter is static or a constructor, we can omit this step. However,
+         * if the target executable is a non-static method we have to find or create an appropriate
+         * instance of the target class before we can call the method.
+         */
+        /*
+        final VariableReference callee;
+        if (executable.isStatic() || executable.isConstructor()) {
+            callee = null; // There is no callee, and we encode this fact by setting it to null.
+        } else { // non-static (virtual) method
+            callee = findOrCreateObject(clazz, test, lastPos);
+            if (callee == null) { // unable to find or create a matching object
+                return null;
+            }
+        }
+
+        // Tries to generate the required input parameters for the method or constructor to call.
+        final List<VariableReference> parameters = satisfyParameters(test, callee, executable, lastPos);
+        if (parameters == null) { // unable to satisfy all input parameters
+            return null;
+        }
+
+        // Creates an appropriate method statement or constructor statement.
+        final Statement statement;
+        if (executable.isConstructor()) {
+            final GenericConstructor constructor = (GenericConstructor) executable;
+            statement = new ConstructorStatement(test, constructor, parameters);
+        } else {
+            final GenericMethod method = (GenericMethod) executable;
+            statement = new MethodStatement(test, method, callee, parameters);
+        }
+
+        // Adds the statement calling the target executable at the end of the test case.
+        // Also returns the variable reference to the return value of that statement.
+        return test.addStatement(statement);
+         */
+    }
+
+    /**
+     * Returns the {@code Class} instance for the target class of the specified goal, using the
+     * fully qualified class name as given by {@link TestFitnessFunction#getTargetClass()}. If
+     * no {@code Class} definition can be found for that name {@code null} is returned.
+     *
+     * @param goal the goal for which to return the target {@code Class}
+     * @return the corresponding {@code Class} instance or {@code null} if no definition is found
+     */
+    private static Class<?> getTargetClass(final TestFitnessFunction goal) {
+        final String name = goal.getTargetClass();
+        try {
+            return TestCluster.getInstance().getClass(name);
+        } catch (ClassNotFoundException e) {
+            error("Unable to reflect unknown class {}", name);
+            return null;
+        }
+    }
+
+    /**
+     * <p>
+     * Tries to reflect the method or constructor targeted by the given coverage goal in the
+     * specified class {@code clazz}, and creates a corresponding {@code GenericMethod} or
+     * {@code GenericConstructor} as appropriate. Callers may safely downcast the returned
+     * {@code GenericExecutableMember} to a {@code GenericMethod} or {@code GenericConstructor}
+     * by checking the concrete subtype via the methods {@link GenericExecutable#isMethod()
+     * isMethod()} and {@link GenericExecutable#isConstructor() isConstructor()}.
+     * </p>
+     * <p>
+     * The correct {@code Class} instance for {@code clazz} can be obtained by calling
+     * {@link GuidedInsertion#getTargetClass(TestFitnessFunction) getTargetClass()}
+     * beforehand.
+     * </p>
+     * <p>
+     * If no public member matching the name and descriptor given by
+     * {@link TestFitnessFunction#getTargetMethod()} can be found as part of the class
+     * represented by {@code clazz}, {@code null} is returned.
+     * </p>
+     *
+     * @param goal  the goal for which to return the target method or constructor
+     * @param clazz the {@code Class} instance representing the goal's target class
+     * @return the {@code GenericExecutableMember} object that represents the goal's target method
+     * or constructor, or {@code null} if no such method or constructor can be found
+     */
+    private static GenericExecutable<?, ?> getTargetExecutable(final TestFitnessFunction goal,
+                                                               final Class<?> clazz) {
+        final String nameDesc = goal.getTargetMethod();
+
+        // nameDesc = name + descriptor, we have to split it into two parts to work with it
+        final int descriptorStartIndex = nameDesc.indexOf('(');
+        assert descriptorStartIndex > 0 : "malformed method name or descriptor";
+        final String name = nameDesc.substring(0, descriptorStartIndex);
+        final String descriptor = nameDesc.substring(descriptorStartIndex);
+
+        // Tries to reflect the argument types.
+        final Class<?>[] argumentTypes;
+        final ClassLoader classLoader = TestGenerationContext.getInstance().getClassLoaderForSUT();
+        try {
+            argumentTypes = ClassLoaderUtils.getArgumentClasses(classLoader, descriptor);
+        } catch (Throwable t) {
+            error("Unable to reflect argument types of method {}", nameDesc);
+            error("\tCause: {}", t.getMessage());
+            return null;
+        }
+
+        final boolean isConstructor = name.equals("<init>");
+        if (isConstructor) {
+            return new GenericConstructor(getConstructor(clazz, argumentTypes), clazz);
+        } else {
+            return new GenericMethod(getMethod(clazz, name, argumentTypes), clazz);
+        }
+    }
+
+    /**
+     * Tries to reflect the public constructor of the class represented by {@code clazz} with the
+     * parameter list specified by {@code argumentTypes}. If no such constructor can be found
+     * {@code null} is returned.
+     *
+     * @param clazz         the class whose constructor to reflect
+     * @param argumentTypes the argument types of the constructor to reflect
+     * @return a {@code Constructor} object that represents the desired constructor, or {@code null}
+     * if no such constructor can be found
+     */
+    private static Constructor<?> getConstructor(final Class<?> clazz,
+                                                 final Class<?>[] argumentTypes) {
+        final Constructor<?> constructor;
+        try {
+            constructor = clazz.getConstructor(argumentTypes);
+        } catch (NoSuchMethodException e) {
+            error("No constructor of {} with argument types {}", clazz.getName(), argumentTypes);
+            return null;
+        }
+        return constructor;
+    }
+
+    /**
+     * Tries to reflect the public method with the given {@code name} and argument types in the
+     * class represented by {@code clazz}. If no such method can be found {@code null} is returned.
+     *
+     * @param clazz         representation of the class containing the method to reflect
+     * @param name          name of the method to reflect
+     * @param argumentTypes representation of the argument types of the method to reflect
+     * @return a {@code Method} object representing the specified method, or {@code null} if no
+     * appropriate method can be found
+     */
+    private static Method getMethod(final Class<?> clazz,
+                                    final String name,
+                                    final Class<?>[] argumentTypes) {
+        final Method method;
+        try {
+            method = clazz.getMethod(name, argumentTypes);
+        } catch (NoSuchMethodException e) {
+            error("No method with name {} and arguments {} in {}", name, argumentTypes,
+                    clazz.getName());
+            return null;
+        }
+        return method;
     }
 
     /**
