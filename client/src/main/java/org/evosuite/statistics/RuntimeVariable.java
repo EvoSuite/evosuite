1d40c2254ecf8858197885d84ce9ebd35eede32e
jose <jcmcampos@fc.ul.pt>
2020-05-11 16:52:38 +0100

    Removed ,

--- client/src/main/java/org/evosuite/statistics/RuntimeVariable.java
+++ client/src/main/java/org/evosuite/statistics/RuntimeVariable.java
@@ -403,7 +403,7 @@ public enum RuntimeVariable {
 	/* -------------------------------------------------------------------- */
 	/** Search budget needed to reach the maximum coverage */ 
 	/** Used in the comparison between LISP and MOSA */
-	Time2MaxCoverage,
+	Time2MaxCoverage
 	
 	;
 	/* -------------------------------------------------- */
