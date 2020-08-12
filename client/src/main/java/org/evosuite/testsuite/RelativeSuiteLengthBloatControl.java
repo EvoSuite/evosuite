f4928370925dedabb0a28e0f64120302b16944f7
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2020-08-12 18:41:47 +0200

    Update default and copy ctor for RelativeSuiteLengthBloatControl

--- client/src/main/java/org/evosuite/testsuite/RelativeSuiteLengthBloatControl.java
+++ client/src/main/java/org/evosuite/testsuite/RelativeSuiteLengthBloatControl.java
@@ -40,10 +40,15 @@ public class RelativeSuiteLengthBloatControl<T extends Chromosome<T>> implements
 	/**
 	 * Longest individual in current generation
 	 */
-	protected int current_max = 0;
+	protected int current_max;
 
-	protected double best_fitness = Double.MAX_VALUE; // FIXXME: Assuming
-	                                                  // minimizing fitness!
+	protected double best_fitness;
+
+	public RelativeSuiteLengthBloatControl() {
+		current_max = 0;
+		best_fitness = Double.MAX_VALUE; // FIXXME: Assuming
+										 // minimizing fitness!
+	}
 
 	public RelativeSuiteLengthBloatControl(final RelativeSuiteLengthBloatControl<?> that) {
 		this.current_max = that.current_max;
