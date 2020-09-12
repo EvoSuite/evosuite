8faa981cb4f75f388503e93290dfa4f29e02d389
Gordon Fraser <gordon.fraser@uni-passau.de>
2020-09-12 14:23:27 +0200

    Remove obsolete method

--- client/src/main/java/org/evosuite/testcase/execution/ExecutionTracer.java
+++ client/src/main/java/org/evosuite/testcase/execution/ExecutionTracer.java
@@ -420,30 +420,6 @@ public class ExecutionTracer {
 	}
 
 	/**
-	 * Called by the instrumented code each time an unconditional branch is
-	 * taken. This is not enabled by default, only some coverage criteria (e.g.,
-	 * LCSAJ) use it.
-	 * 
-	 * @param opcode
-	 *            a int.
-	 * @param branch
-	 *            a int.
-	 * @param bytecode_id
-	 *            a int.
-	 */
-	public static void passedUnconditionalBranch(int opcode, int branch, int bytecode_id) {
-		ExecutionTracer tracer = getExecutionTracer();
-		if (tracer.disabled)
-			return;
-
-		if (isThreadNeqCurrentThread())
-			return;
-
-		// Add current branch to control trace
-		tracer.trace.branchPassed(branch, bytecode_id, 0.0, 0.0);
-	}
-
-	/**
 	 * Called by the instrumented code each time a new branch is taken
 	 * 
 	 * @param val
