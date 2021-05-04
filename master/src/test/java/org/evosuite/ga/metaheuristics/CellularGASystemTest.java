cfd51ad5e15658ba3e47c00d32f4c7d1cf109ea3
VoglSebastian <voglseb@fim.uni-passau.de>
2021-05-04 11:26:44 +0200

    Replaced tabs with 4 whitespaces in CellularGASystemTest

--- master/src/test/java/org/evosuite/ga/metaheuristics/CellularGASystemTest.java
+++ master/src/test/java/org/evosuite/ga/metaheuristics/CellularGASystemTest.java
@@ -41,47 +41,47 @@ import com.examples.with.different.packagename.XMLElement2;
  *
  */
 public class CellularGASystemTest extends SystemTestBase{
-	
-	public List<Chromosome<?>> setup(StoppingCondition sc, int budget, String cut){
-		Properties.CRITERION = new Criterion[1];
-		Properties.CRITERION[0] = Criterion.BRANCH;
-		Properties.ALGORITHM = Algorithm.CELLULAR_GA;
-	    Properties.POPULATION = 50;
-	    Properties.STOPPING_CONDITION = sc;
-	    Properties.SEARCH_BUDGET = budget;
-	    Properties.MINIMIZE = false;
 
-	    EvoSuite evosuite = new EvoSuite();
+    public List<Chromosome<?>> setup(StoppingCondition sc, int budget, String cut){
+        Properties.CRITERION = new Criterion[1];
+        Properties.CRITERION[0] = Criterion.BRANCH;
+        Properties.ALGORITHM = Algorithm.CELLULAR_GA;
+        Properties.POPULATION = 50;
+        Properties.STOPPING_CONDITION = sc;
+        Properties.SEARCH_BUDGET = budget;
+        Properties.MINIMIZE = false;
 
-		Properties.TARGET_CLASS = cut;
+        EvoSuite evosuite = new EvoSuite();
 
-	    String[] command = new String[] {"-generateSuite", "-class", cut};
+        Properties.TARGET_CLASS = cut;
 
-	    Object result = evosuite.parseCommandLine(command);
-	    Assert.assertNotNull(result);
+        String[] command = new String[] {"-generateSuite", "-class", cut};
 
-	    GeneticAlgorithm<?> ga = getGAFromResult(result);
+        Object result = evosuite.parseCommandLine(command);
+        Assert.assertNotNull(result);
 
-		return new ArrayList<>(ga.getBestIndividuals());
-	}
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
 
-	@Test
-	public void testCellularGAWithLimitedTime(){
-		
-		List<Chromosome<?>> population = this.setup(StoppingCondition.MAXTIME, 15, XMLElement2.class.getCanonicalName());
-		
-	    for (Chromosome<?> p : population) {
+        return new ArrayList<>(ga.getBestIndividuals());
+    }
+
+    @Test
+    public void testCellularGAWithLimitedTime(){
+
+        List<Chromosome<?>> population = this.setup(StoppingCondition.MAXTIME, 15, XMLElement2.class.getCanonicalName());
+
+        for (Chromosome<?> p : population) {
             Assert.assertNotEquals(p.getCoverage(), 1.0);
         }
-	}
-	
-	@Test
-	public void testCellularGAWithLimitedGenerations(){
-		
-	    List<Chromosome<?>> population = this.setup(StoppingCondition.MAXGENERATIONS, 10, ClassHierarchyIncludingInterfaces.class.getCanonicalName());
-	    
-	    for (Chromosome<?> p : population) {
+    }
+
+    @Test
+    public void testCellularGAWithLimitedGenerations(){
+
+        List<Chromosome<?>> population = this.setup(StoppingCondition.MAXGENERATIONS, 10, ClassHierarchyIncludingInterfaces.class.getCanonicalName());
+
+        for (Chromosome<?> p : population) {
             Assert.assertNotEquals(p.getCoverage(), 1.0);
         }
-	}
+    }
 }
