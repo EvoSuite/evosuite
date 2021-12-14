65544fb308d055ad5fbf69934f8606b0ea797d83
João Afonso <johnlandon1999@gmail.com>
2021-12-14 16:54:39 +0000

    DummySystemTest

---
+++ master/src/test/java/org/evosuite/DummySystemTest.java
@@ -0,0 +1,27 @@
+package org.evosuite;
+
+import com.examples.with.different.packagename.mutation.SimpleMutationExample2;
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.strategy.TestGenerationStrategy;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummySystemTest extends SystemTestBase{
+
+    @Test
+    public void test1() {
+        EvoSuite evosuite = new EvoSuite();
+
+        String targetClass = SimpleMutationExample2.class.getCanonicalName();
+
+        String[] command = new String[] { "-generateMOSuite", "-class", targetClass };
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<TestSuiteChromosome> ga = getGAFromResult(result);
+        TestSuiteChromosome best = ga.getBestIndividual();
+
+        System.out.println("CoveredGoals:\n" + best.getCoveredGoals());
+        System.out.println("EvolvedTestSuite:\n" + best);
+    }
+
+}
