ff0f809c179cd622e24bca114eee45c044bc6aa9
João Afonso <johnlandon1999@gmail.com>
2021-12-21 14:55:59 +0000

    BrittleAssertion and UnusedInputs

---
+++ client/src/main/java/org/evosuite/testsmells/smells/BrittleAssertion.java
@@ -0,0 +1,30 @@
+package org.evosuite.testsmells.smells;
+
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.statements.MethodStatement;
+import org.evosuite.testsmells.AbstractTestSmell;
+import org.evosuite.testcase.statements.Statement;
+
+public class BrittleAssertion extends AbstractTestSmell {
+
+    public BrittleAssertion(String smellName) {
+        super(smellName);
+    }
+
+    @Override
+    public int obtainSmellCount(TestChromosome chromosome) {
+        int size = chromosome.size();
+        int count = 0;
+
+        Statement currentStatement;
+
+        for (int i = 0; i < size; i++){
+            currentStatement = chromosome.getTestCase().getStatement(i);
+
+            if(!(currentStatement instanceof MethodStatement)){
+                count += currentStatement.hasAssertions() ? 1 : 0;
+            }
+        }
+        return count;
+    }
+}
