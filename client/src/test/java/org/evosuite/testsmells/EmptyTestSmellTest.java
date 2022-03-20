c94702b6836ea37343a8362d84aba8a86c87d6a9
João Afonso <johnlandon1999@gmail.com>
2022-03-20 23:51:41 +0000

    EmptyTest & IndirectTesting unit tests

---
+++ client/src/test/java/org/evosuite/testsmells/EmptyTestSmellTest.java
@@ -0,0 +1,100 @@
+package org.evosuite.testsmells;
+
+import com.examples.with.different.packagename.testsmells.TestSmellsTestingClass1;
+import org.evosuite.Properties;
+import org.evosuite.assertion.PrimitiveAssertion;
+import org.evosuite.symbolic.TestCaseBuilder;
+import org.evosuite.testcase.DefaultTestCase;
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.statements.Statement;
+import org.evosuite.testcase.variable.VariableReference;
+import org.evosuite.testsmells.smells.EmptyTest;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmptyTestSmellTest {
+
+    AbstractTestCaseSmell emptyTest;
+
+    @Before
+    public void setUp() {
+        Properties.TARGET_CLASS = TestSmellsTestingClass1.class.getCanonicalName();
+        this.emptyTest = new EmptyTest();
+    }
+
+    @Test
+    public void testNonEmptyTestCase() throws NoSuchMethodException {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase0();
+        testCase.setTestCase(test0);
+
+        int smellCount = this.emptyTest.computeNumberOfSmells(testCase);
+        int expected = 0;
+        assertEquals(expected, smellCount);
+    }
+
+    @Test
+    public void testEmptyTestCase() {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase1();
+        testCase.setTestCase(test0);
+
+        int smellCount = this.emptyTest.computeNumberOfSmells(testCase);
+        int expected = Integer.MAX_VALUE;
+        assertEquals(expected, smellCount);
+    }
+
+    @Test
+    public void testFullTestSuite() throws NoSuchMethodException {
+        TestSuiteChromosome suite = new TestSuiteChromosome();
+        DefaultTestCase test0 = createTestCase0();
+        DefaultTestCase test1 = createTestCase1();
+        suite.addTest(test0);
+        suite.addTest(test1);
+
+        int smellCount = this.emptyTest.computeNumberOfSmells(suite);
+        int expected = Integer.MAX_VALUE;
+        assertEquals(expected, smellCount);
+    }
+
+    private DefaultTestCase createTestCase0() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        Statement currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase1() {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+        return builder.getDefaultTestCase();
+    }
+}
