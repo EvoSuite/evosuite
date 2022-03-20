4f0fa467edf92860acb416e125329398d48b7aad
João Afonso <johnlandon1999@gmail.com>
2022-03-20 23:27:02 +0000

    IndirectTesting update

--- client/src/main/java/org/evosuite/testsmells/smells/IndirectTesting.java
+++ client/src/main/java/org/evosuite/testsmells/smells/IndirectTesting.java
@@ -1,11 +1,15 @@
 package org.evosuite.testsmells.smells;
 
 import org.evosuite.Properties;
+import org.evosuite.assertion.Assertion;
+import org.evosuite.assertion.InspectorAssertion;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.statements.MethodStatement;
 import org.evosuite.testcase.statements.Statement;
 import org.evosuite.testsmells.AbstractTestCaseSmell;
-import org.evosuite.utils.generic.GenericMethod;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 public class IndirectTesting extends AbstractTestCaseSmell {
 
@@ -20,14 +24,28 @@ public class IndirectTesting extends AbstractTestCaseSmell {
         int size = chromosome.size();
 
         Statement currentStatement;
-        GenericMethod method;
+        Method method;
 
         for (int i = 0; i < size; i++){
             currentStatement = chromosome.getTestCase().getStatement(i);
             if(currentStatement instanceof MethodStatement){
-                method = ((MethodStatement) currentStatement).getMethod();
+                method = ((MethodStatement) currentStatement).getMethod().getMethod();
                 if(!method.getDeclaringClass().getCanonicalName().equals(Properties.TARGET_CLASS)){
                     smellCount++;
+                }
+            }
+
+            if(currentStatement.hasAssertions()){
+
+                Set<Assertion> assertions = currentStatement.getAssertions();
+
+                for(Assertion assertion : assertions) {
+                    if (assertion instanceof InspectorAssertion) {
+                        method = ((InspectorAssertion) assertion).getInspector().getMethod();
+                        if(!method.getDeclaringClass().getCanonicalName().equals(Properties.TARGET_CLASS)){
+                            smellCount++;
+                        }
+                    }
                 }
             }
         }
