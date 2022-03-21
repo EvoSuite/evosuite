a80390b27ed50a0953f5da26ea150ef815464e02
João Afonso <johnlandon1999@gmail.com>
2022-03-21 11:49:13 +0000

    lazyTest correction & unit test

--- client/src/main/java/org/evosuite/testsmells/smells/LazyTest.java
+++ client/src/main/java/org/evosuite/testsmells/smells/LazyTest.java
@@ -1,13 +1,16 @@
 package org.evosuite.testsmells.smells;
 
+import org.evosuite.assertion.Assertion;
+import org.evosuite.assertion.InspectorAssertion;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.statements.MethodStatement;
 import org.evosuite.testcase.statements.Statement;
 import org.evosuite.testsmells.AbstractTestSmell;
 import org.evosuite.testsuite.TestSuiteChromosome;
-import org.evosuite.utils.generic.GenericMethod;
 
+import java.lang.reflect.Method;
 import java.util.LinkedHashMap;
+import java.util.Set;
 
 public class LazyTest extends AbstractTestSmell {
 
@@ -20,7 +23,7 @@ public class LazyTest extends AbstractTestSmell {
         Statement currentStatement;
         int count = 0;
 
-        LinkedHashMap<GenericMethod, TestChromosome> methodsCalledByTestCases = new LinkedHashMap<>();
+        LinkedHashMap<Method, TestChromosome> methodsCalledByTestCases = new LinkedHashMap<>();
 
         for(TestChromosome testCase : chromosome.getTestChromosomes()){
 
@@ -31,13 +34,29 @@ public class LazyTest extends AbstractTestSmell {
 
                 if(currentStatement instanceof MethodStatement){
 
-                    GenericMethod method = ((MethodStatement) currentStatement).getMethod();
+                    Method method = ((MethodStatement) currentStatement).getMethod().getMethod();
 
                     //Verify if a different test case tests the same method
                     if(methodsCalledByTestCases.containsKey(method) && !methodsCalledByTestCases.get(method).equals(testCase)){
                         count++;
                     }else{
                         methodsCalledByTestCases.put(method, testCase);
+                    }
+                }
+
+                if(currentStatement.hasAssertions()){
+
+                    Set<Assertion> assertions = currentStatement.getAssertions();
+
+                    for(Assertion assertion : assertions) {
+                        if (assertion instanceof InspectorAssertion) {
+                            Method method = ((InspectorAssertion) assertion).getInspector().getMethod();
+                            if (methodsCalledByTestCases.containsKey(method) && !methodsCalledByTestCases.get(method).equals(testCase)) {
+                                count++;
+                            } else {
+                                methodsCalledByTestCases.put(method, testCase);
+                            }
+                        }
                     }
                 }
             }
---
+++ client/src/test/java/org/evosuite/testsmells/LazyTestSmellTest.java
@@ -0,0 +1,215 @@
+package org.evosuite.testsmells;
+
+import com.examples.with.different.packagename.testsmells.TestSmellsTestingClass1;
+import org.evosuite.Properties;
+import org.evosuite.assertion.Inspector;
+import org.evosuite.assertion.InspectorAssertion;
+import org.evosuite.assertion.PrimitiveAssertion;
+import org.evosuite.symbolic.TestCaseBuilder;
+import org.evosuite.testcase.DefaultTestCase;
+import org.evosuite.testcase.statements.Statement;
+import org.evosuite.testcase.variable.VariableReference;
+import org.evosuite.testsmells.smells.LazyTest;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+public class LazyTestSmellTest {
+
+    AbstractTestSmell lazyTest;
+
+    @Before
+    public void setUp() {
+        Properties.TARGET_CLASS = TestSmellsTestingClass1.class.getCanonicalName();
+        this.lazyTest = new LazyTest();
+    }
+
+    @Test
+    public void testEachTestCaseTestsADifferentMethod() throws NoSuchMethodException {
+        TestSuiteChromosome suite = new TestSuiteChromosome();
+        DefaultTestCase test0 = createTestCase0();
+        DefaultTestCase test1 = createTestCase1();
+        suite.addTest(test0);
+        suite.addTest(test1);
+
+        int smellCount = this.lazyTest.computeNumberOfSmells(suite);
+        int expected = 0;
+        assertEquals(expected, smellCount);
+    }
+
+    @Test
+    public void testAllTestCasesTestTheSameMethod() throws NoSuchMethodException {
+        TestSuiteChromosome suite = new TestSuiteChromosome();
+        DefaultTestCase test0 = createTestCase0();
+        DefaultTestCase test1 = createTestCase0();
+        DefaultTestCase test2 = createTestCase0();
+        suite.addTest(test0);
+        suite.addTest(test1);
+        suite.addTest(test2);
+
+        int smellCount = this.lazyTest.computeNumberOfSmells(suite);
+        int expected = 2;
+        assertEquals(expected, smellCount);
+    }
+
+    @Test
+    public void testTwoTestCasesTestTheSameMethod() throws NoSuchMethodException {
+        TestSuiteChromosome suite = new TestSuiteChromosome();
+        DefaultTestCase test0 = createTestCase0();
+        DefaultTestCase test1 = createTestCase2();
+        suite.addTest(test0);
+        suite.addTest(test1);
+
+        int smellCount = this.lazyTest.computeNumberOfSmells(suite);
+        int expected = 1;
+        assertEquals(expected, smellCount);
+    }
+
+    @Test
+    public void testRepeatedMethodInInspectorAssertion() throws NoSuchMethodException {
+        TestSuiteChromosome suite = new TestSuiteChromosome();
+        DefaultTestCase test0 = createTestCase1();
+        DefaultTestCase test1 = createTestCase3();
+        suite.addTest(test0);
+        suite.addTest(test1);
+
+        int smellCount = this.lazyTest.computeNumberOfSmells(suite);
+        int expected = 2;
+        assertEquals(expected, smellCount);
+    }
+
+    private DefaultTestCase createTestCase0() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        Statement currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase1() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        VariableReference intStatement0 = builder.appendIntPrimitive(5);
+
+        Method setNumberMethod0 = TestSmellsTestingClass1.class.getMethod("setNumber", int.class);
+        builder.appendMethod(constructorStatement0, setNumberMethod0, intStatement0);
+
+        Method getNumberMethod0 = TestSmellsTestingClass1.class.getMethod("getNumber");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNumberMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue(5);
+        Statement currentStatement = testCase.getStatement(4);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase2() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        VariableReference intStatement0 = builder.appendIntPrimitive(5);
+
+        Method setNumberMethod0 = TestSmellsTestingClass1.class.getMethod("setNumber", int.class);
+        builder.appendMethod(constructorStatement0, setNumberMethod0, intStatement0);
+
+        Method getNumberMethod0 = TestSmellsTestingClass1.class.getMethod("getNumber");
+        VariableReference methodStatement1 = builder.appendMethod(constructorStatement0, getNumberMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        Statement currentStatement;
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        PrimitiveAssertion primitiveAssertion1 = new PrimitiveAssertion();
+        primitiveAssertion1.setSource(methodStatement1);
+        primitiveAssertion1.setValue(5);
+        currentStatement = testCase.getStatement(5);
+        currentStatement.addAssertion(primitiveAssertion1);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase3() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        VariableReference intStatement0 = builder.appendIntPrimitive(5);
+
+        Method setNumberMethod0 = TestSmellsTestingClass1.class.getMethod("setNumber", int.class);
+        builder.appendMethod(constructorStatement0, setNumberMethod0, intStatement0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        Statement currentStatement = testCase.getStatement(3);
+        Inspector inspector = new Inspector(TestSmellsTestingClass1.class, TestSmellsTestingClass1.class.getMethod("getNumber"));
+        InspectorAssertion inspectorAssertion0 = new InspectorAssertion(inspector, currentStatement, constructorStatement0, 5);
+        currentStatement.addAssertion(inspectorAssertion0);
+
+        return testCase;
+    }
+}
