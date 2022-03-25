48520c37eaacb0ebe360602017e883c7187a7484
João Afonso <johnlandon1999@gmail.com>
2022-03-25 11:54:03 +0000

    TestRedundancy descriptions

--- client/src/main/java/org/evosuite/testsmells/smells/TestRedundancy.java
+++ client/src/main/java/org/evosuite/testsmells/smells/TestRedundancy.java
@@ -8,6 +8,19 @@ import org.evosuite.testsuite.TestSuiteChromosome;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Detection:
+ * 1 - Retrieve all coverage goals covered by the test suite chromosome
+ * 2 - Iterate over the test cases of the test suite
+ * 3 - Clone the initial test suite
+ * 4 - Get the test chromosomes of the clone
+ * 5 - Delete the current test case of the clone test suite
+ * 6 - Verify if the clone test suites covers the same number of goals as the initial test suite:
+ * 7 (True):
+ *    7.1 - This indicates that it is possible to remove the current test case from the test suite without decreasing
+ *          the code coverage: increment the smell counter
+ * 8 - Return the smell counter
+ */
 public class TestRedundancy extends AbstractTestSmell {
 
     public TestRedundancy() {
