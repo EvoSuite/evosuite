b5073e3f671891b9c28b8a8647d9a3749753eaaf
João Afonso <johnlandon1999@gmail.com>
2022-03-26 16:50:56 +0000

    SlowTests and SensitiveEquality updated description V1

--- client/src/main/java/org/evosuite/testsmells/smells/SensitiveEquality.java
+++ client/src/main/java/org/evosuite/testsmells/smells/SensitiveEquality.java
@@ -11,6 +11,29 @@ import org.evosuite.testsmells.AbstractTestCaseSmell;
 import java.lang.reflect.Method;
 import java.util.Set;
 
+/**
+ * Detection:
+ * 1 - Iterate over the statements of a test case
+ * 2 - Verify if the current statement is an instance of MethodStatement
+ * 3 (2 is True):
+ *    3.1 - Get the name of the method called in the respective statement
+ *    3.2 - Get the name of the class that declares the method
+ *    3.3 - Verify whether (1) the name of the method is equal to "toString" and (2) the class that declares the method
+ *          does not correspond to the class under test - we have to make this distinction because the class under test
+ *          may implement its own version of the "toString" method, so it is necessary to exercise the method
+ *    3.4 (3.3 is True):
+ *       3.4.1 - If the current statement has assertions: increment the smell counter
+ * 4 - Verify if the current statement has assertions
+ * 5 (4 is True):
+ *    5.1 - Iterate over the assertions of the current statement
+ *    5.2 - Verify if the current assertion is an instance of InspectorAssertion
+ *    5.3 (5.2 is True):
+ *       5.3.1 - Get the name of the method on which the assertion is made and the name of the class within
+ *               which this method is declared
+ *       5.3.2 - If the name of the method is equal to "toString" and the class that declares the method is different
+ *               from the class under test: increment the smell counter
+ * 6 - Return the smell counter
+ */
 public class SensitiveEquality extends AbstractTestCaseSmell {
 
     public SensitiveEquality() {
--- client/src/main/java/org/evosuite/testsmells/smells/SlowTests.java
+++ client/src/main/java/org/evosuite/testsmells/smells/SlowTests.java
@@ -3,6 +3,14 @@ package org.evosuite.testsmells.smells;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testsmells.AbstractTestCaseSmell;
 
+/**
+ * 1 - Verify if the last execution result is different than null
+ * 2 (1 is True):
+ *    2.1 - Get the execution time of the last execution result
+ *    2.2 - Return the last execution time
+ * 3 (1 is False):
+ *    3.1 - Return 0
+ */
 public class SlowTests extends AbstractTestCaseSmell {
 
     public SlowTests() {
