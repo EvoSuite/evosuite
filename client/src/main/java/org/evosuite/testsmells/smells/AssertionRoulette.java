b2cce9173163f731bf588e22e7b3870853e96f6d
João Afonso <johnlandon1999@gmail.com>
2022-03-28 15:49:27 +0100

    AssertionRoulette unit test update

--- client/src/main/java/org/evosuite/testsmells/smells/AssertionRoulette.java
+++ client/src/main/java/org/evosuite/testsmells/smells/AssertionRoulette.java
@@ -1,10 +1,37 @@
 package org.evosuite.testsmells.smells;
 
+import org.evosuite.Properties;
 import org.evosuite.ga.FitnessFunction;
 import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.statements.MethodStatement;
 import org.evosuite.testsmells.AbstractTestCaseSmell;
 import org.evosuite.testcase.statements.Statement;
 
+import java.lang.reflect.Method;
+
+/**
+ * Definition:
+ * A test case has several unexplained assertions.
+ *
+ * Adaptation:
+ * This smell typically applies to test cases that have assertions without assertion messages. However, given that
+ * EvoSuite does not add assertions with such messages, this metric focuses on avoiding
+ *
+ * Metric:
+ * Number of assertions in a test case that exceed the number of statements that call methods of the class under test.
+ *
+ * Computation:
+ * 1 - Iterate over the statements of a test case
+ * 2 - Increment the smell counter by the number of assertions in the current statement
+ * 3 - Verify if the current statement is an instance of MethodStatement
+ * 4 (3 is True):
+ *    4.1 - Get the method called in the respective statement
+ *    4.2 - If the class that declares this method is the same as the class under test: increment numStatements counter
+ * 5 - To determine the number of assertions that exceed the number of statements that call methods of the class
+ *     under test, it is necessary to calculate the difference between the smell counter and the numStatements
+ *     counter: Math.max() is used to ensure that the result is always greater than or equal to zero
+ * 6 - Return the normalized result
+ */
 public class AssertionRoulette extends AbstractTestCaseSmell {
 
     public AssertionRoulette() {
@@ -15,14 +42,22 @@ public class AssertionRoulette extends AbstractTestCaseSmell {
     public double computeNumberOfSmells(TestChromosome chromosome) {
         int size = chromosome.size();
         int count = 0;
+        int numStatements = 0;
 
         Statement currentStatement;
 
         for (int i = 0; i < size; i++){
             currentStatement = chromosome.getTestCase().getStatement(i);
             count += currentStatement.getAssertions().size();
+
+            if(currentStatement instanceof MethodStatement){
+                Method method = ((MethodStatement) currentStatement).getMethod().getMethod();
+                if(method.getDeclaringClass().getCanonicalName().equals(Properties.TARGET_CLASS)){
+                    numStatements++;
+                }
+            }
         }
 
-        return FitnessFunction.normalize(count);
+        return FitnessFunction.normalize(Math.max(0, count - numStatements));
     }
 }
--- client/src/test/java/org/evosuite/testsmells/AssertionRouletteSmellTest.java
+++ client/src/test/java/org/evosuite/testsmells/AssertionRouletteSmellTest.java
@@ -1,5 +1,6 @@
 package org.evosuite.testsmells;
 
+import com.examples.with.different.packagename.testsmells.TestSmellsSimpleUser;
 import com.examples.with.different.packagename.testsmells.TestSmellsTestingClass1;
 import org.evosuite.Properties;
 import org.evosuite.assertion.NullAssertion;
@@ -36,40 +37,84 @@ public class AssertionRouletteSmellTest {
         testCase.setTestCase(test0);
 
         double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
-        double expected = 0;
+        double expected = 0.0;
         assertEquals(expected, smellCount, 0.01);
     }
 
     @Test
-    public void testOneAssertion() throws NoSuchMethodException {
+    public void testOneAssertionOneStatementThatCallsMethodOfTheClassUnderTest() throws NoSuchMethodException {
         TestChromosome testCase = new TestChromosome();
         DefaultTestCase test0 = createTestCase1();
         testCase.setTestCase(test0);
 
         double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
-        double expected = 1;
+        double expected = 0.0;
         assertEquals(expected, smellCount, 0.01);
     }
 
     @Test
-    public void testOneStatementWithMultipleAssertions() throws NoSuchMethodException {
+    public void testTwoAssertionsOneStatementThatCallsMethodOfTheClassUnderTest() throws NoSuchMethodException {
         TestChromosome testCase = new TestChromosome();
         DefaultTestCase test0 = createTestCase2();
         testCase.setTestCase(test0);
 
         double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
-        double expected = 2;
+        double expected = 0.5;
         assertEquals(expected, smellCount, 0.01);
     }
 
     @Test
-    public void testSeveralStatementsWithAssertions() throws NoSuchMethodException {
+    public void testOneAssertionTwoStatementsThatCallMethodsOfTheClassUnderTest() throws NoSuchMethodException {
         TestChromosome testCase = new TestChromosome();
         DefaultTestCase test0 = createTestCase3();
         testCase.setTestCase(test0);
 
         double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
-        double expected = 3;
+        double expected = 0.0;
+        assertEquals(expected, smellCount, 0.01);
+    }
+
+    @Test
+    public void testOneAssertionOneStatementThatCallsMethodOfDifferentClass() throws NoSuchMethodException {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase4();
+        testCase.setTestCase(test0);
+
+        double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
+        double expected = 0.5;
+        assertEquals(expected, smellCount, 0.01);
+    }
+
+    @Test
+    public void testTwoAssertionsOneStatementThatCallsMethodOfDifferentClass() throws NoSuchMethodException {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase5();
+        testCase.setTestCase(test0);
+
+        double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
+        double expected = 2.0 / (1.0 + 2.0);
+        assertEquals(expected, smellCount, 0.01);
+    }
+
+    @Test
+    public void testOneAssertionTwoStatementsThatCallMethodsOfDifferentClass() throws NoSuchMethodException {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase6();
+        testCase.setTestCase(test0);
+
+        double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
+        double expected = 0.5;
+        assertEquals(expected, smellCount, 0.01);
+    }
+
+    @Test
+    public void testEightAssertionsFiveStatementsThatCallMethodsOfTheClassUnderTest() throws NoSuchMethodException {
+        TestChromosome testCase = new TestChromosome();
+        DefaultTestCase test0 = createTestCase7();
+        testCase.setTestCase(test0);
+
+        double smellCount = this.assertionRoulette.computeNumberOfSmells(testCase);
+        double expected = 0.75;
         assertEquals(expected, smellCount, 0.01);
     }
 
@@ -80,13 +125,21 @@ public class AssertionRouletteSmellTest {
         DefaultTestCase test1 = createTestCase1();
         DefaultTestCase test2 = createTestCase2();
         DefaultTestCase test3 = createTestCase3();
+        DefaultTestCase test4 = createTestCase4();
+        DefaultTestCase test5 = createTestCase5();
+        DefaultTestCase test6 = createTestCase6();
+        DefaultTestCase test7 = createTestCase7();
         suite.addTest(test0);
         suite.addTest(test1);
         suite.addTest(test2);
         suite.addTest(test3);
+        suite.addTest(test4);
+        suite.addTest(test5);
+        suite.addTest(test6);
+        suite.addTest(test7);
 
         double smellCount = this.assertionRoulette.computeNumberOfSmells(suite);
-        double expected = 6;
+        double expected = 0.5 * 3 + 0.75 + 2.0 / (1.0 + 2.0);
         assertEquals(expected, smellCount, 0.01);
     }
 
@@ -188,8 +241,138 @@ public class AssertionRouletteSmellTest {
         Method setNumberMethod0 = TestSmellsTestingClass1.class.getMethod("setNumber", int.class);
         builder.appendMethod(constructorStatement0, setNumberMethod0, intStatement0);
 
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        Statement currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase4() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsSimpleUser> const0 = TestSmellsSimpleUser.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsSimpleUser.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        Statement currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase5() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsSimpleUser> const0 = TestSmellsSimpleUser.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsSimpleUser.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        Statement currentStatement;
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        NullAssertion nullAssertion0 = new NullAssertion();
+        nullAssertion0.setSource(methodStatement0);
+        nullAssertion0.setValue(false);
+        currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(nullAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase6() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsSimpleUser> const0 = TestSmellsSimpleUser.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsSimpleUser.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        Method getNameMethod1 = TestSmellsSimpleUser.class.getMethod("getName");
+        builder.appendMethod(constructorStatement0, getNameMethod1);
+
+        DefaultTestCase testCase = builder.getDefaultTestCase();
+
+        // Add assertions
+
+        PrimitiveAssertion primitiveAssertion0 = new PrimitiveAssertion();
+        primitiveAssertion0.setSource(methodStatement0);
+        primitiveAssertion0.setValue("Bob");
+        Statement currentStatement = testCase.getStatement(2);
+        currentStatement.addAssertion(primitiveAssertion0);
+
+        return testCase;
+    }
+
+    private DefaultTestCase createTestCase7() throws NoSuchMethodException {
+
+        // Create test case
+
+        TestCaseBuilder builder = new TestCaseBuilder();
+
+        VariableReference stringStatement0 = builder.appendStringPrimitive("Bob");
+
+        Constructor<TestSmellsTestingClass1> const0 = TestSmellsTestingClass1.class.getConstructor(String.class);
+        VariableReference constructorStatement0 = builder.appendConstructor(const0, stringStatement0);
+
+        Method getNameMethod0 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement0 = builder.appendMethod(constructorStatement0, getNameMethod0);
+
+        VariableReference intStatement0 = builder.appendIntPrimitive(5);
+
+        Method setNumberMethod0 = TestSmellsTestingClass1.class.getMethod("setNumber", int.class);
+        builder.appendMethod(constructorStatement0, setNumberMethod0, intStatement0);
+
         Method getNumberMethod0 = TestSmellsTestingClass1.class.getMethod("getNumber");
-        VariableReference methodStatement2 = builder.appendMethod(constructorStatement0, getNumberMethod0);
+        VariableReference methodStatement1 = builder.appendMethod(constructorStatement0, getNumberMethod0);
+
+        Method getNameMethod1 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement2 = builder.appendMethod(constructorStatement0, getNameMethod1);
+
+        Method getNameMethod2 = TestSmellsTestingClass1.class.getMethod("getName");
+        VariableReference methodStatement3 = builder.appendMethod(constructorStatement0, getNameMethod2);
 
         DefaultTestCase testCase = builder.getDefaultTestCase();
 
@@ -210,10 +393,40 @@ public class AssertionRouletteSmellTest {
         currentStatement.addAssertion(nullAssertion0);
 
         PrimitiveAssertion primitiveAssertion1 = new PrimitiveAssertion();
-        primitiveAssertion1.setSource(methodStatement2);
+        primitiveAssertion1.setSource(methodStatement1);
         primitiveAssertion1.setValue(5);
         currentStatement = testCase.getStatement(5);
         currentStatement.addAssertion(primitiveAssertion1);
+
+        NullAssertion nullAssertion1 = new NullAssertion();
+        nullAssertion1.setSource(methodStatement1);
+        nullAssertion1.setValue(false);
+        currentStatement = testCase.getStatement(5);
+        currentStatement.addAssertion(nullAssertion1);
+
+        PrimitiveAssertion primitiveAssertion2 = new PrimitiveAssertion();
+        primitiveAssertion2.setSource(methodStatement2);
+        primitiveAssertion2.setValue("Bob");
+        currentStatement = testCase.getStatement(6);
+        currentStatement.addAssertion(primitiveAssertion2);
+
+        NullAssertion nullAssertion2 = new NullAssertion();
+        nullAssertion2.setSource(methodStatement2);
+        nullAssertion2.setValue(false);
+        currentStatement = testCase.getStatement(6);
+        currentStatement.addAssertion(nullAssertion2);
+
+        PrimitiveAssertion primitiveAssertion3 = new PrimitiveAssertion();
+        primitiveAssertion3.setSource(methodStatement3);
+        primitiveAssertion3.setValue("Bob");
+        currentStatement = testCase.getStatement(7);
+        currentStatement.addAssertion(primitiveAssertion3);
+
+        NullAssertion nullAssertion3 = new NullAssertion();
+        nullAssertion3.setSource(methodStatement3);
+        nullAssertion3.setValue(false);
+        currentStatement = testCase.getStatement(7);
+        currentStatement.addAssertion(nullAssertion3);
 
         return testCase;
     }
