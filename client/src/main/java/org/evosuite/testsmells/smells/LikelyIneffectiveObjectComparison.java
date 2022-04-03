d6405defb2e53094d195f8382613ba231986477f
João Afonso <johnlandon1999@gmail.com>
2022-04-03 16:58:04 +0100

    LikelyIneffectiveObjectComparison update

--- client/src/main/java/org/evosuite/testsmells/smells/LikelyIneffectiveObjectComparison.java
+++ client/src/main/java/org/evosuite/testsmells/smells/LikelyIneffectiveObjectComparison.java
@@ -9,6 +9,26 @@ import org.evosuite.testsmells.AbstractNormalizedTestCaseSmell;
 
 import java.util.List;
 
+/**
+ * Definition:
+ * A test case has object comparisons that will never fail.
+ *
+ * Metric:
+ * Number of times the "equals" method of a class other than the one under test is used to compare an object with itself.
+ *
+ * Computation:
+ * 1 - Iterate over the statements of a test case
+ * [1: Start loop]
+ * 2 - Verify if the current statement is an instance of MethodStatement
+ * 3 (2 is True):
+ *    3.1 - Verify if (1) the current method is named "equals" and (2) the class that declares this method is different
+ *          from the class under test
+ *    3.2 (3.1 is True):
+ *       3.2.1 - If the "equals" method is used to compare two objects which were created in the same position of the
+ *               test case (i.e., an object is being compared with itself): increment the smell counter
+ * [1: End loop]
+ * 4 - Return the smell counter
+ */
 public class LikelyIneffectiveObjectComparison extends AbstractNormalizedTestCaseSmell {
 
     public LikelyIneffectiveObjectComparison() {
@@ -29,12 +49,12 @@ public class LikelyIneffectiveObjectComparison extends AbstractNormalizedTestCas
 
                 String curr = ((MethodStatement) currentStatement).getMethodName();
                 String className = ((MethodStatement) currentStatement).getDeclaringClassName();
-                VariableReference callee = ((MethodStatement) currentStatement).getCallee();
 
-                if(curr.equals("equals") && !className.equals(Properties.TARGET_CLASS) && callee != null){
+                if(curr.equals("equals") && !className.equals(Properties.TARGET_CLASS)){
+                    VariableReference callee = ((MethodStatement) currentStatement).getCallee();
                     List<VariableReference> parameters = ((MethodStatement) currentStatement).getParameterReferences();
 
-                    if(parameters.size() == 1 && callee.getStPosition() == parameters.get(0).getStPosition()){
+                    if(parameters.size() == 1 && callee != null && callee.getStPosition() == parameters.get(0).getStPosition()){
                         count++;
                     }
                 }
--- client/src/test/java/org/evosuite/testsmells/LikelyIneffectiveObjectComparisonSmellTest.java
+++ client/src/test/java/org/evosuite/testsmells/LikelyIneffectiveObjectComparisonSmellTest.java
@@ -34,9 +34,13 @@ public class LikelyIneffectiveObjectComparisonSmellTest {
         DefaultTestCase test0 = createTestCase0();
         testCase.setTestCase(test0);
 
-        double smellCount = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
-        double expected = 0;
-        assertEquals(expected, smellCount, 0.01);
+        long smellCount = this.likelyIneffectiveObjectComparison.computeNumberOfTestSmells(testCase);
+        long expectedSmellCount = 0;
+        assertEquals(expectedSmellCount, smellCount);
+
+        double computedMetric = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
+        double expectedComputedMetric = 0;
+        assertEquals(expectedComputedMetric, computedMetric, 0.01);
     }
 
     @Test
@@ -45,9 +49,13 @@ public class LikelyIneffectiveObjectComparisonSmellTest {
         DefaultTestCase test0 = createTestCase1();
         testCase.setTestCase(test0);
 
-        double smellCount = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
-        double expected = 0;
-        assertEquals(expected, smellCount, 0.01);
+        long smellCount = this.likelyIneffectiveObjectComparison.computeNumberOfTestSmells(testCase);
+        long expectedSmellCount = 0;
+        assertEquals(expectedSmellCount, smellCount);
+
+        double computedMetric = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
+        double expectedComputedMetric = 0;
+        assertEquals(expectedComputedMetric, computedMetric, 0.01);
     }
 
     @Test
@@ -56,9 +64,13 @@ public class LikelyIneffectiveObjectComparisonSmellTest {
         DefaultTestCase test0 = createTestCase2();
         testCase.setTestCase(test0);
 
-        double smellCount = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
-        double expected = 0;
-        assertEquals(expected, smellCount, 0.01);
+        long smellCount = this.likelyIneffectiveObjectComparison.computeNumberOfTestSmells(testCase);
+        long expectedSmellCount = 0;
+        assertEquals(expectedSmellCount, smellCount);
+
+        double computedMetric = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
+        double expectedComputedMetric = 0;
+        assertEquals(expectedComputedMetric, computedMetric, 0.01);
     }
 
     @Test
@@ -67,9 +79,13 @@ public class LikelyIneffectiveObjectComparisonSmellTest {
         DefaultTestCase test0 = createTestCase3();
         testCase.setTestCase(test0);
 
-        double smellCount = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
-        double expected = 1;
-        assertEquals(expected, smellCount, 0.01);
+        long smellCount = this.likelyIneffectiveObjectComparison.computeNumberOfTestSmells(testCase);
+        long expectedSmellCount = 1;
+        assertEquals(expectedSmellCount, smellCount);
+
+        double computedMetric = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(testCase);
+        double expectedComputedMetric = 0.5;
+        assertEquals(expectedComputedMetric, computedMetric, 0.01);
     }
 
     @Test
@@ -84,9 +100,9 @@ public class LikelyIneffectiveObjectComparisonSmellTest {
         suite.addTest(test2);
         suite.addTest(test3);
 
-        double smellCount = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(suite);
-        double expected = 1;
-        assertEquals(expected, smellCount, 0.01);
+        double computedMetric = this.likelyIneffectiveObjectComparison.computeTestSmellMetric(suite);
+        double expectedComputedMetric = 0.5;
+        assertEquals(expectedComputedMetric, computedMetric, 0.01);
     }
 
     private DefaultTestCase createTestCase0() throws NoSuchMethodException {
