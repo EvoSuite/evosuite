9beae9ce6c07167f2caafd0c94e4e6a0d1ad9b8f
João Afonso <johnlandon1999@gmail.com>
2022-04-03 22:40:01 +0100

    ResourceOptimism new version

--- client/src/main/java/org/evosuite/testsmells/smells/ResourceOptimism.java
+++ client/src/main/java/org/evosuite/testsmells/smells/ResourceOptimism.java
@@ -1,17 +1,13 @@
 package org.evosuite.testsmells.smells;
 
 import org.evosuite.testcase.TestChromosome;
-import org.evosuite.testcase.TestCodeVisitor;
 import org.evosuite.testcase.statements.ConstructorStatement;
 import org.evosuite.testcase.statements.MethodStatement;
 import org.evosuite.testcase.statements.Statement;
 import org.evosuite.testcase.variable.VariableReference;
 import org.evosuite.testsmells.AbstractNormalizedTestCaseSmell;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class ResourceOptimism extends AbstractNormalizedTestCaseSmell {
 
@@ -24,10 +20,9 @@ public class ResourceOptimism extends AbstractNormalizedTestCaseSmell {
         int size = chromosome.size();
         long count = 0;
 
-        LinkedHashMap<Integer, Boolean> filesUsedByTestCase = new LinkedHashMap<>();
         Statement currentStatement;
 
-        TestCodeVisitor visitor = new TestCodeVisitor();
+        LinkedHashMap<Integer, Boolean> filesUsedByTestCase = new LinkedHashMap<>();
 
         List<String> optimismMethods = new ArrayList<>(
                 Arrays.asList(
@@ -41,21 +36,21 @@ public class ResourceOptimism extends AbstractNormalizedTestCaseSmell {
             currentStatement = chromosome.getTestCase().getStatement(i);
 
             if (currentStatement instanceof ConstructorStatement) {
-                if (visitor.getClassName(currentStatement.getReturnClass()).equals("File")) {
+
+                if(currentStatement.getReturnClass().getSimpleName().equals("File")){
                     filesUsedByTestCase.put(i, false);
                 }
-            }
 
-            if (currentStatement instanceof MethodStatement) {
+            } else if (currentStatement instanceof MethodStatement) {
 
                 String methodName = ((MethodStatement) currentStatement).getMethodName();
-                List<VariableReference> parameters = ((MethodStatement) currentStatement).getParameterReferences();
                 VariableReference callee = ((MethodStatement) currentStatement).getCallee();
 
                 int position;
 
-                for(VariableReference parameter : parameters){
-                    position = parameter.getStPosition();
+                if(callee != null){
+                    position = callee.getStPosition();
+
                     if (filesUsedByTestCase.containsKey(position)) {
                         if (optimismMethods.contains(methodName)) {
                             filesUsedByTestCase.put(position, true);
@@ -67,9 +62,10 @@ public class ResourceOptimism extends AbstractNormalizedTestCaseSmell {
                     }
                 }
 
-                if(callee != null){
-                    position = callee.getStPosition();
+                List<VariableReference> parameters = ((MethodStatement) currentStatement).getParameterReferences();
 
+                for(VariableReference parameter : parameters){
+                    position = parameter.getStPosition();
                     if (filesUsedByTestCase.containsKey(position)) {
                         if (optimismMethods.contains(methodName)) {
                             filesUsedByTestCase.put(position, true);
