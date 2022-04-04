643d89b4829b70de240d25957fa85818f8dccd06
João Afonso <johnlandon1999@gmail.com>
2022-04-04 17:01:48 +0100

    RottenGreenTests description

--- client/src/main/java/org/evosuite/testsmells/smells/RottenGreenTests.java
+++ client/src/main/java/org/evosuite/testsmells/smells/RottenGreenTests.java
@@ -7,6 +7,25 @@ import org.evosuite.testsmells.AbstractNormalizedTestCaseSmell;
 import java.util.Collections;
 import java.util.Set;
 
+/**
+ * Definition:
+ * Tests have assertions that are not executed, thus giving a false sense of security.
+ *
+ * Adaptation:
+ * In the proposed metric, this smell occurs if a test case continues to exercise code after the statement in which
+ * the first exception was raised.
+ *
+ * Metric:
+ * Number of statements that there are in a test case after the statement that raised the first exception.
+ *
+ * Computation:
+ * 1 - Verify if the last execution result is not null
+ * 2 (1 is True):
+ *    2.1 - Get the positions of the statements that raised exceptions in the last execution of the test case
+ *    2.2 - If any exceptions were raised: return the number of statements that exist after the position of the
+ *          statement that raised the first exception
+ * 3 - Return 0
+ */
 public class RottenGreenTests extends AbstractNormalizedTestCaseSmell {
 
     public RottenGreenTests() {
