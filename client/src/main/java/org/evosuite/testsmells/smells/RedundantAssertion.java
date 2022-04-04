d15addff42330d158f1f6d84be35ddf9bab96863
João Afonso <johnlandon1999@gmail.com>
2022-04-04 04:05:51 +0100

    RedundantAssertion description and unit test

--- client/src/main/java/org/evosuite/testsmells/smells/RedundantAssertion.java
+++ client/src/main/java/org/evosuite/testsmells/smells/RedundantAssertion.java
@@ -9,6 +9,25 @@ import org.evosuite.testcase.statements.Statement;
 
 import java.util.Set;
 
+/**
+ * Definition:
+ * Test cases that have assertions that are either permanently true or permanently false.
+ *
+ * Metric:
+ * Count the total number of assertions that check primitive statements.
+ *
+ * Computation:
+ * 1 - Iterate over the statements of a test case
+ * [1: Start loop]
+ * 2 - Verify if the current statement is an instance of PrimitiveStatement
+ * 3 (2 is True):
+ *    3.1 - Iterate over the assertions of the current statement
+ *    [3.1: Start loop]
+ *    3.2 - If the current assertion is not an instance of InspectorAssertion: increment the smell counter
+ *    [3.1: End loop]
+ * [1: End loop]
+ * 4 - Return the smell counter
+ */
 public class RedundantAssertion extends AbstractNormalizedTestCaseSmell {
 
     public RedundantAssertion() {
