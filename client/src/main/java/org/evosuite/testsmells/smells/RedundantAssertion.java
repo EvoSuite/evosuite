91d1c71ba865de12d7bbd4c5588cc471130dff25
João Afonso <johnlandon1999@gmail.com>
2022-04-04 11:40:07 +0100

    RedundantAssertion correction

--- client/src/main/java/org/evosuite/testsmells/smells/RedundantAssertion.java
+++ client/src/main/java/org/evosuite/testsmells/smells/RedundantAssertion.java
@@ -2,6 +2,7 @@ package org.evosuite.testsmells.smells;
 
 import org.evosuite.assertion.Assertion;
 import org.evosuite.assertion.InspectorAssertion;
+import org.evosuite.testcase.TestCase;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.statements.PrimitiveStatement;
 import org.evosuite.testsmells.AbstractNormalizedTestCaseSmell;
@@ -19,14 +20,15 @@ import java.util.Set;
  * Computation:
  * 1 - Iterate over the statements of a test case
  * [1: Start loop]
- * 2 - Verify if the current statement is an instance of PrimitiveStatement
- * 3 (2 is True):
- *    3.1 - Iterate over the assertions of the current statement
- *    [3.1: Start loop]
- *    3.2 - If the current assertion is not an instance of InspectorAssertion: increment the smell counter
- *    [3.1: End loop]
+ * 2 - Iterate over the assertions of the current statement
+ * [2: Start loop]
+ * 3 - Verify if the current assertion is not an instance of InspectorAssertion
+ * 4 (3 is True):
+ *    4.1 - Get the statement that contains the variable on which the assertion is made
+ *    4.2 - If this statement is an instance of PrimitiveStatement: increment the smell counter
+ * [2: End loop]
  * [1: End loop]
- * 4 - Return the smell counter
+ * 5 - Return the smell counter
  */
 public class RedundantAssertion extends AbstractNormalizedTestCaseSmell {
 
@@ -39,17 +41,17 @@ public class RedundantAssertion extends AbstractNormalizedTestCaseSmell {
         int size = chromosome.size();
         long count = 0;
 
-        Statement currentStatement;
+        TestCase testCase = chromosome.getTestCase();
 
         for (int i = 0; i < size; i++){
-            currentStatement = chromosome.getTestCase().getStatement(i);
 
-            if(currentStatement instanceof PrimitiveStatement){
+            Set<Assertion> assertions = testCase.getStatement(i).getAssertions();
 
-                Set<Assertion> assertions = currentStatement.getAssertions();
+            for(Assertion assertion : assertions){
 
-                for(Assertion assertion : assertions){
-                    count += assertion instanceof InspectorAssertion ? 0 : 1;
+                if(!(assertion instanceof InspectorAssertion)){
+                    Statement variableStatement = testCase.getStatement(assertion.getSource().getStPosition());
+                    count += variableStatement instanceof PrimitiveStatement ? 1 : 0;
                 }
             }
         }
