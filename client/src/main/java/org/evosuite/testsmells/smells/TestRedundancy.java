26c4aec52656e47419d3f846fa696b9b7c0b7cc6
João Afonso <johnlandon1999@gmail.com>
2022-04-04 03:05:48 +0100

    TestRedundancy description update

--- client/src/main/java/org/evosuite/testsmells/smells/TestRedundancy.java
+++ client/src/main/java/org/evosuite/testsmells/smells/TestRedundancy.java
@@ -10,17 +10,25 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * Definition:
+ * A test case that can be removed without affecting the fault detection effectiveness of the test suite.
+ *
+ * Metric:
+ * The number of test cases that can be removed from the test suite without decreasing the code coverage.
+ *
  * Detection:
  * 1 - Retrieve all coverage goals covered by the test suite chromosome
  * 2 - Iterate over the test cases of the test suite
+ * [2: Start loop]
  * 3 - Clone the initial test suite
  * 4 - Get the test chromosomes of the clone
  * 5 - Delete the current test case of the clone test suite
- * 6 - Verify if the clone test suites covers the same number of goals as the initial test suite:
- * 7 (True):
+ * 6 - Verify if the clone test suite covers the same number of goals as the initial test suite:
+ * 7 (6 is True):
  *    7.1 - This indicates that it is possible to remove the current test case from the test suite without decreasing
  *          the code coverage: increment the smell counter
- * 8 - Return the smell counter
+ * [2: End loop]
+ * 8 - Return the normalized value for the smell counter
  */
 public class TestRedundancy extends AbstractTestSmell {
 
