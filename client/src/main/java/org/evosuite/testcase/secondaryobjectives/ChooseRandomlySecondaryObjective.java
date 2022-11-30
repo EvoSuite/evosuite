c82e320020115da19cbb9e823cb66c5a4e0f605c
João Afonso <johnlandon1999@gmail.com>
2022-11-30 19:48:35 +0000

    Use Randomness instead of Random

--- client/src/main/java/org/evosuite/testcase/secondaryobjectives/ChooseRandomlySecondaryObjective.java
+++ client/src/main/java/org/evosuite/testcase/secondaryobjectives/ChooseRandomlySecondaryObjective.java
@@ -2,25 +2,22 @@ package org.evosuite.testcase.secondaryobjectives;
 
 import org.evosuite.ga.SecondaryObjective;
 import org.evosuite.testcase.TestChromosome;
-
-import java.util.Random;
+import org.evosuite.utils.Randomness;
 
 public class ChooseRandomlySecondaryObjective extends SecondaryObjective<TestChromosome> {
 
     private static final long serialVersionUID = -8543152934847182447L;
-
-    private final Random random = new Random();
 
     /**
      * {@inheritDoc}
      */
     @Override
     public double compareChromosomes(TestChromosome chromosome1, TestChromosome chromosome2) {
-        return random.nextBoolean() ? -1 : 1;
+        return Randomness.nextBoolean() ? -1 : 1;
     }
 
     @Override
     public double compareGenerations(TestChromosome parent1, TestChromosome parent2, TestChromosome child1, TestChromosome child2) {
-        return random.nextBoolean() ? -1 : 1;
+        return Randomness.nextBoolean() ? -1 : 1;
     }
 }
--- client/src/main/java/org/evosuite/testsuite/secondaryobjectives/ChooseRandomlySecondaryObjective.java
+++ client/src/main/java/org/evosuite/testsuite/secondaryobjectives/ChooseRandomlySecondaryObjective.java
@@ -2,22 +2,19 @@ package org.evosuite.testsuite.secondaryobjectives;
 
 import org.evosuite.ga.SecondaryObjective;
 import org.evosuite.testsuite.TestSuiteChromosome;
-
-import java.util.Random;
+import org.evosuite.utils.Randomness;
 
 public class ChooseRandomlySecondaryObjective extends SecondaryObjective<TestSuiteChromosome> {
 
     private static final long serialVersionUID = -3687478373591441255L;
 
-    private final Random random = new Random();
-
     @Override
     public double compareChromosomes(TestSuiteChromosome chromosome1, TestSuiteChromosome chromosome2) {
-        return random.nextBoolean() ? -1 : 1;
+        return Randomness.nextBoolean() ? -1 : 1;
     }
 
     @Override
     public double compareGenerations(TestSuiteChromosome parent1, TestSuiteChromosome parent2, TestSuiteChromosome child1, TestSuiteChromosome child2) {
-        return random.nextBoolean() ? -1 : 1;
+        return Randomness.nextBoolean() ? -1 : 1;
     }
 }
